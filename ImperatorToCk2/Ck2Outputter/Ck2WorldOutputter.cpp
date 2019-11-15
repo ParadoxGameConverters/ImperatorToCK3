86cac9f3bf18a1d3571648b9e1e10c0097612fa7
Idhrendur <idhrendur@hotmail.com>
2019-11-14 17:22:18 -0800

    Output mod file with hard-coded name

--- ImperatorToCk2/Ck2Outputter/Ck2WorldOutputter.cpp
+++ ImperatorToCk2/Ck2Outputter/Ck2WorldOutputter.cpp
@@ -1,11 +1,30 @@
 #include "Ck2WorldOutputter.h"
 #include <fstream>
+#include <string>
 
 
+
+namespace Ck2WorldOutputter
+{
+
+void outputModFile(const std::string& outputName);
+
+}
 
 void Ck2WorldOutputter::outputWorld(const Ck2Interface::World& world)
 {
 	std::ofstream output("output.txt");
 	output << world.getMessage();
 	output.close();
+
+	outputModFile("CK2tester");
+}
+
+
+void Ck2WorldOutputter::outputModFile(const std::string& outputName)
+{
+	std::ofstream modFile("output/" + outputName + ".mod");
+	modFile << "name = \"Converted - " << outputName << "\"\n";
+	modFile << "path = \"mod/" << outputName << "\"";
+	modFile.close();
 }
