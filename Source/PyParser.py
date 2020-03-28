187708e64e6de14170605ea038a0a2f9b2d7b32f
IlikeTrains <sewerynpresnal@gmail.com>
2020-03-28 11:44:34 +0100

    Cleaning of PyParser, expanded .gitignore

--- Source/PyParser.py
+++ Source/PyParser.py
@@ -1,9 +1,9 @@
-import re #regular expressions
+import re  # regular expressions
 
-registeredKeywordStrings = {} #dict
-registeredKeywordRegexes = [] #tuple
-registeredRegexes = [] #tuple
-generatedRegexes = [] #tuple
+registeredKeywordStrings = {}  # dict
+registeredKeywordRegexes = []  # tuple
+registeredRegexes = []  # tuple
+generatedRegexes = []  # tuple
 
 
 def peek_char(file):
@@ -13,15 +13,15 @@ def peek_char(file):
     return char
 
 
-def registerKeyword (keyword, function, regex=False): #regex is bool: True if the keyword string is meant to be a regex
+def registerKeyword(keyword, function, regex=False):  # regex is bool: True if the keyword string is meant to be regex
     if regex:
         pair = (keyword, function)
         registeredRegexes.append(pair)
     else:
-        registeredKeywordStrings.update({keyword, function})
+        registeredKeywordStrings.update({keyword: function})
+        print(registeredKeywordStrings.items())
 
-
-def registerRegex (keyword, function):
+def registerRegex(keyword, function):
     pair = (keyword, function)
     registeredKeywordRegexes.append(pair)
 
@@ -30,30 +30,30 @@ def clearRegisteredKeywords():
     registeredKeywordStrings.clear()
     registeredKeywordRegexes.clear()
     registeredRegexes.clear()
-    
+
 
 def getNextLexeme(theStream):
     toReturn = ''
     inQuotes = False
-    while (peek_char(theStream)):
+    while peek_char(theStream):
         inputChar = peek_char(theStream)
-        if (not inQuotes and inputChar=='#'):
+        if not inQuotes and inputChar == '#':
             inputChar = theStream.read(1)
             bitBucket = theStream.readline()
-            if (toReturn !=''):
+            if toReturn != '':
                 break
-        elif inputChar=='\n':
+        elif inputChar == '\n':
             inputChar = theStream.read(1)
             if not inQuotes:
-                if toReturn !='':
+                if toReturn != '':
                     break
             else:
-                toReturn += ' ' # fix Paradox' mistake and don't break proper names in half
-        elif inputChar=='"' and not inQuotes and toReturn=='':
+                toReturn += ' '  # fix Paradox' mistake and don't break proper names in half
+        elif inputChar == '"' and not inQuotes and toReturn == '':
             inputChar = theStream.read(1)
             inQuotes = True
             toReturn += inputChar
-        elif inputChar=='"' and inQuotes:
+        elif inputChar == '"' and inQuotes:
             inputChar = theStream.read(1)
             toReturn += inputChar
             break
@@ -86,7 +86,7 @@ def getNextToken(theStream):
     toReturn = ''
     gotToken = False
     while not gotToken:
-        if (not peek_char(theStream)): #I use this instead of eof
+        if not peek_char(theStream):  # I use this instead of eof
             return None
         toReturn = getNextLexeme(theStream)
         matched = False
@@ -94,14 +94,14 @@ def getNextToken(theStream):
         if pair[1] is not None:
             pair[1](toReturn, theStream)
             matched = True
-            
+
         if not matched:
             for registration in generatedRegexes:
                 if re.match(registration[0], toReturn):
                     registration[1](toReturn, theStream)
                     matched = True
                     break
-                
+
         if not matched:
             for registration in registeredRegexes:
                 if re.match(registration[0], toReturn):
@@ -110,50 +110,50 @@ def getNextToken(theStream):
                     break
         if not matched:
             gotToken = True
-    if not toReturn=='':
+    if not toReturn == '':
         return toReturn
     return None
-                
-        
+
 
 def getNextTokenWithoutMatching(theStream):
     toReturn = ''
     gotToken = False
     while not gotToken:
-        if (not peek_char(theStream)): #I use this instead of eof
+        if not peek_char(theStream):  # I use this instead of eof
             return None
         toReturn = getNextLexeme(theStream)
         gotToken = True
-    if not toReturn=='':
+    if not toReturn == '':
         return toReturn
     return None
-
 
 
 def parseStream(theStream):
     braceDepth = 0
     for keywordItr in registeredKeywordRegexes:
-        pair = (keywordItr[0], keywordItr[1]) #keywordItr[0] is regex, keywordItr[1] is parsingfunction
+        pair = (keywordItr[0], keywordItr[1])  # keywordItr[0] is regex, keywordItr[1] is parsingfunction
         generatedRegexes.append(pair)
 
-    while(True):
+    while True:
         token = getNextToken(theStream)
-        if (token):
+        if token:
             if token == '=':
                 continue
             if token == '{':
                 braceDepth += 1
             elif token == '}':
                 braceDepth -= 1
-                if braceDepth==0:
+                if braceDepth == 0:
                     break
-            else: print("Unknown token while parsing stream: " + token)
-        else: break
+            else:
+                print("Unknown token while parsing stream: " + token)
+        else:
+            break
     generatedRegexes.clear()
 
-        
+
 def parseFile(filename):
-    #filename = filename.decode('utf8') #TODO: fix
+    # filename = filename.decode('utf8')  # TODO: fix
     try:
         theFile = open(filename, 'r')
     except FileNotFoundError:
@@ -162,16 +162,15 @@ def parseFile(filename):
     except:
         print("Could not open " + filename + " for parsing.")
         return
-	
+
     firstChar = peek_char(theFile)
-    if (firstChar == '\xEF'):
-        bitBucket=theFile.read(3)
+    if firstChar == '\xEF':
+        bitBucket = theFile.read(3)
 
     parseStream(theFile)
     theFile.close()
 
-
-#temp:
-print("Insert save filename")
-filename = input()
-parseFile(filename)
+# temp:
+# print("Insert save filename")
+# filename = input()
+# parseFile(filename)
