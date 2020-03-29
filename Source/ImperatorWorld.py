41a4f60cd7b4e40c78c6a359f0eeba4cddf2db84
IlikeTrains <sewerynpresnal@gmail.com>
2020-03-29 21:52:52 +0200

    Read mod list

--- Source/ImperatorWorld.py
+++ Source/ImperatorWorld.py
@@ -4,14 +4,19 @@ from ParserHelpers import getSingleString, getStringList
 unused = ''
 
 DateTextOutput = []
-def getDateText(dateText, theStream):
-    DateTextOutput.append('')  # DateTextOutput[0] empty, dateText is not used
+def getDateText(unused, theStream):
+    DateTextOutput.append('')  # DateTextOutput[0] empty, keyword is not used
     DateTextOutput.append(getSingleString(theStream))  # so DateTextOutput[1] is dateString
 
-DLCOutput = []
-def getDLCOutput(enabled_dlcs, theStream):
-    DLCOutput.append('')  # DLCOutput[0] empty, enabled_dlcs is not used
-    DLCOutput.append(getStringList(theStream))  # so DLCOutput[1] is a list of DLC strings
+DLCsOutput = []
+def getDLCsOutput(unused, theStream):
+    DLCsOutput.append('')  # DLCsOutput[0] empty, keyword is not used
+    DLCsOutput.append(getStringList(theStream))  # so DLCsOutput[1] is a list of DLC strings
+
+ModsOutput = []
+def getModsOutput(unused, theStream):
+    ModsOutput.append('')  # ModsOutput[0] empty, keyword is not used
+    ModsOutput.append(getStringList(theStream))  # so ModsOutput[1] is a list of DLC strings
 
 def World(ImperatorSaveFileName):
     print("*** Hello Imperator, loading ImperatorWorld. ***")
@@ -19,11 +24,13 @@ def World(ImperatorSaveFileName):
     # PyParser.registerKeyword(o'versin', fun1())
 
     registerKeyword('date', getDateText)
-    registerKeyword('enabled_dlcs', getDLCOutput)
+    registerKeyword('enabled_dlcs', getDLCsOutput)
+    registerKeyword('enabled_mods', getModsOutput)
 
     print("Importing Imperator save.")
 
     parseFile(ImperatorSaveFileName)
 
     print(DateTextOutput[1])  # debug
-    print(DLCOutput[1])  # debug
+    print(DLCsOutput[1])  # debug
+    print(ModsOutput[1])  # debug
--- Source/Main.py
+++ Source/Main.py
@@ -3,4 +3,3 @@ import ImperatortoCK3Converter
 ImperatorSaveFileName = input("Insert save filename: ")
 
 ImperatortoCK3Converter.ConvertImperatorToCK3(ImperatorSaveFileName)
-
