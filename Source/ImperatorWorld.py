d7671b2408038e26db2c4c4c295815fa96eb0dd7
IlikeTrains <sewerynpresnal@gmail.com>
2020-03-31 20:02:30 +0200

    Reformatting ImperatorWorld.py, renaming things for less confusion

--- Source/ImperatorWorld.py
+++ Source/ImperatorWorld.py
@@ -1,44 +1,32 @@
 from Parser.PyParser import registerKeyword, parseFile
 from Parser.ParserHelpers import getSingleString, getStringList
 
-unused = ''
-
 DateTextOutput = []
-
-
-def getDateText(unused, theStream):
-    DateTextOutput.append('')  # DateTextOutput[0] empty, keyword is not used
-    DateTextOutput.append(getSingleString(theStream))  # so DateTextOutput[1] is dateString
-
-
 DLCsOutput = []
-
-
-def getDLCsOutput(unused, theStream):
-    DLCsOutput.append('')  # DLCsOutput[0] empty, keyword is not used
-    DLCsOutput.append(getStringList(theStream))  # so DLCsOutput[1] is a list of DLC strings
-
-
 ModsOutput = []
 
 
-def getModsOutput(unused, theStream):
-    ModsOutput.append('')  # ModsOutput[0] empty, keyword is not used
-    ModsOutput.append(getStringList(theStream))  # so ModsOutput[1] is a list of DLC strings
+def getDateStringInList(unused, theStream):
+    DateTextOutput.append(getSingleString(theStream))  # DateTextOutput[0] is a date in a string
+
+
+def getDLCsListInList(unused, theStream):
+    DLCsOutput.append(getStringList(theStream))  # DLCsOutput[0] is a list of DLC strings
+
+
+def getModsListInList(unused, theStream):
+    ModsOutput.append(getStringList(theStream))  # ModsOutput[0] is a list of mod strings
 
 
 def World(ImperatorSaveFileName):
     print("*** Hello Imperator, loading ImperatorWorld. ***")
-    # PyParser.registerKeyword('save_game_version', fun1())
-
-    registerKeyword('date', getDateText)
-    registerKeyword('enabled_dlcs', getDLCsOutput)
-    registerKeyword('enabled_mods', getModsOutput)
+    registerKeyword('date', getDateStringInList)
+    registerKeyword('enabled_dlcs', getDLCsListInList)
+    registerKeyword('enabled_mods', getModsListInList)
 
     print("Importing Imperator save.")
-
     parseFile(ImperatorSaveFileName)
 
-    print('Date is ' + DateTextOutput[1])  # debug
-    print('Activated DLCs: ' + DLCsOutput[1])  # debug
-    print('Activated mods: ' + ModsOutput[1])  # debug
+    print('Date is ' + DateTextOutput[0])  # debug
+    print('Activated DLCs:', DLCsOutput[0])  # debug
+    print('Activated mods:', ModsOutput[0])  # debug
