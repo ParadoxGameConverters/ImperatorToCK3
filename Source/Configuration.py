3f34904965daf3eb3e725184820350095b490fda
IlikeTrains <sewerynpresnal@gmail.com>
2020-04-01 11:06:35 +0200

    Disabled the CK3 parts of configuration.py to stop the tests' bitching

--- Source/Configuration.py
+++ Source/Configuration.py
@@ -35,31 +35,31 @@ def instantiate(unused, theStream):
 
     registerKeyword('ImperatorDocumentsDirectory', getImperatorDocumentsDirectory)
 
-    def verifyCK3Path(path):
-        if not os.path.isdir(path):
-            sys.exit(path + ' does not exist!')
-        print('\tCK3 install path is ' + path)
+    # def verifyCK3Path(path):
+    #     if not os.path.isdir(path):
+    #         sys.exit(path + ' does not exist!')
+    #     print('\tCK3 install path is ' + path)
 
-    CK3DirectoryOutput = []
+    # CK3DirectoryOutput = []
 
-    def getCK3Directory(unused, theStream):
-        CK3DirectoryOutput.append('')
-        CK3DirectoryOutput.append(getSingleString(theStream))
-        verifyCK3Path(CK3DirectoryOutput[1])
+    # def getCK3Directory(unused, theStream):
+    #     CK3DirectoryOutput.append('')
+    #    CK3DirectoryOutput.append(getSingleString(theStream))
+    #     verifyCK3Path(CK3DirectoryOutput[1])
 
     # registerKeyword('CK3Directory', getCK3Directory)  # TODO #5: enable when CK3 is released
 
-    def verifyCK3DocumentsPath(path):
-        if not os.path.isdir(path):
-            sys.exit(path + ' does not exist!')
-        print('\tCK3 documents directory path is ' + path)
+    # def verifyCK3DocumentsPath(path):
+    #     if not os.path.isdir(path):
+    #         sys.exit(path + ' does not exist!')
+    #     print('\tCK3 documents directory path is ' + path)
 
-    CK3DocumentsDirectoryOutput = []
+    # CK3DocumentsDirectoryOutput = []
 
-    def getCK3DocumentsDirectory(unused, theStream):
-        CK3DocumentsDirectoryOutput.append('')
-        CK3DocumentsDirectoryOutput.append(getSingleString(theStream))
-        verifyCK3DocumentsPath(CK3DocumentsDirectoryOutput[1])
+    # def getCK3DocumentsDirectory(unused, theStream):
+    #     CK3DocumentsDirectoryOutput.append('')
+    #     CK3DocumentsDirectoryOutput.append(getSingleString(theStream))
+    #     verifyCK3DocumentsPath(CK3DocumentsDirectoryOutput[1])
 
     # registerKeyword('CK3DocumentsDirectory', getCK3DocumentsDirectory)  # TODO #5: enable when CK3 is released
 
