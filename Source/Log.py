2b3f45c6b9e489e2a829824543f0689356e46930
IlikeTrains <sewerynpresnal@gmail.com>
2020-04-04 00:57:03 +0200

    Log module

---
+++ Source/Log.py
@@ -0,0 +1,26 @@
+import datetime
+
+logFileCreated = False
+logFile = None
+levelDict = {'error': '[ERROR]', 'warning': '[WARNING]', 'info': '[INFO]', 'debug': '[DEBUG]'}
+
+
+def initiate():
+    global logFile, logFileCreated
+    logFile = open("log.txt", "w")
+    logFileCreated = True
+
+
+def WriteToConsole(message):
+    print(message)
+
+
+def WriteToFile(level, message):
+    global logFile
+    dateTime = datetime.datetime.now().strftime("%Y-%m-%d %X")
+    logFile.write(dateTime + '  ' + levelDict.get(level) + '\t' + message + '\n')
+
+
+def Log(level, message):
+    WriteToConsole(message)
+    WriteToFile(level, message)
--- Source/Main.py
+++ Source/Main.py
@@ -1,7 +1,9 @@
 import ImperatortoCK3Converter
 import Configuration
+import Log
 
 Configuration.readConfigurationFile("configuration.txt")
+Log.initiate()
 
 ImperatorSaveFileName = Configuration.getSavePath()
 if ImperatorSaveFileName is not None:
