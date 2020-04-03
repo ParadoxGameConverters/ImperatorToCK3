84f68df246d9829bc053c8fa596c3f5e45aa4788
IlikeTrains <sewerynpresnal@gmail.com>
2020-04-04 01:17:33 +0200

    Close the log file in the end

--- Source/Main.py
+++ Source/Main.py
@@ -9,3 +9,5 @@ Configuration.readConfigurationFile("configuration.txt")
 ImperatorSaveFileName = Configuration.getSavePath()
 if ImperatorSaveFileName is not None:
     ImperatortoCK3Converter.ConvertImperatorToCK3(ImperatorSaveFileName)
+
+Log.endLog()
