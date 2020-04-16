86a61b908cc6d653fdc4871afa38f728e0eda727
IlikeTrains <sewerynpresnal@gmail.com>
2020-04-16 02:16:43 +0200

    Simplified the VersionTests

--- ImperatorToCk3Tests/CommonTests/VersionTests.cpp
+++ ImperatorToCk3Tests/CommonTests/VersionTests.cpp
@@ -4,44 +4,44 @@
 
 TEST(ImperatorWorld_VersionTests, VersionHigherOrEqual)
 {
-	const Version version1 = Version("1.4.1");
-	const Version version2 = Version("1.3.2");
-	const Version requiredVersion = Version("1.3.2.0");
+	const Version version1("1.4.1");
+	const Version version2("1.3.2");
+	const Version requiredVersion("1.3.2.0");
 	ASSERT_TRUE(version1 >= requiredVersion);
 	ASSERT_TRUE(version2 >= requiredVersion);
 }
 TEST(ImperatorWorld_VersionTests, VersionHigher)
 {
-	const Version version = Version("1.4.1");
-	const Version requiredVersion = Version("1.3.2");
+	const Version version("1.4.1");
+	const Version requiredVersion("1.3.2");
 	ASSERT_TRUE(version > requiredVersion);
 }
 
 TEST(ImperatorWorld_VersionTests, VersionLowerOrEqual)
 {
-	const Version version1 = Version("1.2.1");
-	const Version version2 = Version("1.3.2");
-	const Version requiredVersion = Version("1.3.2.0");
+	const Version version1("1.2.1");
+	const Version version2("1.3.2");
+	const Version requiredVersion("1.3.2.0");
 	ASSERT_TRUE(version1 <= requiredVersion);
 	ASSERT_TRUE(version2 <= requiredVersion);
 }
 TEST(ImperatorWorld_VersionTests, VersionLower)
 {
-	const Version version = Version("1.1");
-	const Version requiredVersion = Version("1.3.2");
+	const Version version("1.1");
+	const Version requiredVersion("1.3.2");
 	ASSERT_TRUE(version < requiredVersion);
 }
 TEST(ImperatorWorld_VersionTests, VersionEqual)
 {
-	const Version version = Version("1.3.3.0");
-	const Version requiredVersion = Version("1.3.3");
+	const Version version("1.3.3.0");
+	const Version requiredVersion("1.3.3");
 	ASSERT_EQ(version, requiredVersion);
 	ASSERT_TRUE(version == requiredVersion);
 }
 
 TEST(ImperatorWorld_VersionTests, VersionNotEqual)
 {
-	const Version version = Version("1.3.3.3"); 
-	const Version requiredVersion = Version("1.3.3");
+	const Version version("1.3.3.3"); 
+	const Version requiredVersion("1.3.3");
 	ASSERT_TRUE(version != requiredVersion);
 }
