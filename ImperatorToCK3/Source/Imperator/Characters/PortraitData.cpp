dcb4730d0c2c3896efb7fea1a694627a40e20d8a
IlikeTrains <sewerynpresnal@gmail.com>
2020-08-25 17:20:53 +0200

    Fixed an arithmetic overflow warning

--- ImperatorToCK3/Source/Imperator/Characters/PortraitData.cpp
+++ ImperatorToCK3/Source/Imperator/Characters/PortraitData.cpp
@@ -18,7 +18,7 @@ long long binaryToDecimal(long long n)
 		int last_digit = int(temp % 10);
 		temp = temp / 10;
 
-		dec_value += long long(last_digit * base);
+		dec_value += last_digit * long long(base);
 
 		base = base * 2;
 	}
