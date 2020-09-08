6033671f268ad45fd2d9ca67372ca3ad4697e8d6
IlikeTrains <sewerynpresnal@gmail.com>
2020-09-08 11:51:22 +0200

    buildingsCount tests and ProvinceMappings desc

--- ImperatorToCK3/Source/CK3/Province/CK3ProvinceMappings.h
+++ ImperatorToCK3/Source/CK3/Province/CK3ProvinceMappings.h
@@ -7,6 +7,28 @@ namespace CK3
 {
 	class ProvinceMappings : commonItems::parser
 	{
+		/*
+		
+		This class is used to read game/history/province_mapping in CK3.
+		CK3 uses province_mapping to set history for provinces that don't need unique entries.
+
+
+		Example unique entry in game/history/provinces:
+		
+		6872 = {
+			religion = coptic
+			culture = coptic
+		}
+
+
+		Example province_mapping in game/history/province_mapping:
+
+		6874 = 6872
+
+
+		Now 6874 history is same as 6872 history.
+		
+		*/
 	  public:
 		ProvinceMappings() = default;
 		explicit ProvinceMappings(const std::string& theFile);
