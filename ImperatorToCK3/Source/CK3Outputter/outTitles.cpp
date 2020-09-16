a6f54a0664e92313d81579eee0ab06f74a3fd415
IlikeTrains <sewerynpresnal@gmail.com>
2020-09-17 00:35:11 +0200

    Output titles to separate files

--- ImperatorToCK3/Source/CK3Outputter/outTitles.cpp
+++ ImperatorToCK3/Source/CK3Outputter/outTitles.cpp
@@ -6,16 +6,15 @@
 void CK3::outputTitles(const std::string& outputModName, const std::map<std::string, std::shared_ptr<Title>>& titles)
 {
 	unsigned char bom[] = { 0xEF,0xBB,0xBF };
-	
-	std::ofstream output("output/" + outputModName + "/common/landed_titles/ImpToCK3_landed_titles.txt"); // dumping all into one file
-	if (!output.is_open())
-		throw std::runtime_error(
-			"Could not create landed titles file: output/" + outputModName + "/common/landed_titles/ImpToCK3_landed_titles.txt");
-	output.write(reinterpret_cast<char*>(bom), sizeof(bom)); // to make the file utf8-bom
-	output << "# number of title: " << titles.size() << "\n";
+
 	for (const auto& [first, second] : titles)
 	{
+		std::ofstream output("output/" + outputModName + "/common/landed_titles/" + first + ".txt"); // dumping all into one file
+		if (!output.is_open())
+			throw std::runtime_error(
+				"Could not create landed titles file: output/" + outputModName + "/common/landed_titles/" + first + ".txt");
+		output.write(reinterpret_cast<char*>(bom), sizeof(bom)); // to make the file utf8-bom
 		output << *second;
+		output.close();
 	}
-	output.close();
 }
