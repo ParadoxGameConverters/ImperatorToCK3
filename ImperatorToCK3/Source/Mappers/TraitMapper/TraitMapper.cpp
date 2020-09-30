400baa52a832ecc9765065f43b9eaefa6d1ce395
IlikeTrains <sewerynpresnal@gmail.com>
2020-09-30 22:07:32 +0200

    TraitMapper

---
+++ ImperatorToCK3/Source/Mappers/TraitMapper/TraitMapper.cpp
@@ -0,0 +1,40 @@
+#include "TraitMapper.h"
+#include "Log.h"
+#include "ParserHelpers.h"
+#include "TraitMapping.h"
+
+mappers::TraitMapper::TraitMapper()
+{
+	LOG(LogLevel::Info) << "-> Parsing trait mappings.";
+	registerKeys();
+	parseFile("configurables/trait_map.txt");
+	clearRegisteredKeywords();
+	LOG(LogLevel::Info) << "<> Loaded " << impToCK3TraitMap.size() << " trait links.";
+}
+
+mappers::TraitMapper::TraitMapper(std::istream& theStream)
+{
+	registerKeys();
+	parseStream(theStream);
+	clearRegisteredKeywords();
+}
+
+void mappers::TraitMapper::registerKeys()
+{
+	registerKeyword("link", [this](const std::string& unused, std::istream& theStream) {
+		const TraitMapping theMapping(theStream);
+		for (const auto& imperatorTrait: theMapping.impTraits)
+		{
+			impToCK3TraitMap.insert(std::make_pair(imperatorTrait, theMapping.ck3Trait));
+		}
+	});
+	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
+}
+
+std::optional<std::string> mappers::TraitMapper::getCK3TraitForImperatorTrait(const std::string& impTrait) const
+{
+	const auto& mapping = impToCK3TraitMap.find(impTrait);
+	if (mapping != impToCK3TraitMap.end())
+		return mapping->second;
+	return std::nullopt;
+}
