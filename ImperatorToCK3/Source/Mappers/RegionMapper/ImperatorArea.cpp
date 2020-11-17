e11172d6aca32dcc1d6da9be95b44c2d0336a406
IlikeTrains <sewerynpresnal@gmail.com>
2020-11-17 18:29:56 +0100

    ImperatorArea

---
+++ ImperatorToCK3/Source/Mappers/RegionMapper/ImperatorArea.cpp
@@ -0,0 +1,24 @@
+#include "ImperatorArea.h"
+#include "ParserHelpers.h"
+#include "Log.h"
+
+mappers::ImperatorArea::ImperatorArea(std::istream& theStream)
+{
+	registerKeys();
+	parseStream(theStream);
+	clearRegisteredKeywords();
+}
+
+void mappers::ImperatorArea::registerKeys()
+{
+	registerKeyword("provinces", [this](const std::string& unused, std::istream& theStream) {
+		for (const auto& id : commonItems::ullongList{theStream}.getULlongs())
+			provinces.insert(id);
+	});
+	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
+}
+
+bool mappers::ImperatorArea::areaContainsProvince(const unsigned long long province) const
+{
+	return provinces.contains(province);
+}
