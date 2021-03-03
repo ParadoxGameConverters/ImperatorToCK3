d9fa2520b78ed7d0746398c9726986be24253af9
IlikeTrains <sewerynpresnal@gmail.com>
2021-03-03 19:10:11 +0100

    Added some FamilyTests

--- ImperatorToCK3Tests/ImperatorWorldTests/Families/FamilyTests.cpp
+++ ImperatorToCK3Tests/ImperatorWorldTests/Families/FamilyTests.cpp
@@ -1,7 +1,11 @@
 #include "gtest/gtest.h"
-#include "../ImperatorToCK3/Source/Imperator/Families/Family.h"
-#include "../ImperatorToCK3/Source/Imperator/Families/FamilyFactory.h"
+#include "Imperator/Families/Family.h"
+#include "Imperator/Families/FamilyFactory.h"
+#include "Imperator/Characters/CharacterFactory.h"
+#include "Imperator/Genes/GenesDB.h"
 #include <sstream>
+
+
 
 TEST(ImperatorWorld_FamilyTests, IDCanBeSet)
 {
@@ -140,4 +144,70 @@ TEST(ImperatorWorld_FamilyTests, keyDefaultsToBlank)
 	const auto theFamily = *Imperator::Family::Factory().getFamily(input, 42);
 
 	ASSERT_TRUE(theFamily.getKey().empty());
+}
+
+TEST(ImperatorWorld_FamilyTests, membersDefaultToEmpty) {
+	std::stringstream input;
+	input << "= {}";
+
+	const auto family = *Imperator::Family::Factory().getFamily(input, 42);
+
+	ASSERT_TRUE(family.getMembers().empty());
+}
+
+TEST(ImperatorWorld_FamilyTests, linkingNullptrMemberIsLogged) {
+	std::stringstream input;
+	input << "= {}";
+
+	auto family = *Imperator::Family::Factory().getFamily(input, 42);
+
+	std::stringstream log;
+	auto* stdOutBuf = std::cout.rdbuf();
+	std::cout.rdbuf(log.rdbuf());
+
+	family.linkMember(nullptr);
+
+	std::cout.rdbuf(stdOutBuf);
+	auto stringLog = log.str();
+	auto newLine = stringLog.find_first_of('\n');
+	stringLog = stringLog.substr(0, newLine);
+
+	ASSERT_EQ(" [WARNING] Family 42: cannot link nullptr member!", stringLog);
+}
+
+TEST(ImperatorWorld_FamilyTests, cannotLinkMemberWithoutPreexistingMatchingID) {
+	std::stringstream input;
+	input << "= { member={40 50 5} }";
+	auto family = *Imperator::Family::Factory().getFamily(input, 42);
+
+	std::stringstream charInput;
+	std::shared_ptr<Imperator::Character> character = Imperator::Character::Factory().getCharacter(charInput, "6", nullptr);
+
+	std::stringstream log;
+	auto* stdOutBuf = std::cout.rdbuf();
+	std::cout.rdbuf(log.rdbuf());
+
+	family.linkMember(character);
+
+	std::cout.rdbuf(stdOutBuf);
+	auto stringLog = log.str();
+	auto newLine = stringLog.find_first_of('\n');
+	stringLog = stringLog.substr(0, newLine);
+
+	ASSERT_EQ(" [WARNING] Family 42: cannot link 6: not found in members!", stringLog);
+}
+
+TEST(ImperatorWorld_FamilyTests, memberLinkingWorks) {
+	std::stringstream input;
+	input << "= { member={40 50 5} }";
+	auto family = *Imperator::Family::Factory().getFamily(input, 42);
+
+	std::stringstream charInput;
+	charInput << "= { culture = kushite }";
+	auto genesDB = std::make_shared<Imperator::GenesDB>();
+	std::shared_ptr<Imperator::Character> character = Imperator::Character::Factory().getCharacter(charInput, "50", genesDB);
+
+	family.linkMember(character);
+	
+	ASSERT_EQ("kushite", family.getMembers()[1].second->getCulture());
 }
