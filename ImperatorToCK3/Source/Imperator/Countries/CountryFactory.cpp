53d5d2bfc6a58c2bfb482e1ec2734b5fedb2d38e
IlikeTrains <sewerynpresnal@gmail.com>
2021-03-07 18:20:33 +0100

    lawRegexStr

--- ImperatorToCK3/Source/Imperator/Countries/CountryFactory.cpp
+++ ImperatorToCK3/Source/Imperator/Countries/CountryFactory.cpp
@@ -7,6 +7,14 @@
 
 
 
+constexpr const char* lawRegexStr = "succession_law|monarchy_military_reforms|monarchy_maritime_laws|monarchy_economic_law|monarchy_citizen_law|monarchy_religious_laws|monarchy_legitimacy_laws|monarchy_contract_law"
+"|jewish_monarchy_divinity_statutes|monarchy_subject_laws"
+"|republic_military_recruitment_laws|republic_election_reforms|corruption_laws|republican_mediterranean_laws|republican_religious_laws|republic_integration_laws"
+"|republic_citizen_laws|republican_land_reforms|republic_military_recruitment_laws_rom|republic_election_reforms_rom|corruption_laws_rom"
+"|republican_mediterranean_laws_rom|republican_religious_laws_rom|republic_integration_laws_rom|republic_citizen_laws_rom|republican_land_reforms_rom"
+"|tribal_religious_law|tribal_currency_laws|tribal_centralization_law|tribal_authority_laws|tribal_autonomy_laws|tribal_domestic_laws|tribal_decentralized_laws|tribal_centralized_laws|tribal_super_decentralized_laws|tribal_super_centralized_laws";
+
+
 Imperator::Country::Factory::Factory() {
 	registerKeyword("tag", [this](std::istream& theStream){
 		country->tag = commonItems::getString(theStream);
