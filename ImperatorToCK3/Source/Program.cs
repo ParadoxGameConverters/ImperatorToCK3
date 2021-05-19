13a477e1974b11339fca4c6edacb13b16a0a030b
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-19 17:51:10 +0200

    Remove submodules for C++

---
+++ ImperatorToCK3/Source/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ImperatorToCK3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if(args.Length > 0)
+            {
+                Console.WriteLine("ImperatorToCK3 takes no parameters.");
+                Console.WriteLine("It uses configuration.txt, configured manually or by the frontend.");
+            }
+        }
+    }
+}
