a17064cc02f4f62dc0b7c46691f747de7b361b43
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-19 21:07:17 +0200

    Create BufferedStreamReader.cs

---
+++ ImperatorToCK3/Source/commonItems/BufferedStreamReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commonItems
+{
+    class BufferedStreamReader : StreamReader
+    {
+        public BufferedStreamReader(Stream strm) : base(strm) { }
+
+        private int lastChar = -1;
+        public override int Read()
+        {
+            int ch;
+
+            if (lastChar >= 0)
+            {
+                ch = lastChar;
+                lastChar = -1;
+            }
+            else
+            {
+                ch = base.Read();  // could be -1 
+            }
+            return ch;
+        }
+
+        public void PushBack(char ch)  // char, don't allow Pushback(-1)
+        {
+            if (lastChar >= 0)
+                throw new InvalidOperationException("PushBack of more than 1 char");
+
+            lastChar = ch;
+        }
+    }
+}
