0c726fdb925ce9002540de5c32c54665786ade33
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-20 16:57:50 +0200

    Create CommonRegexes.cs

---
+++ ImperatorToCK3/Source/commonItems/CommonRegexes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commonItems
+{
+    public class CommonRegexes
+    {
+        // catchall:
+        //		We grab everything that's NOT =, { or }, OR we grab everything within quotes, except newlines, which we already drop
+        //		in the parser.
+        public const string Catchall = @"[^=^{^}]+|"".+ """;
+
+        // numbers
+        public const string Integer = @"-?\d+";
+        public const string QuotedInteger = @""" -?\d+""";
+        public const string Float = @"-?\d+(.\d+)?";
+        public const string quotedFloat = @""" -?\d+(.\d+)?""";
+
+        // strings
+        public const string StringRegex = @"[^[:s:]^=^\{^\}^\""]+";
+        public const string QuotedString = @"""[^\n^=^\{^\}^\""]+""";
+    }
+}
