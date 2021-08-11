3ae8edc83cc33f3252e083b050e2856780f8a276
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-11 13:43:34 +0200

    DeathReasonMapper

---
+++ ImperatorToCK3/Mappers/DeathReasonMapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using commonItems;
+
+namespace ImperatorToCK3.Mappers {
+    public class DeathReasonMapper : Parser {
+        public DeathReasonMapper() {
+            Logger.Log(LogLevel.Info, "Parsing death reason mappings.");
+            RegisterKeys();
+            ParseFile("configurables/deathMappings.txt");
+            ClearRegisteredRules();
+            Logger.Log(LogLevel.Info, "Loaded " + impToCK3ReasonMap.Count + " death reason links.");
+        }
+        public DeathReasonMapper(BufferedReader reader) {
+            RegisterKeys();
+            ParseStream(reader);
+            ClearRegisteredRules();
+        }
+        public string? GetCK3ReasonForImperatorReason(string impReason) {
+            var gotValue = impToCK3ReasonMap.TryGetValue(impReason, out var value);
+            if (gotValue) {
+                return value;
+            }
+            return null;
+        }
+
+        private void RegisterKeys() {
+            RegisterKeyword("link", (reader) => {
+                var mapping = new DeathReasonMapping(reader);
+                if (mapping.Ck3Reason != null) {
+                    foreach (var impReason in mapping.ImpReasons) {
+                        impToCK3ReasonMap.Add(impReason, mapping.Ck3Reason);
+                    }
+                }
+            });
+        }
+        private Dictionary<string, string> impToCK3ReasonMap = new();
+    }
+}
