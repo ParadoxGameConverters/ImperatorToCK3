7f55744b121677b0c3387bb23916c53049ac8df3
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-12 20:26:31 +0200

    Create TraitMapping.cs

---
+++ ImperatorToCK3/Mappers/Trait/TraitMapping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using commonItems;
+
+namespace ImperatorToCK3.Mappers.Trait {
+    public class TraitMapping {
+        public SortedSet<string> ImpTraits { get; set; } = new();
+        public string? Ck3Trait { get; set; }
+
+        public TraitMapping(BufferedReader reader) {
+            var parser = new Parser();
+            parser.RegisterKeyword("ck3", (reader) => {
+                Ck3Trait = new SingleString(reader).String;
+            });
+            parser.RegisterKeyword("imp", (reader) => {
+                ImpTraits.Add(new SingleString(reader).String);
+            });
+            parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+            parser.ParseStream(reader);
+            parser.ClearRegisteredRules();
+        }
+    }
+}
