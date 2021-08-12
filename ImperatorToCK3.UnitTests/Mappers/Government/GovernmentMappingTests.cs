5c13cde22682d1c89e983a711f02dd9af279f8b6
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-12 16:55:16 +0200

    GovernmentMapperTests

--- ImperatorToCK3.UnitTests/Mappers/Government/GovernmentMappingTests.cs
+++ ImperatorToCK3.UnitTests/Mappers/Government/GovernmentMappingTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 using commonItems;
 using ImperatorToCK3.Mappers.Government;
 
@@ -16,13 +11,13 @@ namespace ImperatorToCK3.UnitTests.Mappers.Government {
             Assert.Empty(mapping.ImperatorGovernments);
         }
         [Fact]
-        public void ck3GovernmentCanBeSet() {
+        public void Ck3GovernmentCanBeSet() {
             var reader = new BufferedReader("= { ck3 = ck3Government }");
             var mapping = new GovernmentMapping(reader);
             Assert.Equal("ck3Government", mapping.Ck3Government);
         }
         [Fact]
-        public void imperatorGovernmentsCanBeSet() {
+        public void ImperatorGovernmentsCanBeSet() {
             var reader = new BufferedReader("= { imp = gov1 imp = gov2 }");
             var mapping = new GovernmentMapping(reader);
             Assert.Collection(mapping.ImperatorGovernments,
