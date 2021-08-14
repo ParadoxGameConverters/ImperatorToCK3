538f11a2cfd70734dbfc02c0af5b3c17ea2cba69
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-08-14 09:38:36 +0200

    GovernmentMapper with tests (#261)
    * Create GovernmentMapping.cs
    
    * Create GovernmentMapper.cs
    
    * GovernmentMappingTests
    
    * GovernmentMapperTests
    
    * Update GovernmentMapper.cs

---
+++ ImperatorToCK3/Mappers/Government/GovernmentMapping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using commonItems;
+
+namespace ImperatorToCK3.Mappers.Government {
+    public class GovernmentMapping {
+        public SortedSet<string> ImperatorGovernments = new();
+        public string Ck3Government = "";
+        public GovernmentMapping(BufferedReader reader) {
+            var parser = new Parser();
+            parser.RegisterKeyword("ck3", (reader) => {
+                Ck3Government = new SingleString(reader).String;
+            });
+            parser.RegisterKeyword("imp", (reader) => {
+                ImperatorGovernments.Add(new SingleString(reader).String);
+            });
+            parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+
+            parser.ParseStream(reader);
+            parser.ClearRegisteredRules();
+        }
+    }
+}
