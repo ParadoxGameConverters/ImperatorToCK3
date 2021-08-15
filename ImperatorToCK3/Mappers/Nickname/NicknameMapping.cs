ad03c2ee9f195c930c2026affc1a0f4a24a2b090
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-15 18:29:18 +0200

    NicknameMappingTests

--- ImperatorToCK3/Mappers/Nickname/NicknameMapping.cs
+++ ImperatorToCK3/Mappers/Nickname/NicknameMapping.cs
@@ -4,14 +4,14 @@ using commonItems;
 namespace ImperatorToCK3.Mappers.Nickname {
 	public class NicknameMapping {
 		public SortedSet<string> ImperatorNicknames { get; private set; } = new();
-		public string? Ck3Nickname { get; private set; } = "";
+		public string? Ck3Nickname { get; private set; }
 		
 		public NicknameMapping(BufferedReader reader) {
 			var parser = new Parser();
 			parser.RegisterKeyword("ck3", (reader) => {
 				Ck3Nickname = new SingleString(reader).String;
 			});
-			parser.RegisterKeyword("ck3", (reader) => {
+			parser.RegisterKeyword("imp", (reader) => {
 				ImperatorNicknames.Add(new SingleString(reader).String);
 			});
 			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
