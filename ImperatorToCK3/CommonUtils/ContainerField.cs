413f4cd33ae16226d9c563290ceb85d88f424155
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-16 20:45:17 +0200

    SimpleField

--- ImperatorToCK3/CommonUtils/ContainerField.cs
+++ ImperatorToCK3/CommonUtils/ContainerField.cs
@@ -6,23 +6,31 @@ using System.Threading.Tasks;
 using commonItems;
 
 namespace ImperatorToCK3.CommonUtils {
-    public struct ContainerFieldStruct {
-        string fieldName;
-        string setter;
-        List<string> initialValue;
-    }
+	public struct ContainerFieldStruct {
+		string fieldName;
+		string setter;
+		List<string> initialValue;
+	}
 
-    class DescendingComparer<T> : IComparer<T> where T : IComparable<T> {
-        public int Compare(T x, T y) {
-            return y.CompareTo(x);
-        }
-    }
-    public class ContainerField {
-        public ContainerField(List<string> initialValue) {
-        }
-        List<string> GetValue(Date date) {
-
-        }
-        public SortedDictionary<Date, List<string>> ValueHistory { get; set; } = new(new DescendingComparer<Date>());
-    }
+	class DescendingComparer<T> : IComparer<T> where T : IComparable<T> {
+		public int Compare(T x, T y) {
+			return y.CompareTo(x);
+		}
+	}
+	public class ContainerField {
+		public ContainerField(List<string> initialValue) {
+			InitialValue = initialValue;
+		}
+		public List<string> GetValue(Date date) {
+			try {
+				var pairWithLastEarlierOrSameDate = ValueHistory.TakeWhile(d => d.Key <= date).Last();
+				return pairWithLastEarlierOrSameDate.Value;
+			}
+			catch (Exception) {
+				return InitialValue;
+			}
+		}
+		public SortedDictionary<Date, List<string>> ValueHistory { get; set; } = new();
+		public List<string> InitialValue { private get; set; } = new();
+	}
 }
---
+++ ImperatorToCK3/CommonUtils/SimpleField.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using commonItems;
+
+namespace ImperatorToCK3.CommonUtils {
+	public struct SimpleFieldStruct {
+		string fieldName;
+		string setter;
+		string? initialValue;
+	}
+	public class SimpleField {
+		public SortedDictionary<Date, string> ValueHistory { get; private set; } = new();
+		public string? InitialValue { private get; set; }
+
+		public SimpleField(string? initialValue) {
+			InitialValue = initialValue;
+		}
+		public string? GetValue(Date date) {
+			try {
+				var pairWithLastEarlierOrSameDate = ValueHistory.TakeWhile(d => d.Key <= date).Last();
+				return pairWithLastEarlierOrSameDate.Value;
+			} catch (Exception) {
+				return InitialValue;
+			}
+		}
+		public void AddValueToHistory(string value, Date date) {
+			ValueHistory[date] = value;
+		}
+	}
+}
