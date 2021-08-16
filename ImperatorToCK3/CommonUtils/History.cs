dc63aec75f5f2038cf3bc0a1cd0631118949c7d6
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-16 20:58:49 +0200

    History

---
+++ ImperatorToCK3/CommonUtils/History.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using commonItems;
+
+namespace ImperatorToCK3.CommonUtils {
+	public class History {
+		private Dictionary<string, SimpleField> simpleFields; // fieldName, field
+		private Dictionary<string, ContainerField> containerFields; // fieldName, field
+
+		public History(Dictionary<string, SimpleField> simpleFields, Dictionary<string, ContainerField> containerFields) {
+			this.simpleFields = simpleFields;
+			this.containerFields = containerFields;
+		}
+		public string? GetSimpleFieldValue(string fieldName, Date date) {
+			if (simpleFields.TryGetValue(fieldName, out var value)) {
+				return value.GetValue(date);
+			}
+			return null;
+		}
+		public List<string>? GetContainerFieldValue(string fieldName, Date date) {
+			if (containerFields.TryGetValue(fieldName, out var value)) {
+				return value.GetValue(date);
+			}
+			return null;
+		}
+	}
+}
