cfdd95663f2e8981e2062d27e258e96c0258e2e5
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-08-16 02:39:10 +0200

    LogConverterVersion, correct output path for ImperatorToCK3 (#267)
    * Update commonItems.NET
    
    * LogConverterVersion
    
    * Set correct OutputPath for ImperatorToCK3

---
+++ ImperatorToCK3/Outputter/OutVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using commonItems;
+
+namespace ImperatorToCK3.Outputter {
+	public static class VersionOutputter {
+		public static void LogConverterVersion(ConverterVersion versionParser) {
+			var compileTime = new DateTime(Builtin.CompileTime, DateTimeKind.Utc);
+			// read commit id
+			string commitID = File.ReadAllText("../commit_id.txt", Encoding.UTF8).Trim();
+			Logger.Log(LogLevel.Info, "************ -= The Paradox Converters Team =- ********************");
+			Logger.Log(LogLevel.Info, "* Converter build based on commit " + commitID);
+			Logger.Log(LogLevel.Info, "* " + versionParser.GetDescription());
+			Logger.Log(LogLevel.Info, "* Built on " + compileTime.ToShortDateString() + " " + compileTime.ToLongTimeString());
+			Logger.Log(LogLevel.Info, "*********** + Imperator: Rome To Crusader Kings III + *************\n");
+		}
+	}
+}
