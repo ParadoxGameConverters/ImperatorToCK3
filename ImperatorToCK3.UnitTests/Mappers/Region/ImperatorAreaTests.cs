00f94d31da2245db34a755ebe2d47205315a41c6
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-18 23:27:05 +0200

    ImperatorRegionTests

---
+++ ImperatorToCK3.UnitTests/Mappers/Region/ImperatorAreaTests.cs
@@ -0,0 +1,37 @@
+﻿using commonItems;
+using ImperatorToCK3.Mappers.Region;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.Mappers.Region {
+	public class ImperatorAreaTests {
+		[Fact]
+		public void BlankAreaLoadsWithNoProvinces() {
+			var reader = new BufferedReader("");
+			var area = new ImperatorArea(reader);
+
+			Assert.Empty(area.Provinces);
+		}
+
+		[Fact]
+		public void ProvinceCanBeLoaded() {
+			var reader = new BufferedReader("provinces = { 69 } \n");
+			var area = new ImperatorArea(reader);
+
+			Assert.Collection(area.Provinces,
+				item => Assert.Equal((ulong)69, item)
+			);
+		}
+
+		[Fact]
+		public void MultipleProvincesCanBeLoaded() {
+			var reader = new BufferedReader("provinces = { 2 69 3 } \n");
+			var area = new ImperatorArea(reader);
+
+			Assert.Collection(area.Provinces,
+				item => Assert.Equal((ulong)2, item),
+				item => Assert.Equal((ulong)3, item),
+				item => Assert.Equal((ulong)69, item)
+			);
+		}
+	}
+}
---
+++ ImperatorToCK3.UnitTests/Mappers/Region/ImperatorRegionTests.cs
@@ -0,0 +1,76 @@
+﻿using commonItems;
+using ImperatorToCK3.Mappers.Region;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.Mappers.Region {
+	public class ImperatorRegionTests {
+		[Fact]
+		public void blankRegionLoadsWithNoAreas() {
+			var reader = new BufferedReader(string.Empty);
+			var region = new ImperatorRegion(reader);
+
+			Assert.Empty(region.Areas);
+		}
+
+		[Fact]
+		public void areaCanBeLoaded() {
+			var reader = new BufferedReader("areas = { testarea } \n");
+			var region = new ImperatorRegion(reader);
+
+			Assert.Collection(region.Areas,
+				item => Assert.Equal("testarea", item.Key)
+			);
+		}
+
+		[Fact]
+		public void multipleAreasCanBeLoaded() {
+			var reader = new BufferedReader("areas = { test1 test2 test3 } \n");
+			var region = new ImperatorRegion(reader);
+
+			Assert.Collection(region.Areas,
+				item => Assert.Equal("test1", item.Key),
+				item => Assert.Equal("test2", item.Key),
+				item => Assert.Equal("test3", item.Key)
+			);
+		}
+
+		[Fact]
+		public void regionCanBeLinkedToArea() {
+			var reader1 = new BufferedReader("areas = { test1 test2 test3 } \n");
+			var region = new ImperatorRegion(reader1);
+
+			var reader2 = new BufferedReader("{ provinces  = { 3 6 2 }} \n");
+			var area = new ImperatorArea(reader2);
+
+			Assert.Null(region.Areas["test2"]); // null before linking
+			region.LinkArea("test2", area);
+			Assert.NotNull(region.Areas["test2"]);
+		}
+
+		[Fact]
+		public void linkedRegionCanLocateProvince() {
+			var reader1 = new BufferedReader(string.Empty);
+			var region = new ImperatorRegion(reader1);
+
+			var reader2 = new BufferedReader("{ provinces  = { 3 6 2 }} \n");
+			var area = new ImperatorArea(reader2);
+
+			region.LinkArea("test2", area);
+
+			Assert.True(region.ContainsProvince(6));
+		}
+
+		[Fact]
+		public void linkedRegionWillFailForProvinceMismatch() {
+			var reader1 = new BufferedReader(string.Empty);
+			var region = new ImperatorRegion(reader1);
+
+			var reader2 = new BufferedReader("{ provinces  = { 3 6 2 }} \n");
+			var area = new ImperatorArea(reader2);
+
+			region.LinkArea("test2", area);
+
+			Assert.False(region.ContainsProvince(7));
+		}
+	}
+}
