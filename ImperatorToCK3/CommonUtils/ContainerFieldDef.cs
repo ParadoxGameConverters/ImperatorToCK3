eeaa8012ce04b8c9dc2922994c21b54913c8a3a8
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-20 13:31:48 +0200

    PR tweaks

--- ImperatorToCK3/CommonUtils/ContainerFieldDef.cs
+++ ImperatorToCK3/CommonUtils/ContainerFieldDef.cs
@@ -4,6 +4,6 @@ namespace ImperatorToCK3.CommonUtils {
 	public class ContainerFieldDef {
 		public string FieldName { get; set; } = "";
 		public string Setter { get; set; } = "";
-		public List<string> InitialValue { get; set; }
+		public List<string> InitialValue { get; set; } = new();
 	}
 }
