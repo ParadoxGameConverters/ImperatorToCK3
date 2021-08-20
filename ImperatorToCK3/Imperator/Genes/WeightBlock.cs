9adcfba5aef91a59bf5b3f031d3bc7f23ce57684
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-08-20 07:54:16 +0200

    Accessory genes (#269)
    * Create WeightBlock.cs
    
    * AccessoryGeneTemplate
    
    * AccessoryGene
    
    * AccessoryGenes
    
    * GenesDB
    
    * WeighBlockTests
    
    * AccessoryGeneTemplateTests
    
    * AccessoryGeneTests
    
    * AccessoryGenesTests
    
    * Create GenesDBTests.cs
    
    * Update WeightBlock.cs
    
    * Format AccessoryGene
    
    * Satisfy CodeFactor

---
+++ ImperatorToCK3/Imperator/Genes/WeightBlock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using commonItems;
+
+namespace ImperatorToCK3.Imperator.Genes {
+	public class WeightBlock : Parser {
+		public uint SumOfAbsoluteWeights { get; private set; } = 0;
+		private readonly List<KeyValuePair<string, uint>> objectsList = new();
+
+		public WeightBlock() { }
+		public WeightBlock(BufferedReader reader) {
+			RegisterKeys();
+			ParseStream(reader);
+			ClearRegisteredRules();
+		}
+		private void RegisterKeys() {
+			RegisterRegex(CommonRegexes.Integer, (reader, absoluteWeightStr) => {
+				var newObjectName = new SingleString(reader).String;
+				if (uint.TryParse(absoluteWeightStr, out var weight)) {
+					AddObject(newObjectName, weight);
+				} else {
+					Logger.Log(LogLevel.Error, "Could not parse absolute weight: " + absoluteWeightStr);
+				}
+			});
+			RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+		}
+		public uint GetAbsoluteWeight(string objectName) {
+			foreach (var (key, value) in objectsList) {
+				if (key == objectName) {
+					return value;
+				}
+			}
+			return 0;
+		}
+		public string? GetMatchingObject(double percentAsDecimal) { // argument must be in range <0; 1>
+			if (percentAsDecimal < 0 || percentAsDecimal > 1) {
+				throw new ArgumentOutOfRangeException("percentAsDecimal is " + percentAsDecimal + ", should be in range <0;1>");
+			}
+			uint sumOfPrecedingAbsoluteWeights = 0;
+			foreach (var (key, value) in objectsList) {
+				sumOfPrecedingAbsoluteWeights += value;
+				if (sumOfPrecedingAbsoluteWeights > 0 && percentAsDecimal <= (double)sumOfPrecedingAbsoluteWeights / SumOfAbsoluteWeights) {
+					return key;
+				}
+			}
+			return null;
+		}
+		public void AddObject(string objectName, uint absoluteWeight) {
+			objectsList.Add(new KeyValuePair<string, uint>(objectName, absoluteWeight));
+			SumOfAbsoluteWeights += absoluteWeight;
+		}
+	}
+}
