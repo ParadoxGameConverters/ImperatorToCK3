20860e796f799c9969324238756ffb19c544fc44
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-08-20 13:45:07 +0200

    Common utilities for history parsing (#270)
    * base for ContainerField
    
    * Update commonItems.NET
    
    * SimpleField
    
    * History
    
    * HistoryFactory
    
    * update commonItems
    
    * DatedHistoryBlockTests
    
    * Create HistoryTests.cs
    
    * tweaks for CodeFactor
    
    * Tweaks for CodeFactor
    
    * Tweaks for Codacy
    
    * Tweaks for Codacy
    
    * Tweaks for Codacy
    
    * PR tweaks
    
    * PR tweaks
    
    * SimpleFieldTests
    
    * ContainerFieldTests

---
+++ ImperatorToCK3.UnitTests/CommonUtils/DatedHistoryBlockTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using commonItems;
+using ImperatorToCK3.CommonUtils;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.CommonUtils {
+	public class DatedHistoryBlockTests {
+		[Fact]
+		public void OnlyRegisteredThingsAreReturned() {
+			var reader = new BufferedReader(
+				@" = {
+				culture = cuman
+				culture = bashkiri
+				religion = jewish
+				title = ""c_sarkel""
+				development = 500
+				monthly_alien_sightings = 5
+			}");
+
+			var datedHistoryBlock = new DatedHistoryBlock(
+				new List<SimpleFieldDef>
+				{
+					new() {FieldName = "culture", Setter = "culture", InitialValue = ""},
+					new() {FieldName = "title", Setter = "title", InitialValue = ""},
+					new()
+					{
+						FieldName = "monthly_alien_sightings", Setter = "monthly_alien_sightings", InitialValue = "0"
+					}
+				},
+				new List<ContainerFieldDef>(),
+				reader
+			);
+			var contents = datedHistoryBlock.Contents;
+
+			Assert.Equal(3, contents.SimpleFieldContents.Count);
+
+			Assert.Equal("cuman", contents.SimpleFieldContents["culture"][0]);
+			Assert.Equal("bashkiri", contents.SimpleFieldContents["culture"][1]);
+			Assert.Equal("c_sarkel", contents.SimpleFieldContents["title"].Last());
+			Assert.Equal("5", contents.SimpleFieldContents["monthly_alien_sightings"].Last());
+
+			Assert.False(contents.SimpleFieldContents.ContainsKey("religion"));
+			Assert.False(contents.SimpleFieldContents.ContainsKey("development"));
+		}
+
+		[Fact]
+		public void QuotedStringsAreNotReadAsKeys() {
+			var reader = new BufferedReader(
+				@"= {
+					culture = cuman
+					""religion"" = jewish
+				}"
+			);
+
+			var contents = new DatedHistoryBlock(
+				new List<SimpleFieldDef> {
+					new() { FieldName = "religion", Setter = "religion", InitialValue = null }
+				},
+				new List<ContainerFieldDef>(),
+				reader
+			).Contents;
+
+			Assert.Empty(contents.SimpleFieldContents);
+		}
+	}
+}
---
+++ ImperatorToCK3/CommonUtils/ContainerField.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using commonItems;
+
+namespace ImperatorToCK3.CommonUtils {
+	public class ContainerField {
+		public ContainerField(List<string> initialValue) {
+			InitialValue = initialValue;
+		}
+		public List<string> GetValue(Date date) {
+			var pairsWithEarlierOrSameDate = ValueHistory.TakeWhile(d => d.Key <= date);
+			if (pairsWithEarlierOrSameDate.Any()) {
+				return pairsWithEarlierOrSameDate.Last().Value;
+			}
+			return InitialValue;
+		}
+		public SortedDictionary<Date, List<string>> ValueHistory { get; private set; } = new();
+		public List<string> InitialValue { private get; set; } = new();
+		public void AddValueToHistory(List<string> value, Date date) {
+			ValueHistory[date] = value;
+		}
+	}
+}
