63474bef4a2f8bc0da83552d2fde8dd02d068780
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-21 09:14:58 +0200

    Province, ProvinceFactory, ProvinceName

---
+++ ImperatorToCK3/Imperator/Provinces/Province.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using commonItems;
+
+namespace ImperatorToCK3.Imperator.Provinces {
+	public enum ProvinceRank { settlement, city, city_metropolis };
+	public class Province {
+		public ulong ID { get; } = 0;
+		public string Name { get; set; } = "";
+		public string Culture { get; set; } = "";
+		public string Religion { get; set; } = "";
+		public KeyValuePair<ulong, Countries.Country> OwnerCountry { get; set; } = new(0, null);
+		public ulong Controller { get; set; } = 0;
+		public Dictionary<ulong, Pops.Pop?> Pops { get; set; } = new();
+		public ProvinceRank ProvinceRank { get; set; } = ProvinceRank.settlement;
+		public bool Fort { get; set; } = false;
+		public bool HolySite { get; set; } = false;
+		public uint BuildingCount { get; set; } = 0;
+		public double CivilizationValue { get; set; } = 0;
+
+		public Province(ulong ID) {
+			this.ID = ID;
+		}
+		public int GetPopCount() {
+			return Pops.Count;
+		}
+		public void LinkOwnerCountry(Country? country) {
+			if (country is null) {
+				Logger.Log(LogLevel.Warning, $"Province {ID}: cannot link null country!");
+				return;
+			}
+			if (country.ID != OwnerCountry.Key) {
+				Logger.Log(LogLevel.Warning, $"Province {ID}: cannot link country {country.ID}: wrong ID!");
+			} else {
+				OwnerCountry.Value = country;
+			}
+		}
+	}
+}
---
+++ ImperatorToCK3/Imperator/Provinces/ProvinceFactory.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using commonItems;
+
+namespace ImperatorToCK3.Imperator.Provinces {
+	public class ProvinceFactory : Parser {
+		private Province province = new(0);
+		public ProvinceFactory() {
+			RegisterKeyword("province_name", reader => {
+				province.Name = new ProvinceName(reader).Name;
+			});
+			RegisterKeyword("culture", reader => {
+				province.Culture = new SingleString(reader).String;
+			});
+			RegisterKeyword("religion", reader => {
+				province.Religion = new SingleString(reader).String;
+			});
+			RegisterKeyword("owner", reader => {
+				province.OwnerCountry = new(new SingleULong(reader).ULong, null);
+			});
+			RegisterKeyword("controller", reader => {
+				province.Controller = new SingleULong(reader).ULong;
+			});
+			RegisterKeyword("pop", reader => {
+				province.Pops.Add(new SingleULong(reader).ULong, null);
+			});
+			RegisterKeyword("civilization_value", reader => {
+				province.CivilizationValue = new SingleDouble(reader).Double;
+			});
+			RegisterKeyword("province_rank", reader => {
+				var provinceRankStr = new SingleString(reader).String;
+				if (provinceRankStr == "settlement")
+					province.ProvinceRank = ProvinceRank.settlement;
+				else if (provinceRankStr == "city")
+					province.ProvinceRank = ProvinceRank.city;
+				else if (provinceRankStr == "city_metropolis")
+					province.ProvinceRank = ProvinceRank.city_metropolis;
+				else
+					Logger.Log(LogLevel.Warning, $"Unknown province rank for province {province.ID}: {provinceRankStr}");
+			});
+			RegisterKeyword("fort", reader => {
+				province.Fort = new SingleString(reader).String == "yes";
+			});
+			RegisterKeyword("holy_site", reader => {
+				province.HolySite = new SingleULong(reader).ULong != 4294967295; // 4294967295 is 2^32 − 1 and is the default value
+			});
+			RegisterKeyword("buildings", reader => {
+				var buildingsList = new IntList(reader).Ints;
+				province.BuildingCount = (uint)buildingsList.Sum();
+			});
+			RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+		}
+		public Province? GetProvince(BufferedReader reader, ulong provinceID) {
+			province = new Province(provinceID);
+			ParseStream(reader);
+			return province;
+		}
+	}
+}
