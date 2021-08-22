de8f799eb31f4f232527941a928a80bb7ae83793
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-22 23:59:09 +0200

    PR tweaks

--- ImperatorToCK3/Imperator/Characters/AccessoryGeneData.cs
+++ ImperatorToCK3/Imperator/Characters/AccessoryGeneData.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using commonItems;
-
-namespace ImperatorToCK3.Imperator.Characters {
+﻿namespace ImperatorToCK3.Imperator.Characters {
 	public class AccessoryGeneData {
 		public string geneName = "";
 		public string geneTemplate = "";
--- ImperatorToCK3/Imperator/Characters/PaletteCoordinates.cs
+++ ImperatorToCK3/Imperator/Characters/PaletteCoordinates.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using commonItems;
-
-namespace ImperatorToCK3.Imperator.Characters {
+﻿namespace ImperatorToCK3.Imperator.Characters {
 	public class PaletteCoordinates {
 		public uint x = 256; // palettes are 512x512
 		public uint y = 256;
