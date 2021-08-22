5306fc3f09bc7c6bfcc74fc6e961b2a57b83c6c5
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-22 23:40:36 +0200

    Character.Parse

--- ImperatorToCK3/Imperator/Characters/Character.cs
+++ ImperatorToCK3/Imperator/Characters/Character.cs
@@ -8,6 +8,9 @@ using ImperatorToCK3.Imperator.Families;
 
 namespace ImperatorToCK3.Imperator.Characters {
 	public class Character {
+		public Character(ulong ID) {
+			this.ID = ID;
+		}
 		public ulong ID { get; } = 0;
 		private string culture = string.Empty;
 		public string Culture {
@@ -48,7 +51,7 @@ namespace ImperatorToCK3.Imperator.Characters {
 					Logger.Warn($"Setting family with ID mismatch: {value.Key} v. {value.Value.ID}!");
 				}
 				family = value;
-			} 
+			}
 		}
 		public List<string> Traits { get; set; } = new();
 		public CharacterAttributes Attributes { get; private set; } = new();
@@ -76,6 +79,90 @@ namespace ImperatorToCK3.Imperator.Characters {
 
 		public void AddYears(int years) {
 			BirthDate.SubtractYears(years);
+		}
+
+		private Genes.GenesDB? genes;
+
+		private static readonly Parser parser = new();
+		private static Character parsedCharacter = new(0);
+		static Character() {
+			parser.RegisterKeyword("first_name_loc", reader => {
+				parsedCharacter.Name = new CharacterName(reader).Name;
+			});
+			parser.RegisterKeyword("province", reader => {
+				parsedCharacter.ProvinceID = new SingleULong(reader).ULong;
+			});
+			parser.RegisterKeyword("culture", reader => {
+				parsedCharacter.culture = new SingleString(reader).String;
+			});
+			parser.RegisterKeyword("religion", reader => {
+				parsedCharacter.Religion = new SingleString(reader).String;
+			});
+			parser.RegisterKeyword("female", reader => {
+				var femStr = new SingleString(reader).String;
+				parsedCharacter.Female = femStr == "yes";
+			});
+			parser.RegisterKeyword("traits", reader => {
+				parsedCharacter.Traits = new StringList(reader).Strings;
+			});
+			parser.RegisterKeyword("birth_date", reader => {
+				var dateStr = new SingleString(reader).String;
+				parsedCharacter.BirthDate = new Date(dateStr, true); // converted to AD
+			});
+			parser.RegisterKeyword("death_date", reader => {
+				var dateStr = new SingleString(reader).String;
+				parsedCharacter.DeathDate = new Date(dateStr, true); // converted to AD
+			});
+			parser.RegisterKeyword("death", reader => {
+				parsedCharacter.DeathReason = new SingleString(reader).String;
+			});
+			parser.RegisterKeyword("age", reader => {
+				parsedCharacter.Age = (uint)new SingleInt(reader).Int;
+			});
+			parser.RegisterKeyword("nickname", reader => {
+				parsedCharacter.Nickname = new SingleString(reader).String;
+			});
+			parser.RegisterKeyword("family", reader => {
+				parsedCharacter.family = new(new SingleULong(reader).ULong, null);
+			});
+			parser.RegisterKeyword("dna", reader => {
+				parsedCharacter.DNA = new SingleString(reader).String;
+			});
+			parser.RegisterKeyword("mother", reader => {
+				parsedCharacter.Mother = new(new SingleULong(reader).ULong, null);
+			});
+			parser.RegisterKeyword("father", reader => {
+				parsedCharacter.Father = new(new SingleULong(reader).ULong, null);
+			});
+			parser.RegisterKeyword("wealth", reader => {
+				parsedCharacter.Wealth = new SingleDouble(reader).Double;
+			});
+			parser.RegisterKeyword("spouse", reader => {
+				foreach (var spouse in new ULongList(reader).ULongs) {
+					parsedCharacter.Spouses.Add(spouse, null);
+				}
+			});
+			parser.RegisterKeyword("children", reader => {
+				foreach (var child in new ULongList(reader).ULongs) {
+					parsedCharacter.Children.Add(child, null);
+				}
+			});
+			parser.RegisterKeyword("attributes", reader => {
+				var attributesFromBloc = CharacterAttributes.Parse(reader);
+			});
+			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreItem);
+		}
+		public static Character Parse(BufferedReader reader, string idString, Genes.GenesDB genesDB) {
+			parsedCharacter = new Character(ulong.Parse(idString)) {
+				genes = genesDB
+			};
+
+			parser.ParseStream(reader);
+			if (parsedCharacter.DNA?.Length == 552) {
+				parsedCharacter.PortraitData = new PortraitData(parsedCharacter.DNA, parsedCharacter.genes, parsedCharacter.AgeSex);
+			}
+
+			return parsedCharacter;
 		}
 	}
 }
