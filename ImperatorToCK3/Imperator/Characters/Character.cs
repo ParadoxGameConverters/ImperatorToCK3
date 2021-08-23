4c45c0db6534db6c8216d4c5b83126d73b248c57
IhateTrains <IhateTrains@users.noreply.github.com>
2021-08-23 22:58:12 +0200

    CharacterTests

--- ImperatorToCK3/Imperator/Characters/Character.cs
+++ ImperatorToCK3/Imperator/Characters/Character.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using commonItems;
 using ImperatorToCK3.Imperator.Families;
 
@@ -148,7 +144,7 @@ namespace ImperatorToCK3.Imperator.Characters {
 				}
 			});
 			parser.RegisterKeyword("attributes", reader => {
-				var attributesFromBloc = CharacterAttributes.Parse(reader);
+				parsedCharacter.Attributes = CharacterAttributes.Parse(reader);
 			});
 			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreItem);
 		}
