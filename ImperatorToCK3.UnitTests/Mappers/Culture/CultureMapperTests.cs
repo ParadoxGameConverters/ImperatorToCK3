25b61b97ac706ef8d5e6994c8926bb6a8df03365
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-08-29 01:42:46 +0200

    CultureMapper with tests (#282)
    * Create CultureMappingRule.cs
    
    * Update CultureMappingRule.cs
    
    * CultureMapper
    
    * CultureMappingTests
    
    * CultureMapperTests
    
    * Update CultureMapperTests.cs

---
+++ ImperatorToCK3.UnitTests/Mappers/Culture/CultureMapperTests.cs
@@ -0,0 +1,209 @@
+﻿using commonItems;
+using ImperatorToCK3.Mappers.Culture;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.Mappers.Culture {
+	[Collection("Sequential")]
+	[CollectionDefinition("Sequential", DisableParallelization = true)]
+	public class CultureMapperTests {
+		[Fact]
+		public void NonMatchGivesEmptyOptional() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = culture }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.Match("nonMatchingCulture", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void SimpleCultureMatches() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = test }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.Match("test", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void SimpleCultureCorrectlyMatches() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.Match("test", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureMatchesWithReligion() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.Match("test", "thereligion", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureFailsWithWrongReligion() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.Match("test", "unreligion", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureFailsWithNoReligion() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.Match("test", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureMatchesWithCapital() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion ck3Province = 4 }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.Match("test", "thereligion", 4, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureFailsWithWrongCapital() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion ck3Province = 4 }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.Match("test", "thereligion", 3, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureMatchesWithOwnerTitle() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion ck3Province = 4 owner = e_roman_empire }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.Match("test", "thereligion", 4, 49, "e_roman_empire"));
+		}
+
+		[Fact]
+		public void CultureFailsWithWrongOwnerTitle() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion ck3Province = 4 owner = e_roman_empire }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.Match("test", "thereligion", 4, 49, "e_reman_empire"));
+		}
+
+		[Fact]
+		public void CultureMatchesWithNoOwnerTitle() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion ck3Province = 4 }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.Match("test", "thereligion", 4, 49, ""));
+		}
+
+		[Fact]
+		public void CultureMatchesWithNoOwnerTitleInRule() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion ck3Province = 4}"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.Match("test", "thereligion", 4, 49, "e_roman_empire"));
+		}
+
+		[Fact]
+		public void CultureFailsWithOwnerTitleInRule() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion ck3Province = 4 owner = e_roman_empire }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.Match("test", "thereligion", 4, 49, ""));
+		}
+
+		[Fact]
+		public void NonMatchGivesEmptyOptionalWithNonReligiousMatch() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = culture }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.NonReligiousMatch("nonMatchingCulture", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void SimpleCultureMatchesWithNonReligiousMatch() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = test }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.NonReligiousMatch("test", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void SimpleCultureCorrectlyMatchesWithNonReligiousMatch() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.NonReligiousMatch("test", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureFailsWithCorrectReligionWithNonReligiousMatch() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.NonReligiousMatch("test", "thereligion", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureFailsWithWrongReligionWithNonReligiousMatch() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.NonReligiousMatch("test", "unreligion", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureFailsWithNoReligionWithNonReligiousMatch() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi religion = thereligion }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Null(culMapper.NonReligiousMatch("test", "", 56, 49, "e_title"));
+		}
+
+		[Fact]
+		public void CultureMatchesWithReligionAndNonReligiousLinkWithNonReligiousMatch() {
+			var reader = new BufferedReader(
+				"link = { ck3 = culture imp = qwe imp = test imp = poi }"
+			);
+			var culMapper = new CultureMapper(reader);
+
+			Assert.Equal("culture", culMapper.NonReligiousMatch("test", "thereligion", 56, 49, "e_title"));
+		}
+	}
+}
