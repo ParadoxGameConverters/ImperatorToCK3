10b977e639ea197c37c28edd613807a21165a105
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-09-02 23:00:03 +0200

    CoatOfArmsOutputter and CharactersOutputter (#289)
    * Create CoatOfArmsOutputter.cs
    
    * Create CharacterOutputter.cs
    
    * Create CharactersOutputter.cs

---
+++ ImperatorToCK3/Outputter/CharactersOutputter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ImperatorToCK3.CK3.Characters;
+using commonItems;
+
+namespace ImperatorToCK3.Outputter {
+	public static class CharactersOutputter {
+		public static void OutputCharacters(string outputModName, Dictionary<string, Character> characters) {
+			// dumping all into one file
+			var path = "output/" + outputModName + "/history/characters/fromImperator.txt";
+			using var output = new StreamWriter(path);
+			output.Write(CommonFunctions.UTF8BOM);
+			foreach (var character in characters.Values) {
+				CharacterOutputter.OutputCharacter(output, character);
+			}
+		}
+	}
+}
