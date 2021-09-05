e7af71489f64630303a27eb68802cc27e3280273
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-09-05 21:41:05 +0200

    VerifyImperatorVersion, VerifyCK3Version (#303)

--- ImperatorToCK3/Converter.cs
+++ ImperatorToCK3/Converter.cs
@@ -1,23 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using commonItems;
+﻿using commonItems;
 
 namespace ImperatorToCK3 {
-    class Converter {
+    internal static class Converter {
         public static void ConvertImperatorToCK3(ConverterVersion converterVersion) {
-            var config = new Configuration();
+			Logger.Progress("0 %");
+			var config = new Configuration(converterVersion);
 			var imperatorWorld = new Imperator.World(config, converterVersion);
 			var ck3World = new CK3.World(imperatorWorld, config);
 			Outputter.WorldOutputter.OutputWorld(ck3World, config);
 			Logger.Info("* Conversion complete! *");
-		}
-
-        private void LogGameVersions(string imperatorPath, string ck3Path) {
-			// TODO
+			Logger.Progress("100 %");
 		}
     }
 }
