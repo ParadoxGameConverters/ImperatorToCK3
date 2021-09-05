56a333ccd5a3536635da973ecfef83ed0808f42c
IhateTrains <IhateTrains@users.noreply.github.com>
2021-09-05 16:29:23 +0200

    Fix localization loaded logging

--- ImperatorToCK3/CK3/Provinces/ProvinceMappings.cs
+++ ImperatorToCK3/CK3/Provinces/ProvinceMappings.cs
@@ -5,16 +5,16 @@ namespace ImperatorToCK3.CK3.Provinces {
 	/// <summary>
 	/// This class is used to read game/history/province_mapping in CK3.
 	/// CK3 uses province_mapping to set history for provinces that don't need unique entries.
-	/// 
+	///
 	/// Example unique entry in game/history/provinces:
 	/// 6872 = {
 	/// 	religion = coptic
 	/// 	culture = coptic
 	/// }
-	/// 
+	///
 	/// Example province_mapping in game/history/province_mapping:
 	/// 6874 = 6872
-	/// 
+	///
 	/// Now 6874 history is same as 6872 history.
 	/// </summary>
 	public class ProvinceMappings : Parser {
