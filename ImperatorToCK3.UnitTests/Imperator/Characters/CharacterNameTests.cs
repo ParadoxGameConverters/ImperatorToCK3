b627057eda69fd9ffc70461bbad5720d7b790be7
IhateTrains <IhateTrains@users.noreply.github.com>
2021-09-05 20:48:47 +0200

    Don't ignore characters' custom names

---
+++ ImperatorToCK3.UnitTests/Imperator/Characters/CharacterNameTests.cs
@@ -0,0 +1,13 @@
+﻿using ImperatorToCK3.Imperator.Characters;
+using commonItems;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.Imperator.Characters {
+	public class CharacterNameTests {
+		[Fact] public void CustomNameOverridesName() {
+			var reader = new BufferedReader("name=a custom_name=b");
+			var characterName = new CharacterName(reader);
+			Assert.Equal("b", characterName.Name);
+		}
+	}
+}
