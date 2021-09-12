a8a2b174ff57f6dbd2b539555d8bc8dd2c653ada
IhateTrains <IhateTrains@users.noreply.github.com>
2021-09-12 18:01:25 +0200

    DefiniteFormMapperTests

---
+++ ImperatorToCK3.UnitTests/Mappers/TagTitle/DefiniteFormMapperTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImperatorToCK3.Mappers.TagTitle;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.Mappers.TagTitle {
+	public class DefiniteFormMapperTests {
+		[Fact] public void MapperReturnsTrueForMatchingName() {
+			var mapper = new DefiniteFormMapper("TestFiles/configurables/definite_form_names.txt");
+			Assert.True(mapper.IsDefiniteForm("PRY_DYN"));
+		}
+		[Fact]
+		public void MapperReturnsFalseForNonMatchingName() {
+			var mapper = new DefiniteFormMapper("TestFiles/configurables/definite_form_names.txt");
+			Assert.False(mapper.IsDefiniteForm("Atlantis"));
+		}
+	}
+}
