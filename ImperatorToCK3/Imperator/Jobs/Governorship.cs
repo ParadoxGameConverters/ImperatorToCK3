5134b8d1405679fcff14ca4e3927687ab55cb78a
IhateTrains <IhateTrains@users.noreply.github.com>
2021-09-18 14:35:57 +0200

    Create Governorship.cs

---
+++ ImperatorToCK3/Imperator/Jobs/Governorship.cs
@@ -0,0 +1,30 @@
+﻿using commonItems;
+
+namespace ImperatorToCK3.Imperator.Jobs {
+	public class Governorship {
+		public ulong CountryID { get; private set; } = 0;
+		public ulong CharacterID { get; private set; } = 0;
+		public Date StartDate { get; private set; } = new(1, 1, 1);
+		public string RegionName { get; private set; } = string.Empty;
+
+		public Governorship(BufferedReader reader) {
+			var parser = new Parser();
+			parser.RegisterKeyword("who", reader => {
+				CountryID = ParserHelpers.GetULong(reader);
+			});
+			parser.RegisterKeyword("character", reader => {
+				CharacterID = ParserHelpers.GetULong(reader);
+			});
+			parser.RegisterKeyword("start_date", reader => {
+				var dateStr = ParserHelpers.GetString(reader);
+				StartDate = new Date(dateStr);
+			});
+			parser.RegisterKeyword("governorship", reader => {
+				RegionName = ParserHelpers.GetString(reader);
+			});
+			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+
+			parser.ParseStream(reader);
+		}
+	}
+}
