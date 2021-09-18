415765916a19e3c803255411b032a77a1046128f
IhateTrains <IhateTrains@users.noreply.github.com>
2021-09-18 16:29:50 +0200

    Jobs

---
+++ ImperatorToCK3.UnitTests/Imperator/Jobs/JobsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using commonItems;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.Imperator.Jobs {
+	[Collection("Sequential")]
+	[CollectionDefinition("Sequential", DisableParallelization = true)]
+	public class JobsTests {
+		[Fact]
+		public void GovernorshipsDefaultToEmpty() {
+			var reader = new BufferedReader(string.Empty);
+			var jobs = new ImperatorToCK3.Imperator.Jobs.Jobs(reader);
+			Assert.Empty(jobs.Governorships);
+		}
+		[Fact]
+		public void GovernorshipsCanBeRead() {
+			var reader = new BufferedReader(
+				"province_job={who=1} province_job={who=2}"
+			);
+			var jobs = new ImperatorToCK3.Imperator.Jobs.Jobs(reader);
+			Assert.Collection(jobs.Governorships,
+				item1 => Assert.Equal((ulong)1, item1.CountryID),
+				item2 => Assert.Equal((ulong)2, item2.CountryID)
+			);
+		}
+		[Fact]
+		public void IgnoredTokensAreLogged() {
+			var output = new StringWriter();
+			Console.SetOut(output);
+
+			var reader = new BufferedReader(
+				"useless_job = {}"
+			);
+			_ = new ImperatorToCK3.Imperator.Jobs.Jobs(reader);
+
+			Assert.Contains("Ignored Jobs tokens: useless_job", output.ToString());
+		}
+	}
+}
---
+++ ImperatorToCK3/Imperator/Jobs/Jobs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using commonItems;
+
+namespace ImperatorToCK3.Imperator.Jobs {
+	public class Jobs {
+		public List<Governorship> Governorships { get; } = new();
+
+		public Jobs(BufferedReader reader) {
+			var ignoredTokens = new List<string>();
+			var parser = new Parser();
+			parser.RegisterKeyword("province_job", reader => {
+				Governorships.Add(new Governorship(reader));
+			});
+			parser.RegisterRegex(CommonRegexes.Catchall, (reader, token) => {
+				ignoredTokens.Add(token);
+				ParserHelpers.IgnoreItem(reader);
+			});
+
+			parser.ParseStream(reader);
+			Logger.Debug("Ignored Jobs tokens: " + string.Join(", ", ignoredTokens));
+		}
+	}
+}
