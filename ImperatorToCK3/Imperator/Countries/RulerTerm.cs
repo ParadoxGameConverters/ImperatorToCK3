4c17b6e48121b4adc31fb67f979abd2388a4fb5d
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-10-08 10:56:28 +0200

    Validate prehistory.txt with data from save

--- ImperatorToCK3/Imperator/Countries/RulerTerm.cs
+++ ImperatorToCK3/Imperator/Countries/RulerTerm.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections.Generic;
-using commonItems;
+﻿using commonItems;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ImperatorToCK3.Imperator.Countries {
 	public class RulerTerm {
+		internal class PreImperatorRulerInfo {
+			public string? Name { get; set; }
+			public Date? BirthDate { get; set; }
+			public Date? DeathDate { get; set; }
+			public string? Religion { get; set; }
+			public string? Culture { get; set; }
+			public string? Nickname { get; set; }
+			public ulong? CountryId { get; set; }
+		}
 		public ulong? CharacterId { get; private set; }
 		public Date StartDate { get; private set; } = new();
 		public string? Government { get; private set; }
+		internal PreImperatorRulerInfo? PreImperatorRuler { get; set; }
 
 		public static RulerTerm Parse(BufferedReader reader) {
 			parsedTerm = new RulerTerm();
@@ -14,6 +25,7 @@ namespace ImperatorToCK3.Imperator.Countries {
 		}
 
 		public static readonly HashSet<string> IgnoredTokens = new();
+
 		private static readonly Parser parser = new();
 		private static RulerTerm parsedTerm = new();
 		static RulerTerm() {
@@ -32,5 +44,54 @@ namespace ImperatorToCK3.Imperator.Countries {
 				ParserHelpers.IgnoreItem(reader);
 			});
 		}
+
+		public RulerTerm() { }
+		public RulerTerm(BufferedReader prehistoryRulerReader, Countries countries) {
+			PreImperatorRuler = new();
+			var prehistoryParser = new Parser();
+
+			prehistoryParser.RegisterKeyword("name", reader => {
+				PreImperatorRuler.Name = ParserHelpers.GetString(reader);
+			});
+			prehistoryParser.RegisterKeyword("birth_date", reader => {
+				var dateStr = ParserHelpers.GetString(reader);
+				PreImperatorRuler.BirthDate = new Date(dateStr, AUC: true);
+			});
+			prehistoryParser.RegisterKeyword("death_date", reader => {
+				var dateStr = ParserHelpers.GetString(reader);
+				PreImperatorRuler.DeathDate = new Date(dateStr, AUC: true);
+			});
+			prehistoryParser.RegisterKeyword("throne_date", reader => {
+				var dateStr = ParserHelpers.GetString(reader);
+				StartDate = new Date(dateStr, AUC: true);
+			});
+			prehistoryParser.RegisterKeyword("religion", reader => {
+				PreImperatorRuler.Religion = ParserHelpers.GetString(reader);
+			});
+			prehistoryParser.RegisterKeyword("culture", reader => {
+				PreImperatorRuler.Culture = ParserHelpers.GetString(reader);
+			});
+			prehistoryParser.RegisterKeyword("nickname", reader => {
+				PreImperatorRuler.Nickname = ParserHelpers.GetString(reader);
+			});
+			prehistoryParser.RegisterKeyword("country", reader => {
+				var tag = ParserHelpers.GetString(reader);
+				if (tagToCountryIdCache.TryGetValue(tag, out var id)) {
+					PreImperatorRuler.CountryId = id;
+				} else {
+					var matchingCountries = countries.StoredCountries.Values.Where(c => c.Tag == tag);
+					if (matchingCountries.Count() != 1) {
+						Logger.Warn($"Pre-Imperator ruler has wrong tag: {tag}!");
+						return;
+					}
+					var countryId = matchingCountries.First().ID;
+					PreImperatorRuler.CountryId = countryId;
+					tagToCountryIdCache.Add(tag, countryId);
+				}
+			});
+
+			prehistoryParser.ParseStream(prehistoryRulerReader);
+		}
+		private static readonly Dictionary<string, ulong> tagToCountryIdCache = new();
 	}
 }
