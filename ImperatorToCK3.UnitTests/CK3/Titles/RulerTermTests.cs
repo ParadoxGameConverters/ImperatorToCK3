a6174c728962d78f140ae9708feb18157b3db67d
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-10-08 19:40:33 +0200

    PreImperatorTermIsCorrectlyConverted test

--- ImperatorToCK3.UnitTests/CK3/Titles/RulerTermTests.cs
+++ ImperatorToCK3.UnitTests/CK3/Titles/RulerTermTests.cs
@@ -35,5 +35,35 @@ namespace ImperatorToCK3.UnitTests.CK3.Titles {
 			Assert.Equal(new Date(500, 2, 3, AUC: true), ck3RulerTerm.StartDate);
 			Assert.Equal("feudal_government", ck3RulerTerm.Government);
 		}
+
+		[Fact] public void PreImperatorTermIsCorrectlyConverted() {
+			var countries = new ImperatorToCK3.Imperator.Countries.Countries();
+			var countryReader = new BufferedReader("= { tag = SPA }");
+			var sparta = ImperatorToCK3.Imperator.Countries.Country.Parse(countryReader, 69);
+			countries.StoredCountries.Add(sparta.ID, sparta);
+
+			var preImpTermReader = new BufferedReader("= { name=\"Alexander\"" +
+				" birth_date=200.1.1 death_date=300.1.1 throne_date=250.1.1" +
+				" nickname=THE_BOLD religion=hellenic culture=spartan" +
+				" country=SPA }"
+			);
+			var impRulerTerm = new ImperatorToCK3.Imperator.Countries.RulerTerm(preImpTermReader, countries);
+
+			var govReader = new BufferedReader("link = {imp=dictatorship ck3=feudal_government }");
+			var govMapper = new GovernmentMapper(govReader);
+			var ck3RulerTerm = new RulerTerm(impRulerTerm,
+				new Dictionary<string, Character>(),
+				govMapper,
+				new LocalizationMapper(),
+				new ReligionMapper(),
+				new CultureMapper(),
+				new ProvinceMapper()
+			);
+			Assert.Equal("imperatorRegnalSPAAlexander504.1.1BC", ck3RulerTerm.CharacterId);
+			Assert.Equal(new Date(250,1,1, AUC: true), ck3RulerTerm.StartDate);
+			var ruler = ck3RulerTerm.PreImperatorRuler;
+			Assert.NotNull(ruler);
+			Assert.Equal("Alexander", ruler.Name);
+		}
 	}
 }
