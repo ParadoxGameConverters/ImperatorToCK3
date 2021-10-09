e788fb5e52435035a66d067008230c6cdcdba0dd
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-10-09 23:59:29 +0200

    Update WeightBlock.cs

--- ImperatorToCK3/Imperator/Genes/WeightBlock.cs
+++ ImperatorToCK3/Imperator/Genes/WeightBlock.cs
@@ -32,6 +32,16 @@ namespace ImperatorToCK3.Imperator.Genes {
 			}
 			return 0;
 		}
+		public double GetMatchingPercentage(string objectName) { // TODO: ADD TESTS
+			uint sumOfPrecedingAbsoluteWeights = 0;
+			foreach (var (key, value) in objectsList) {
+				if (key == objectName) {
+					return (double)sumOfPrecedingAbsoluteWeights / SumOfAbsoluteWeights;
+				}
+				sumOfPrecedingAbsoluteWeights += value;
+			}
+			throw new KeyNotFoundException($"Set entry {objectName} not found!");
+		}
 		public string? GetMatchingObject(double percentAsDecimal) { // argument must be in range <0; 1>
 			if (percentAsDecimal < 0 || percentAsDecimal > 1) {
 				throw new ArgumentOutOfRangeException("percentAsDecimal is " + percentAsDecimal + ", should be in range <0;1>");
