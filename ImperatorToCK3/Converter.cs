98e0ce63bbcc9c5368167e29ad3058f028fd8aa6
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-11-19 22:16:24 +0100

    LogSystemInfo

--- ImperatorToCK3/Converter.cs
+++ ImperatorToCK3/Converter.cs
@@ -1,15 +1,25 @@
 ﻿using commonItems;
+using System;
+using System.Globalization;
 
 namespace ImperatorToCK3 {
 	internal static class Converter {
 		public static void ConvertImperatorToCK3(ConverterVersion converterVersion) {
 			Logger.Progress("0 %");
+			LogSystemInfo();
 			var config = new Configuration(converterVersion);
 			var imperatorWorld = new Imperator.World(config, converterVersion);
 			var ck3World = new CK3.World(imperatorWorld, config);
 			Outputter.WorldOutputter.OutputWorld(ck3World, config);
 			Logger.Info("* Conversion complete! *");
 			Logger.Progress("100 %");
+		}
+
+		private static void LogSystemInfo() {
+			CultureInfo ci = CultureInfo.InstalledUICulture;
+			Logger.DebugFormat("Installed UI Language Info: {0}", ci.Name);
+			ci = CultureInfo.CurrentUICulture;
+			Logger.DebugFormat("Current UI Language Info: {0}", ci.Name);
 		}
 	}
 }
