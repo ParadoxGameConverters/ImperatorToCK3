92d6ab9d1a760b77530f0dd3d25db5199476cda9
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-11-27 09:55:54 +0100

    Fix SuccessionTriggersOutputter missing closing braces

--- ImperatorToCK3/Outputter/WorldOutputter.cs
+++ ImperatorToCK3/Outputter/WorldOutputter.cs
@@ -5,7 +5,7 @@ using System.IO;
 namespace ImperatorToCK3.Outputter {
 	public static class WorldOutputter {
 		public static void OutputWorld(World ck3World, Configuration theConfiguration, Date conversionDate) {
-			var directoryToClear = "output/" + theConfiguration.OutputModName;
+			var directoryToClear = $"output/{theConfiguration.OutputModName}";
 			var di = new DirectoryInfo(directoryToClear);
 			if (di.Exists) {
 				Logger.Info("Clearing the output mod folder...");
@@ -55,14 +55,14 @@ namespace ImperatorToCK3.Outputter {
 			var outputPath = "output/" + theConfiguration.OutputModName;
 
 			Logger.Info("Copying named colors...");
-			SystemUtils.TryCopyFile(theConfiguration.ImperatorPath + "/game/common/named_colors/default_colors.txt",
-									 outputPath + "/common/named_colors/imp_colors.txt");
+			SystemUtils.TryCopyFile($"{theConfiguration.ImperatorPath}/game/common/named_colors/default_colors.txt",
+									 $"{outputPath}/common/named_colors/imp_colors.txt");
 
 			Logger.Info("Copying Coats of Arms...");
 			ColoredEmblemsOutputter.CopyColoredEmblems(theConfiguration, outputName);
 			CoatOfArmsOutputter.OutputCoas(outputName, ck3World.LandedTitles);
-			SystemUtils.TryCopyFolder(theConfiguration.ImperatorPath + "/game/gfx/coat_of_arms/patterns",
-							outputPath + "/gfx/coat_of_arms/patterns");
+			SystemUtils.TryCopyFolder($"{theConfiguration.ImperatorPath}/game/gfx/coat_of_arms/patterns",
+							$"{outputPath}/gfx/coat_of_arms/patterns");
 
 			Logger.Info("Copying blankMod files to output...");
 			SystemUtils.TryCopyFolder("blankMod/output", outputPath);
@@ -75,7 +75,7 @@ namespace ImperatorToCK3.Outputter {
 		}
 
 		private static void OutputModFile(string outputName) {
-			using var modFile = new StreamWriter("output/" + outputName + ".mod");
+			using var modFile = new StreamWriter($"output/{outputName}.mod");
 			modFile.WriteLine($"name = \"Converted - {outputName}\"");
 			modFile.WriteLine($"path = \"mod/{outputName}\"");
 			modFile.WriteLine("replace_path = \"common/landed_titles\"");
@@ -90,34 +90,34 @@ namespace ImperatorToCK3.Outputter {
 		}
 
 		private static void CreateFolders(string outputName) {
-			SystemUtils.TryCreateFolder("output/" + outputName + "/history");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/history/titles");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/history/characters");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/history/provinces");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/history/province_mapping");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/bookmarks");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/bookmark_portraits");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/coat_of_arms");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/coat_of_arms/coat_of_arms");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/dynasties");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/landed_titles");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/named_colors");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/common/scripted_triggers");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization/replace");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization/replace/english");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization/replace/french");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization/replace/german");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization/replace/russian");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization/replace/simp_chinese");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/localization/replace/spanish");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/gfx");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/gfx/coat_of_arms");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/gfx/coat_of_arms/colored_emblems");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/gfx/coat_of_arms/patterns");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/gfx/interface");
-			SystemUtils.TryCreateFolder("output/" + outputName + "/gfx/interface/bookmarks");
+			SystemUtils.TryCreateFolder($"output/{outputName}/history");
+			SystemUtils.TryCreateFolder($"output/{outputName}/history/titles");
+			SystemUtils.TryCreateFolder($"output/{outputName}/history/characters");
+			SystemUtils.TryCreateFolder($"output/{outputName}/history/provinces");
+			SystemUtils.TryCreateFolder($"output/{outputName}/history/province_mapping");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/bookmarks");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/bookmark_portraits");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/coat_of_arms");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/coat_of_arms/coat_of_arms");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/dynasties");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/landed_titles");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/named_colors");
+			SystemUtils.TryCreateFolder($"output/{outputName}/common/scripted_triggers");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization/replace");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization/replace/english");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization/replace/french");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization/replace/german");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization/replace/russian");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization/replace/simp_chinese");
+			SystemUtils.TryCreateFolder($"output/{outputName}/localization/replace/spanish");
+			SystemUtils.TryCreateFolder($"output/{outputName}/gfx");
+			SystemUtils.TryCreateFolder($"output/{outputName}/gfx/coat_of_arms");
+			SystemUtils.TryCreateFolder($"output/{outputName}/gfx/coat_of_arms/colored_emblems");
+			SystemUtils.TryCreateFolder($"output/{outputName}/gfx/coat_of_arms/patterns");
+			SystemUtils.TryCreateFolder($"output/{outputName}/gfx/interface");
+			SystemUtils.TryCreateFolder($"output/{outputName}/gfx/interface/bookmarks");
 		}
 	}
 }
