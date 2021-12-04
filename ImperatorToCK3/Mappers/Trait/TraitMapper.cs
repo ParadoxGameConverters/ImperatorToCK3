b5b19d0d292a421c23b6e11c7397a6bcc27a66ae
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-04 16:27:34 +0100

    A few tests for mappers

--- ImperatorToCK3/Mappers/Trait/TraitMapper.cs
+++ ImperatorToCK3/Mappers/Trait/TraitMapper.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 
 namespace ImperatorToCK3.Mappers.Trait {
-	public class TraitMapper : Parser {
+	public class TraitMapper {
 		private readonly Dictionary<string, string> impToCK3TraitMap = new();
 
 		public TraitMapper(string filePath) {
-			Logger.Info("Parsing trait mappings.");
-			RegisterKeys();
-			ParseFile(filePath);
-			ClearRegisteredRules();
+			Logger.Info("Parsing trait mappings...");
+			var parser = new Parser();
+			RegisterKeys(parser);
+			parser.ParseFile(filePath);
 			Logger.Info($"Loaded {impToCK3TraitMap.Count} trait links.");
 		}
 		public TraitMapper(BufferedReader reader) {
-			RegisterKeys();
-			ParseStream(reader);
-			ClearRegisteredRules();
+			var parser = new Parser();
+			RegisterKeys(parser);
+			parser.ParseStream(reader);
 		}
-		private void RegisterKeys() {
-			RegisterKeyword("link", reader => {
+		private void RegisterKeys(Parser parser) {
+			parser.RegisterKeyword("link", reader => {
 				var mapping = new TraitMapping(reader);
 				if (mapping.CK3Trait is null) {
 					return;
@@ -27,7 +27,7 @@ namespace ImperatorToCK3.Mappers.Trait {
 					impToCK3TraitMap.Add(imperatorTrait, mapping.CK3Trait);
 				}
 			});
-			RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
 		}
 		public string? GetCK3TraitForImperatorTrait(string impTrait) {
 			return impToCK3TraitMap.TryGetValue(impTrait, out var ck3Trait) ? ck3Trait : null;
