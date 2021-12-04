8d80c8b30d3e31f0798e33a8ab8d88ae292e7652
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-04 17:08:59 +0100

    RakalyCallerReportsWrongExitCode test

--- ImperatorToCK3/Helpers/RakalyCaller.cs
+++ ImperatorToCK3/Helpers/RakalyCaller.cs
@@ -27,7 +27,7 @@ namespace ImperatorToCK3.Helpers {
 				throw new FormatException($"Rakaly melter failed to melt {savePath} with exit code {returnCode}");
 			}
 
-			var meltedSaveName = CommonFunctions.TrimExtension(savePath) + "_melted.rome";
+			var meltedSaveName = $"{CommonFunctions.TrimExtension(savePath)}_melted.rome";
 			const string destFileName = "temp/melted_save.rome";
 			//first, delete target file if exists, as File.Move() does not support overwrite
 			if (File.Exists(destFileName)) {
