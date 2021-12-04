56634bcfc9cb86b7af9b38d30b5260f71b497145
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-04 18:03:02 +0100

    Update RakalyCaller.cs

--- ImperatorToCK3/Helpers/RakalyCaller.cs
+++ ImperatorToCK3/Helpers/RakalyCaller.cs
@@ -14,7 +14,6 @@ namespace ImperatorToCK3.Helpers {
 			string arguments = $"melt --unknown-key stringify \"{savePath}\"";
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
 				executablePath = "Resources/rakaly/rakaly-0.3.15-x86_64-apple-darwin/rakaly";
-				process.StartInfo.UseShellExecute = true;
 				Exec($"chmod +x {currentDir}/{executablePath}");
 			} else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
 				executablePath = "Resources/rakaly/rakaly-0.3.15-x86_64-unknown-linux-musl/rakaly";
