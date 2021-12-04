037ecf8c1072c1cdc4122779213f292aa3371611
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-04 17:51:54 +0100

    chmod +x for rakaly binary on Linux

--- ImperatorToCK3/Helpers/RakalyCaller.cs
+++ ImperatorToCK3/Helpers/RakalyCaller.cs
@@ -7,15 +7,18 @@ using System.Runtime.InteropServices;
 namespace ImperatorToCK3.Helpers {
 	public static class RakalyCaller {
 		public static void MeltSave(string savePath) {
+			using Process process = new();
+
+			string currentDir = Directory.GetCurrentDirectory();
 			string executablePath = "Resources/rakaly/rakaly-0.3.15-x86_64-pc-windows-msvc/rakaly.exe";
 			string arguments = $"melt --unknown-key stringify \"{savePath}\"";
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
 				executablePath = "Resources/rakaly/rakaly-0.3.15-x86_64-apple-darwin/rakaly";
 			} else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
 				executablePath = "Resources/rakaly/rakaly-0.3.15-x86_64-unknown-linux-musl/rakaly";
-				arguments = $"sudo {arguments}";
+				Exec($"chmod +x {currentDir}/{executablePath}");
 			}
-			using Process process = new();
+
 			process.StartInfo.UseShellExecute = false;
 			process.StartInfo.FileName = executablePath;
 			process.StartInfo.Arguments = arguments;
@@ -35,6 +38,25 @@ namespace ImperatorToCK3.Helpers {
 				File.Delete(destFileName);
 			}
 			File.Move(meltedSaveName, destFileName);
+		}
+
+		// https://stackoverflow.com/a/47918132/10249243
+		private static void Exec(string cmd) {
+			var escapedArgs = cmd.Replace("\"", "\\\"");
+
+			using var process = new Process {
+				StartInfo = new ProcessStartInfo {
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true,
+					WindowStyle = ProcessWindowStyle.Hidden,
+					FileName = "/bin/bash",
+					Arguments = $"-c \"{escapedArgs}\""
+				}
+			};
+
+			process.Start();
+			process.WaitForExit();
 		}
 	}
 }
