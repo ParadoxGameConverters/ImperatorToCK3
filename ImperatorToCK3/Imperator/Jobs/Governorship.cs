9bff6f2ce640a1c16dbeb8be1f6adefa10e88839
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-12 00:59:24 +0100

    File-scoped namespaces for Pops and Jobs (#472)
    * File-scoped namespace in Imperator Pops
    
    * File-scoped namespace for Imperator Jobs

--- ImperatorToCK3/Imperator/Jobs/Governorship.cs
+++ ImperatorToCK3/Imperator/Jobs/Governorship.cs
@@ -1,24 +1,21 @@
 ﻿using commonItems;
 
-namespace ImperatorToCK3.Imperator.Jobs {
-	public class Governorship {
-		public ulong CountryId { get; private set; } = 0;
-		public ulong CharacterId { get; private set; } = 0;
-		public Date StartDate { get; private set; } = new(1, 1, 1);
-		public string RegionName { get; private set; } = string.Empty;
+namespace ImperatorToCK3.Imperator.Jobs;
 
-		public Governorship(BufferedReader reader) {
-			var parser = new Parser();
-			parser.RegisterKeyword("who", reader => CountryId = reader.GetULong());
-			parser.RegisterKeyword("character", reader => CharacterId = reader.GetULong());
-			parser.RegisterKeyword("start_date", reader => {
-				var dateStr = reader.GetString();
-				StartDate = new Date(dateStr, AUC: true);
-			});
-			parser.RegisterKeyword("governorship", reader => RegionName = reader.GetString());
-			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+public class Governorship {
+	public ulong CountryId { get; private set; } = 0;
+	public ulong CharacterId { get; private set; } = 0;
+	public Date StartDate { get; private set; } = new(1, 1, 1);
+	public string RegionName { get; private set; } = string.Empty;
 
-			parser.ParseStream(reader);
-		}
+	public Governorship(BufferedReader reader) {
+		var parser = new Parser();
+		parser.RegisterKeyword("who", reader => CountryId = reader.GetULong());
+		parser.RegisterKeyword("character", reader => CharacterId = reader.GetULong());
+		parser.RegisterKeyword("start_date", reader => StartDate = new Date(reader.GetString(), AUC: true));
+		parser.RegisterKeyword("governorship", reader => RegionName = reader.GetString());
+		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+
+		parser.ParseStream(reader);
 	}
 }
