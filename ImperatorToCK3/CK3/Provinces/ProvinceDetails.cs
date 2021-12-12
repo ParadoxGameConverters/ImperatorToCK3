f9f3b214641656bdfa30244863a4e100248cab11
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-12 01:00:02 +0100

    File-scoped namespaces for CK3 Map, Dynasties, Provinces (#474)

--- ImperatorToCK3/CK3/Provinces/ProvinceDetails.cs
+++ ImperatorToCK3/CK3/Provinces/ProvinceDetails.cs
@@ -3,67 +3,67 @@ using ImperatorToCK3.CommonUtils;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ImperatorToCK3.CK3.Provinces {
-	public class ProvinceDetails {
-		// These values are open to ease management.
-		// This is a storage container for CK3::Province.
-		public string Culture { get; set; } = string.Empty;
-		public string Religion { get; set; } = string.Empty;
-		public string Holding { get; set; } = "none";
-		public List<string> Buildings { get; } = new();
+namespace ImperatorToCK3.CK3.Provinces;
 
-		public ProvinceDetails() { }
-		public ProvinceDetails(ProvinceDetails otherDetails) {
-			Culture = otherDetails.Culture;
-			Religion = otherDetails.Religion;
-			Holding = otherDetails.Holding;
-			Buildings = new List<string>(otherDetails.Buildings);
-		}
-		public ProvinceDetails(BufferedReader reader, Date ck3BookmarkDate) {
-			var history = historyFactory.GetHistory(reader);
+public class ProvinceDetails {
+	// These values are open to ease management.
+	// This is a storage container for CK3::Province.
+	public string Culture { get; set; } = string.Empty;
+	public string Religion { get; set; } = string.Empty;
+	public string Holding { get; set; } = "none";
+	public List<string> Buildings { get; } = new();
 
-			var cultureOpt = history.GetFieldValue("culture", ck3BookmarkDate);
-			if (cultureOpt is string cultureStr) {
-				Culture = cultureStr;
-			}
-			var religionOpt = history.GetFieldValue("religion", ck3BookmarkDate);
-			if (religionOpt is string religionStr) {
-				Religion = religionStr;
-			}
-			switch (history.GetFieldValue("holding", ck3BookmarkDate)) {
-				case null:
-					Logger.Warn("Province's holding can't be null!");
-					break;
-				case string holdingStr:
-					Holding = holdingStr;
-					break;
-				default:
-					Logger.Warn("Wrong province holding value!");
-					break;
-			}
-
-			var buildingsValue = history.GetFieldValue("buildings", ck3BookmarkDate);
-			switch (buildingsValue) {
-				case null:
-					Logger.Warn("Province's buildings list can't be null!");
-					break;
-				case IList<object> buildingsList:
-					Buildings = new List<string>(buildingsList.Select(b => b.ToString()!));
-					break;
-				case IList<string> buildingsList:
-					Buildings = (List<string>)buildingsList;
-					break;
-				default:
-					Logger.Warn($"Wrong province buildings value: {buildingsValue}");
-					break;
-			}
-		}
-
-		private static readonly HistoryFactory historyFactory = new HistoryFactory.HistoryFactoryBuilder()
-			.WithSimpleField("culture", "culture", null)
-			.WithSimpleField("religion", "religion", null)
-			.WithSimpleField("holding", "holding", "none")
-			.WithContainerField("buildings", "buildings", new())
-			.Build();
+	public ProvinceDetails() { }
+	public ProvinceDetails(ProvinceDetails otherDetails) {
+		Culture = otherDetails.Culture;
+		Religion = otherDetails.Religion;
+		Holding = otherDetails.Holding;
+		Buildings = new(otherDetails.Buildings);
 	}
+	public ProvinceDetails(BufferedReader reader, Date ck3BookmarkDate) {
+		var history = historyFactory.GetHistory(reader);
+
+		var cultureOpt = history.GetFieldValue("culture", ck3BookmarkDate);
+		if (cultureOpt is string cultureStr) {
+			Culture = cultureStr;
+		}
+		var religionOpt = history.GetFieldValue("religion", ck3BookmarkDate);
+		if (religionOpt is string religionStr) {
+			Religion = religionStr;
+		}
+		switch (history.GetFieldValue("holding", ck3BookmarkDate)) {
+			case null:
+				Logger.Warn("Province's holding can't be null!");
+				break;
+			case string holdingStr:
+				Holding = holdingStr;
+				break;
+			default:
+				Logger.Warn("Wrong province holding value!");
+				break;
+		}
+
+		var buildingsValue = history.GetFieldValue("buildings", ck3BookmarkDate);
+		switch (buildingsValue) {
+			case null:
+				Logger.Warn("Province's buildings list can't be null!");
+				break;
+			case IList<object> buildingsList:
+				Buildings = new List<string>(buildingsList.Select(b => b.ToString()!));
+				break;
+			case IList<string> buildingsList:
+				Buildings = (List<string>)buildingsList;
+				break;
+			default:
+				Logger.Warn($"Wrong province buildings value: {buildingsValue}");
+				break;
+		}
+	}
+
+	private static readonly HistoryFactory historyFactory = new HistoryFactory.HistoryFactoryBuilder()
+		.WithSimpleField("culture", "culture", null)
+		.WithSimpleField("religion", "religion", null)
+		.WithSimpleField("holding", "holding", "none")
+		.WithContainerField("buildings", "buildings", new())
+		.Build();
 }
--- ImperatorToCK3/CK3/Provinces/ProvinceMappings.cs
+++ ImperatorToCK3/CK3/Provinces/ProvinceMappings.cs
@@ -1,46 +1,46 @@
 ﻿using commonItems;
 using System.Collections.Generic;
 
-namespace ImperatorToCK3.CK3.Provinces {
-	/// <summary>
-	/// <para>
-	/// This class is used to read game/history/province_mapping in CK3.
-	/// CK3 uses province_mapping to set history for provinces that don't need unique entries.
-	/// </para>
-	/// <para>
-	/// Example unique entry in game/history/provinces:
-	/// 6872 = {
-	/// 	religion = coptic
-	/// 	culture = coptic
-	/// }
-	/// </para>
-	/// <para>
-	/// Example province_mapping in game/history/province_mapping:
-	/// 6874 = 6872
-	/// </para>
-	/// <para>Now 6874 history is same as 6872 history.</para>
-	/// </summary>
-	public class ProvinceMappings : Dictionary<ulong, ulong> {
-		public ProvinceMappings(string filePath) {
-			var parser = new Parser();
-			RegisterKeys(parser);
-			parser.ParseFile(filePath);
-		}
+namespace ImperatorToCK3.CK3.Provinces;
 
-		private void RegisterKeys(Parser parser) {
-			parser.RegisterRegex(CommonRegexes.Integer, (reader, provIdString) => {
-				var targetProvId = ulong.Parse(provIdString);
-				var baseProvId = reader.GetULong();
-				if (targetProvId == baseProvId) { // if left and right IDs are equal, no point in mapping
-					return;
-				}
+/// <summary>
+/// <para>
+/// This class is used to read game/history/province_mapping in CK3.
+/// CK3 uses province_mapping to set history for provinces that don't need unique entries.
+/// </para>
+/// <para>
+/// Example unique entry in game/history/provinces:
+/// 6872 = {
+/// 	religion = coptic
+/// 	culture = coptic
+/// }
+/// </para>
+/// <para>
+/// Example province_mapping in game/history/province_mapping:
+/// 6874 = 6872
+/// </para>
+/// <para>Now 6874 history is same as 6872 history.</para>
+/// </summary>
+public class ProvinceMappings : Dictionary<ulong, ulong> {
+	public ProvinceMappings(string filePath) {
+		var parser = new Parser();
+		RegisterKeys(parser);
+		parser.ParseFile(filePath);
+	}
 
-				if (ContainsKey(targetProvId)) {
-					Logger.Warn($"Duplicate province mapping for {targetProvId}, overwriting!");
-				}
-				this[targetProvId] = baseProvId;
-			});
-			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
-		}
+	private void RegisterKeys(Parser parser) {
+		parser.RegisterRegex(CommonRegexes.Integer, (reader, provIdString) => {
+			var targetProvId = ulong.Parse(provIdString);
+			var baseProvId = reader.GetULong();
+			if (targetProvId == baseProvId) { // if left and right IDs are equal, no point in mapping
+				return;
+			}
+
+			if (ContainsKey(targetProvId)) {
+				Logger.Warn($"Duplicate province mapping for {targetProvId}, overwriting!");
+			}
+			this[targetProvId] = baseProvId;
+		});
+		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
 	}
 }
