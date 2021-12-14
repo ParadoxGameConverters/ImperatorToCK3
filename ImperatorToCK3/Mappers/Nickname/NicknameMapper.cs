3d381cc55e535340d3883729b225c441bda58afa
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-14 23:33:41 +0100

    File-scoped namespaces for some mappers

--- ImperatorToCK3/Mappers/Nickname/NicknameMapper.cs
+++ ImperatorToCK3/Mappers/Nickname/NicknameMapper.cs
@@ -1,40 +1,40 @@
 ﻿using commonItems;
 using System.Collections.Generic;
 
-namespace ImperatorToCK3.Mappers.Nickname {
-	public class NicknameMapper {
-		private readonly Dictionary<string, string> impToCK3NicknameMap = new();
+namespace ImperatorToCK3.Mappers.Nickname;
 
-		public NicknameMapper(string filePath) {
-			Logger.Info("Parsing nickname mappings...");
-			var parser = new Parser();
-			RegisterKeys(parser);
-			parser.ParseFile(filePath);
-			Logger.Info($"Loaded {impToCK3NicknameMap.Count} nickname links.");
-		}
-		public NicknameMapper(BufferedReader reader) {
-			var parser = new Parser();
-			RegisterKeys(parser);
-			parser.ParseStream(reader);
-		}
-		private void RegisterKeys(Parser parser) {
-			parser.RegisterKeyword("link", reader => {
-				var mapping = new NicknameMapping(reader);
-				if (mapping.CK3Nickname is null) {
-					return;
-				}
+public class NicknameMapper {
+	private readonly Dictionary<string, string> impToCK3NicknameMap = new();
 
-				foreach (var imperatorNickname in mapping.ImperatorNicknames) {
-					impToCK3NicknameMap.Add(imperatorNickname, mapping.CK3Nickname);
-				}
-			});
-			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
-		}
-		public string? GetCK3NicknameForImperatorNickname(string? impNickname) {
-			if (impNickname is null) {
-				return null;
+	public NicknameMapper(string filePath) {
+		Logger.Info("Parsing nickname mappings...");
+		var parser = new Parser();
+		RegisterKeys(parser);
+		parser.ParseFile(filePath);
+		Logger.Info($"Loaded {impToCK3NicknameMap.Count} nickname links.");
+	}
+	public NicknameMapper(BufferedReader reader) {
+		var parser = new Parser();
+		RegisterKeys(parser);
+		parser.ParseStream(reader);
+	}
+	private void RegisterKeys(Parser parser) {
+		parser.RegisterKeyword("link", reader => {
+			var mapping = new NicknameMapping(reader);
+			if (mapping.CK3Nickname is null) {
+				return;
 			}
-			return impToCK3NicknameMap.TryGetValue(impNickname, out var ck3Nickname) ? ck3Nickname : null;
+
+			foreach (var imperatorNickname in mapping.ImperatorNicknames) {
+				impToCK3NicknameMap.Add(imperatorNickname, mapping.CK3Nickname);
+			}
+		});
+		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+	}
+	public string? GetCK3NicknameForImperatorNickname(string? impNickname) {
+		if (impNickname is null) {
+			return null;
 		}
+		return impToCK3NicknameMap.TryGetValue(impNickname, out var ck3Nickname) ? ck3Nickname : null;
 	}
 }
