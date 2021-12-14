f51c9db40faec9f06ebd5723557c8e2a1c63ce39
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-14 08:33:45 +0100

    Support mod CoAs

--- ImperatorToCK3/Mappers/CoA/CoaMapper.cs
+++ ImperatorToCK3/Mappers/CoA/CoaMapper.cs
@@ -1,35 +1,32 @@
 ﻿using commonItems;
 using System.Collections.Generic;
-using System.IO;
 
-namespace ImperatorToCK3.Mappers.CoA {
-	public class CoaMapper : Parser {
-		public CoaMapper(Configuration theConfiguration) {
-			var coasPath = Path.Combine(theConfiguration.ImperatorPath, "game", "common", "coat_of_arms", "coat_of_arms");
-			var fileNames = SystemUtils.GetAllFilesInFolderRecursive(coasPath);
-			Logger.Info("Parsing CoAs...");
-			RegisterKeys();
-			foreach (var fileName in fileNames) {
-				ParseFile(Path.Combine(coasPath, fileName));
-			}
-			ClearRegisteredRules();
-			Logger.Info($"Loaded {coasMap.Count} coats of arms.");
-		}
-		public CoaMapper(string coaFilePath) {
-			RegisterKeys();
-			ParseFile(coaFilePath);
-			ClearRegisteredRules();
-		}
-		private void RegisterKeys() {
-			RegisterKeyword("template", ParserHelpers.IgnoreItem); // we don't need templates, we need CoAs!
-			RegisterRegex(CommonRegexes.Catchall, (reader, flagName) => coasMap.Add(flagName, new StringOfItem(reader).ToString()));
-		}
+namespace ImperatorToCK3.Mappers.CoA;
 
-		public string? GetCoaForFlagName(string impFlagName) {
-			bool contains = coasMap.TryGetValue(impFlagName, out string? value);
-			return contains ? value : null;
-		}
+public class CoaMapper {
+	public CoaMapper(Configuration config, List<Mod> mods) {
+		Logger.Info("Parsing CoAs...");
+		var parser = new Parser();
+		RegisterKeys(parser);
+		const string coasPath = "common/coat_of_arms/coat_of_arms";
+		parser.ParseGameFolder(coasPath, config.ImperatorPath, mods, true);
 
-		private readonly Dictionary<string, string> coasMap = new();
+		Logger.Info($"Loaded {coasMap.Count} coats of arms.");
 	}
+	public CoaMapper(string coaFilePath) {
+		var parser = new Parser();
+		RegisterKeys(parser);
+		parser.ParseFile(coaFilePath);
+	}
+	private void RegisterKeys(Parser parser) {
+		parser.RegisterKeyword("template", ParserHelpers.IgnoreItem); // we don't need templates, we need CoAs!
+		parser.RegisterRegex(CommonRegexes.Catchall, (reader, flagName) => coasMap.Add(flagName, reader.GetStringOfItem().ToString()));
+	}
+
+	public string? GetCoaForFlagName(string impFlagName) {
+		bool contains = coasMap.TryGetValue(impFlagName, out string? value);
+		return contains ? value : null;
+	}
+
+	private readonly Dictionary<string, string> coasMap = new();
 }
