6e325e715f989626e38523629a4b35dbee46206a
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-15 07:03:35 +0100

    File-scoped namespaces for some mappers (#487)

--- ImperatorToCK3/Mappers/SuccessionLaw/SuccessionLawMapper.cs
+++ ImperatorToCK3/Mappers/SuccessionLaw/SuccessionLawMapper.cs
@@ -1,42 +1,42 @@
 ﻿using commonItems;
 using System.Collections.Generic;
 
-namespace ImperatorToCK3.Mappers.SuccessionLaw {
-	public class SuccessionLawMapper : Parser {
-		private readonly Dictionary<string, SortedSet<string>> impToCK3SuccessionLawMap = new();
-		public SuccessionLawMapper(string filePath) {
-			Logger.Info("Parsing succession law mappings.");
-			RegisterKeys();
-			ParseFile(filePath);
-			ClearRegisteredRules();
-			Logger.Info($"Loaded {impToCK3SuccessionLawMap.Count} succession law links.");
-		}
-		public SuccessionLawMapper(BufferedReader reader) {
-			RegisterKeys();
-			ParseStream(reader);
-			ClearRegisteredRules();
-		}
-		private void RegisterKeys() {
-			RegisterKeyword("link", reader => {
-				var mapping = new SuccessionLawMapping(reader);
-				if (mapping.CK3SuccessionLaws.Count == 0) {
-					Logger.Warn("SuccessionLawMapper: link with no CK3 successions laws");
-					return;
-				}
-				if (!impToCK3SuccessionLawMap.TryAdd(mapping.ImperatorLaw, mapping.CK3SuccessionLaws)) {
-					impToCK3SuccessionLawMap[mapping.ImperatorLaw].UnionWith(mapping.CK3SuccessionLaws);
-				}
-			});
-			RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
-		}
-		public SortedSet<string> GetCK3LawsForImperatorLaws(SortedSet<string> impLaws) {
-			var lawsToReturn = new SortedSet<string>();
-			foreach (var impLaw in impLaws) {
-				if (impToCK3SuccessionLawMap.TryGetValue(impLaw, out var ck3Laws)) {
-					lawsToReturn.UnionWith(ck3Laws);
-				}
+namespace ImperatorToCK3.Mappers.SuccessionLaw;
+
+public class SuccessionLawMapper {
+	private readonly Dictionary<string, SortedSet<string>> impToCK3SuccessionLawMap = new();
+	public SuccessionLawMapper(string filePath) {
+		Logger.Info("Parsing succession law mappings...");
+		var parser = new Parser();
+		RegisterKeys(parser);
+		parser.ParseFile(filePath);
+		Logger.Info($"Loaded {impToCK3SuccessionLawMap.Count} succession law links.");
+	}
+	public SuccessionLawMapper(BufferedReader reader) {
+		var parser = new Parser();
+		RegisterKeys(parser);
+		parser.ParseStream(reader);
+	}
+	private void RegisterKeys(Parser parser) {
+		parser.RegisterKeyword("link", reader => {
+			var mapping = new SuccessionLawMapping(reader);
+			if (mapping.CK3SuccessionLaws.Count == 0) {
+				Logger.Warn("SuccessionLawMapper: link with no CK3 successions laws");
+				return;
 			}
-			return lawsToReturn;
+			if (!impToCK3SuccessionLawMap.TryAdd(mapping.ImperatorLaw, mapping.CK3SuccessionLaws)) {
+				impToCK3SuccessionLawMap[mapping.ImperatorLaw].UnionWith(mapping.CK3SuccessionLaws);
+			}
+		});
+		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+	}
+	public SortedSet<string> GetCK3LawsForImperatorLaws(SortedSet<string> impLaws) {
+		var lawsToReturn = new SortedSet<string>();
+		foreach (var impLaw in impLaws) {
+			if (impToCK3SuccessionLawMap.TryGetValue(impLaw, out var ck3Laws)) {
+				lawsToReturn.UnionWith(ck3Laws);
+			}
 		}
+		return lawsToReturn;
 	}
 }
