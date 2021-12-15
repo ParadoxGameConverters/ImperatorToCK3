0832c918cb4f6df5e6102a9622ce3e19d1aab006
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2021-12-15 11:55:21 +0100

    File-scoped namespaces for most outputters

--- ImperatorToCK3/Outputter/CharactersOutputter.cs
+++ ImperatorToCK3/Outputter/CharactersOutputter.cs
@@ -2,16 +2,15 @@
 using ImperatorToCK3.CK3.Characters;
 using System.IO;
 
-namespace ImperatorToCK3.Outputter {
-	public static class CharactersOutputter {
-		public static void OutputCharacters(string outputModName, CharacterCollection characters, Date conversionDate) {
-			// dumping all into one file
-			var path = $"output/{outputModName}/history/characters/fromImperator.txt";
-			using var stream = File.OpenWrite(path);
-			using var output = new StreamWriter(stream, System.Text.Encoding.UTF8);
-			foreach (var character in characters) {
-				CharacterOutputter.OutputCharacter(output, character, conversionDate);
-			}
+namespace ImperatorToCK3.Outputter;
+public static class CharactersOutputter {
+	public static void OutputCharacters(string outputModName, CharacterCollection characters, Date conversionDate) {
+		// dumping all into one file
+		var path = $"output/{outputModName}/history/characters/fromImperator.txt";
+		using var stream = File.OpenWrite(path);
+		using var output = new StreamWriter(stream, System.Text.Encoding.UTF8);
+		foreach (var character in characters) {
+			CharacterOutputter.OutputCharacter(output, character, conversionDate);
 		}
 	}
 }
--- ImperatorToCK3/Outputter/DynastiesOutputter.cs
+++ ImperatorToCK3/Outputter/DynastiesOutputter.cs
@@ -3,16 +3,15 @@ using ImperatorToCK3.CK3.Dynasties;
 using System.IO;
 using System.Text;
 
-namespace ImperatorToCK3.Outputter {
-	public static class DynastiesOutputter {
-		public static void OutputDynasties(string outputModName, DynastyCollection dynasties) {
-			var outputPath = Path.Combine("output", outputModName, "common/dynasties/imp_dynasties.txt");
+namespace ImperatorToCK3.Outputter;
+public static class DynastiesOutputter {
+	public static void OutputDynasties(string outputModName, DynastyCollection dynasties) {
+		var outputPath = Path.Combine("output", outputModName, "common/dynasties/imp_dynasties.txt");
 
-			using FileStream stream = File.OpenWrite(outputPath);
-			using var output = new StreamWriter(stream, encoding: Encoding.UTF8); // dumping all into one file
-			foreach (var dynasty in dynasties) {
-				output.WriteLine($"{dynasty.Id} = {PDXSerializer.Serialize(dynasty, string.Empty)}");
-			}
+		using FileStream stream = File.OpenWrite(outputPath);
+		using var output = new StreamWriter(stream, encoding: Encoding.UTF8); // dumping all into one file
+		foreach (var dynasty in dynasties) {
+			output.WriteLine($"{dynasty.Id} = {PDXSerializer.Serialize(dynasty, string.Empty)}");
 		}
 	}
 }
