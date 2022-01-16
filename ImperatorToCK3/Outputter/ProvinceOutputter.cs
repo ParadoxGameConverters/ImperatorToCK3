cb411edf50b644f7bbeace1f6ad25d1a21aa59ae
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-01-16 14:50:29 +0100

    ProvinceOutputterTests

--- ImperatorToCK3/Outputter/ProvinceOutputter.cs
+++ ImperatorToCK3/Outputter/ProvinceOutputter.cs
@@ -4,17 +4,17 @@ using System.IO;
 namespace ImperatorToCK3.Outputter;
 
 public static class ProvinceOutputter {
-	public static void OutputProvince(StreamWriter writer, Province province) {
-		writer.WriteLine($"{province.Id} = {{");
+	public static void OutputProvince(TextWriter writer, Province province) {
+		writer.WriteLine($"{province.Id}={{");
 		if (!string.IsNullOrEmpty(province.Culture)) {
-			writer.WriteLine($"\tculture = {province.Culture}");
+			writer.WriteLine($"\tculture={province.Culture}");
 		}
 		if (!string.IsNullOrEmpty(province.Religion)) {
-			writer.WriteLine($"\treligion = {province.Religion}");
+			writer.WriteLine($"\treligion={province.Religion}");
 		}
-		writer.WriteLine($"\tholding = {province.Holding}");
+		writer.WriteLine($"\tholding={province.Holding}");
 		if (province.Buildings.Count > 0) {
-			writer.WriteLine("\tbuildings = {");
+			writer.WriteLine("\tbuildings={");
 			foreach (var building in province.Buildings) {
 				writer.WriteLine($"\t\t{building}");
 			}
