abbbc181b9ec77fb7b1445821ec8a7b875234659
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-01-16 13:33:17 +0100

    SuccessionTriggersOutputterTests

--- ImperatorToCK3/Outputter/SuccessionTriggersOutputter.cs
+++ ImperatorToCK3/Outputter/SuccessionTriggersOutputter.cs
@@ -6,7 +6,7 @@ using System.IO;
 namespace ImperatorToCK3.Outputter;
 public static class SuccessionTriggersOutputter {
 	public static void OutputSuccessionTriggers(string outputModName, Title.LandedTitles landedTitles, Date ck3BookmarkDate) {
-		var outputPath = Path.Combine("output", outputModName, "common/scripted_triggers/IRToCK3_succession_triggers.txt");
+		var outputPath = Path.Combine("output", outputModName, "common", "scripted_triggers", "IRToCK3_succession_triggers.txt");
 
 		using var outputStream = File.OpenWrite(outputPath);
 		using var output = new StreamWriter(outputStream, System.Text.Encoding.UTF8);
@@ -15,30 +15,32 @@ public static class SuccessionTriggersOutputter {
 		var seniorityTitles = new List<string>();
 
 		foreach (var landedTitle in landedTitles) {
-			if (landedTitle.GetDeFactoLiege(ck3BookmarkDate) is null) {
-				if (landedTitle.SuccessionLaws.Contains("single_heir_succession_law")) {
-					primogenitureTitles.Add(landedTitle.Id);
-				}
-				if (landedTitle.SuccessionLaws.Contains("single_heir_dynasty_house")) {
-					seniorityTitles.Add(landedTitle.Id);
-				}
+			if (landedTitle.GetDeFactoLiege(ck3BookmarkDate) is not null) {
+				continue;
+			}
+
+			if (landedTitle.SuccessionLaws.Contains("single_heir_succession_law")) {
+				primogenitureTitles.Add(landedTitle.Id);
+			}
+			if (landedTitle.SuccessionLaws.Contains("single_heir_dynasty_house")) {
+				seniorityTitles.Add(landedTitle.Id);
 			}
 		}
 
-		output.WriteLine("historical_succession_access_single_heir_succession_law_trigger = {");
-		output.WriteLine(" OR = {");
+		output.WriteLine("historical_succession_access_single_heir_succession_law_trigger={");
+		output.WriteLine("\tOR={");
 		foreach (var primogenitureTitle in primogenitureTitles) {
-			output.WriteLine($"  has_title = title:{primogenitureTitle}");
+			output.WriteLine($"\t\thas_title=title:{primogenitureTitle}");
 		}
-		output.WriteLine(" }");
+		output.WriteLine("\t}");
 		output.WriteLine("}");
 
-		output.WriteLine("historical_succession_access_single_heir_dynasty_house_trigger = {");
-		output.WriteLine(" OR = {");
+		output.WriteLine("historical_succession_access_single_heir_dynasty_house_trigger={");
+		output.WriteLine("\tOR={");
 		foreach (var seniorityTitle in seniorityTitles) {
-			output.WriteLine($"  has_title = title:{seniorityTitle}");
+			output.WriteLine($"\t\thas_title=title:{seniorityTitle}");
 		}
-		output.WriteLine(" }");
+		output.WriteLine("\t}");
 		output.WriteLine("}");
 	}
 }
