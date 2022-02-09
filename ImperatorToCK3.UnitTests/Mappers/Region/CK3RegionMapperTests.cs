30feb6b9c40f9211f19a6eaa6a13c7b2ce421032
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-02-09 07:53:41 +0100

    Update CK3 region loading for CK3 1.5 (#577)
    * Update version.txt
    
    * Update region loading for CK3 1.5

--- ImperatorToCK3.UnitTests/Mappers/Region/CK3RegionMapperTests.cs
+++ ImperatorToCK3.UnitTests/Mappers/Region/CK3RegionMapperTests.cs
@@ -4,201 +4,200 @@ using ImperatorToCK3.Mappers.Region;
 using System.Collections.Generic;
 using Xunit;
 
-namespace ImperatorToCK3.UnitTests.Mappers.Region {
-	[Collection("Sequential")]
-	[CollectionDefinition("Sequential", DisableParallelization = true)]
-	public class CK3RegionMapperTests {
-		private const string islandRegionPath = "TestFiles/regions/island_regions.txt";
-		[Fact]
-		public void RegionMapperCanBeEnabled() {
-			// We start humble, it's a machine.
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/empty.txt";
+namespace ImperatorToCK3.UnitTests.Mappers.Region;
 
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
-			Assert.False(mapper.ProvinceIsInRegion(1, "test"));
-			Assert.False(mapper.RegionNameIsValid("test"));
-			Assert.Null(mapper.GetParentCountyName(1));
-			Assert.Null(mapper.GetParentDuchyName(1));
-			Assert.Null(mapper.GetParentRegionName(1));
-		}
-		[Fact]
-		public void LoadingBrokenRegionWillThrowException() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_anglia = { d_aquitane = { c_mers = { b_hgy = { province = 69 } } } }"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LoadingBrokenRegionWillThrowException.txt";
-			void action() => mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
-			KeyNotFoundException exception = Assert.Throws<KeyNotFoundException>(action);
-			Assert.Equal("Region's test_region2 region test_region does not exist!", exception.Message);
-		}
-		[Fact]
-		public void LoadingBrokenDuchyWillThrowException() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_anglia = { d_broken_aquitane = { c_mers = { b_hgy = { province = 69 } } } }"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LoadingBrokenDuchyWillThrowException.txt";
-			void action() => mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
-			KeyNotFoundException exception = Assert.Throws<KeyNotFoundException>(action);
-			Assert.Equal("Region's test_region duchy d_aquitane does not exist!", exception.Message);
-		}
-		[Fact]
-		public void LoadingBrokenCountyWillThrowException() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_anglia = { d_aquitane = { c_mers_broken = { b_hgy = { province = 69 } } } } \n"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LoadingBrokenCountyWillThrowException.txt";
-			void action() => mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
-			KeyNotFoundException exception = Assert.Throws<KeyNotFoundException>(action);
-			Assert.Equal("Region's test_region county c_mers does not exist!", exception.Message);
-		}
+[Collection("Sequential")]
+[CollectionDefinition("Sequential", DisableParallelization = true)]
+public class CK3RegionMapperTests {
+	[Fact]
+	public void RegionMapperCanBeEnabled() {
+		// We start humble, it's a machine.
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/empty";
 
-		[Fact]
-		public void LocationServicesWork() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"d_aquitane = { c_mers = { b_hgy = { province = 69 } } }"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LocationServicesWork.txt";
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+		mapper.LoadRegions(landedTitles, ck3Path);
+		Assert.False(mapper.ProvinceIsInRegion(1, "test"));
+		Assert.False(mapper.RegionNameIsValid("test"));
+		Assert.Null(mapper.GetParentCountyName(1));
+		Assert.Null(mapper.GetParentDuchyName(1));
+		Assert.Null(mapper.GetParentRegionName(1));
+	}
+	[Fact]
+	public void LoadingBrokenRegionWillThrowException() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_anglia = { d_aquitane = { c_mers = { b_hgy = { province = 69 } } } }"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LoadingBrokenRegionWillThrowException";
+		void action() => mapper.LoadRegions(landedTitles, ck3Path);
+		KeyNotFoundException exception = Assert.Throws<KeyNotFoundException>(action);
+		Assert.Equal("Region's test_region2 region test_region does not exist!", exception.Message);
+	}
+	[Fact]
+	public void LoadingBrokenDuchyWillThrowException() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_anglia = { d_broken_aquitane = { c_mers = { b_hgy = { province = 69 } } } }"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LoadingBrokenDuchyWillThrowException";
+		void action() => mapper.LoadRegions(landedTitles, ck3Path);
+		KeyNotFoundException exception = Assert.Throws<KeyNotFoundException>(action);
+		Assert.Equal("Region's test_region duchy d_aquitane does not exist!", exception.Message);
+	}
+	[Fact]
+	public void LoadingBrokenCountyWillThrowException() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_anglia = { d_aquitane = { c_mers_broken = { b_hgy = { province = 69 } } } } \n"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LoadingBrokenCountyWillThrowException";
+		void Action() => mapper.LoadRegions(landedTitles, ck3Path);
+		KeyNotFoundException exception = Assert.Throws<KeyNotFoundException>(Action);
+		Assert.Equal("Region's test_region county c_mers does not exist!", exception.Message);
+	}
 
-			Assert.True(mapper.ProvinceIsInRegion(69, "c_mers"));
-			Assert.True(mapper.ProvinceIsInRegion(69, "d_aquitane"));
-			Assert.True(mapper.ProvinceIsInRegion(69, "test_region"));
-			Assert.True(mapper.ProvinceIsInRegion(69, "test_region_bigger"));
-			Assert.True(mapper.ProvinceIsInRegion(69, "test_region_biggest"));
-		}
+	[Fact]
+	public void LocationServicesWork() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"d_aquitane = { c_mers = { b_hgy = { province = 69 } } }"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LocationServicesWork";
+		mapper.LoadRegions(landedTitles, ck3Path);
 
-		[Fact]
-		public void LocationServicesCorrectlyFail() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"d_testduchy = { 1 2 3 } \n" +
-				"d_testduchy2 = { 4 5 6 } "
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LocationServicesCorrectlyFail.txt";
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+		Assert.True(mapper.ProvinceIsInRegion(69, "c_mers"));
+		Assert.True(mapper.ProvinceIsInRegion(69, "d_aquitane"));
+		Assert.True(mapper.ProvinceIsInRegion(69, "test_region"));
+		Assert.True(mapper.ProvinceIsInRegion(69, "test_region_bigger"));
+		Assert.True(mapper.ProvinceIsInRegion(69, "test_region_biggest"));
+	}
 
-			Assert.False(mapper.ProvinceIsInRegion(4, "d_testduchy")); // province in different duchy
-			Assert.False(mapper.ProvinceIsInRegion(9, "d_testduchy")); // province missing completely
-			Assert.False(mapper.ProvinceIsInRegion(5, "test_region")); // province in different region
-		}
+	[Fact]
+	public void LocationServicesCorrectlyFail() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"d_testduchy = { 1 2 3 } \n" +
+			"d_testduchy2 = { 4 5 6 } "
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LocationServicesCorrectlyFail";
+		mapper.LoadRegions(landedTitles, ck3Path);
 
-		[Fact]
-		public void LocationServicesFailForNonsense() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } } } } \n" +
-				"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } }"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LocationServicesFailForNonsense.txt";
+		Assert.False(mapper.ProvinceIsInRegion(4, "d_testduchy")); // province in different duchy
+		Assert.False(mapper.ProvinceIsInRegion(9, "d_testduchy")); // province missing completely
+		Assert.False(mapper.ProvinceIsInRegion(5, "test_region")); // province in different region
+	}
 
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+	[Fact]
+	public void LocationServicesFailForNonsense() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } } } } \n" +
+			"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } }"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LocationServicesFailForNonsense";
 
-			Assert.False(mapper.ProvinceIsInRegion(1, "nonsense"));
-			Assert.False(mapper.ProvinceIsInRegion(6, "test_superregion"));
-		}
+		mapper.LoadRegions(landedTitles, ck3Path);
 
-		[Fact]
-		public void CorrectParentLocationsReported() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } } } } \n" +
-				"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } } \n"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/CorrectParentLocationsReported.txt";
+		Assert.False(mapper.ProvinceIsInRegion(1, "nonsense"));
+		Assert.False(mapper.ProvinceIsInRegion(6, "test_superregion"));
+	}
 
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+	[Fact]
+	public void CorrectParentLocationsReported() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } } } } \n" +
+			"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } } \n"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/CorrectParentLocationsReported";
 
-			Assert.Equal("c_athens", mapper.GetParentCountyName(79));
-			Assert.Equal("d_wakaba", mapper.GetParentDuchyName(79));
-			Assert.Equal("test_region", mapper.GetParentRegionName(79));
-			Assert.Equal("c_defff", mapper.GetParentCountyName(6));
-			Assert.Equal("d_hujhu", mapper.GetParentDuchyName(6));
-			Assert.Equal("test_region2", mapper.GetParentRegionName(6));
-		}
+		mapper.LoadRegions(landedTitles, ck3Path);
 
-		[Fact]
-		public void WrongParentLocationsReturnNull() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"d_testduchy = { 1 2 3 } \n" +
-				"d_testduchy2 = { 4 5 6 } "
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/WrongParentLocationsReturnNull.txt";
+		Assert.Equal("c_athens", mapper.GetParentCountyName(79));
+		Assert.Equal("d_wakaba", mapper.GetParentDuchyName(79));
+		Assert.Equal("test_region", mapper.GetParentRegionName(79));
+		Assert.Equal("c_defff", mapper.GetParentCountyName(6));
+		Assert.Equal("d_hujhu", mapper.GetParentDuchyName(6));
+		Assert.Equal("test_region2", mapper.GetParentRegionName(6));
+	}
 
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+	[Fact]
+	public void WrongParentLocationsReturnNull() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"d_testduchy = { 1 2 3 } \n" +
+			"d_testduchy2 = { 4 5 6 } "
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/WrongParentLocationsReturnNull";
 
-			Assert.Null(mapper.GetParentCountyName(7));
-			Assert.Null(mapper.GetParentDuchyName(7));
-			Assert.Null(mapper.GetParentRegionName(7));
-		}
+		mapper.LoadRegions(landedTitles, ck3Path);
 
-		[Fact]
-		public void LocationNameValidationWorks() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } } } } \n" +
-				"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } } \n" +
-				"c_county = { b_barony = { province = 69 } } \n"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LocationNameValidationWorks.txt";
+		Assert.Null(mapper.GetParentCountyName(7));
+		Assert.Null(mapper.GetParentDuchyName(7));
+		Assert.Null(mapper.GetParentRegionName(7));
+	}
 
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+	[Fact]
+	public void LocationNameValidationWorks() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } } } } \n" +
+			"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } } \n" +
+			"c_county = { b_barony = { province = 69 } } \n"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LocationNameValidationWorks";
 
-			Assert.True(mapper.RegionNameIsValid("d_wakaba"));
-			Assert.True(mapper.RegionNameIsValid("test_region2"));
-			Assert.True(mapper.RegionNameIsValid("test_region3"));
-			Assert.True(mapper.RegionNameIsValid("c_county"));
-			Assert.False(mapper.RegionNameIsValid("nonsense"));
-		}
+		mapper.LoadRegions(landedTitles, ck3Path);
 
-		[Fact]
-		public void LocationServicesSucceedsForProvinceField() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LocationServicesSucceedsForProvinceField.txt";
+		Assert.True(mapper.RegionNameIsValid("d_wakaba"));
+		Assert.True(mapper.RegionNameIsValid("test_region2"));
+		Assert.True(mapper.RegionNameIsValid("test_region3"));
+		Assert.True(mapper.RegionNameIsValid("c_county"));
+		Assert.False(mapper.RegionNameIsValid("nonsense"));
+	}
 
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+	[Fact]
+	public void LocationServicesSucceedsForProvinceField() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LocationServicesSucceedsForProvinceField";
 
-			Assert.True(mapper.ProvinceIsInRegion(69, "test_region"));
-		}
+		mapper.LoadRegions(landedTitles, ck3Path);
 
-		[Fact]
-		public void LocationServicesSucceedsForCountyField() {
-			var mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } }"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			const string regionPath = "TestFiles/regions/CK3RegionMapperTests/LocationServicesSucceedsForCountyField.txt";
+		Assert.True(mapper.ProvinceIsInRegion(69, "test_region"));
+	}
 
-			mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+	[Fact]
+	public void LocationServicesSucceedsForCountyField() {
+		var mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"c_athens = { b_athens = { province = 79 } b_newbarony = { province = 56 } }"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		const string ck3Path = "TestFiles/regions/CK3RegionMapperTests/LocationServicesSucceedsForCountyField";
 
-			Assert.True(mapper.ProvinceIsInRegion(79, "test_region"));
-		}
+		mapper.LoadRegions(landedTitles, ck3Path);
+
+		Assert.True(mapper.ProvinceIsInRegion(79, "test_region"));
 	}
 }
--- ImperatorToCK3.UnitTests/Mappers/Religion/ReligionMappingTests.cs
+++ ImperatorToCK3.UnitTests/Mappers/Religion/ReligionMappingTests.cs
@@ -4,131 +4,130 @@ using ImperatorToCK3.Mappers.Region;
 using ImperatorToCK3.Mappers.Religion;
 using Xunit;
 
-namespace ImperatorToCK3.UnitTests.Mappers.Religion {
-	[Collection("Sequential")]
-	[CollectionDefinition("Sequential", DisableParallelization = true)]
-	public class ReligionMappingTests {
-		private const string regionPath = "TestFiles/regions/regions.txt";
-		private const string islandRegionPath = "TestFiles/regions/island_regions.txt";
+namespace ImperatorToCK3.UnitTests.Mappers.Religion;
 
-		[Fact]
-		public void RegularMatchOnSimpleReligion() {
-			var reader = new BufferedReader("ck3 = flemish imp = dutch");
-			var mapping = ReligionMapping.Parse(reader);
+[Collection("Sequential")]
+[CollectionDefinition("Sequential", DisableParallelization = true)]
+public class ReligionMappingTests {
+	private const string ck3Path = "TestFiles/regions/ReligionMappingTests";
 
-			mapping.CK3RegionMapper = new CK3RegionMapper();
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void RegularMatchOnSimpleReligion() {
+		var reader = new BufferedReader("ck3 = flemish imp = dutch");
+		var mapping = ReligionMapping.Parse(reader);
 
-			Assert.Equal("flemish", mapping.Match("dutch", 0, 0));
-		}
+		mapping.CK3RegionMapper = new CK3RegionMapper();
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
 
-		[Fact]
-		public void MatchOnProvince() {
-			var reader = new BufferedReader("ck3 = dutch imp = german ck3Province = 17");
-			var mapping = ReligionMapping.Parse(reader);
+		Assert.Equal("flemish", mapping.Match("dutch", 0, 0));
+	}
 
-			mapping.CK3RegionMapper = new CK3RegionMapper();
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void MatchOnProvince() {
+		var reader = new BufferedReader("ck3 = dutch imp = german ck3Province = 17");
+		var mapping = ReligionMapping.Parse(reader);
 
-			Assert.Equal("dutch", mapping.Match("german", 17, 0));
-		}
+		mapping.CK3RegionMapper = new CK3RegionMapper();
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
 
-		[Fact]
-		public void MatchOnProvinceFailsForWrongProvince() {
-			var reader = new BufferedReader("ck3 = dutch imp = german ck3Province = 17");
-			var mapping = ReligionMapping.Parse(reader);
+		Assert.Equal("dutch", mapping.Match("german", 17, 0));
+	}
 
-			mapping.CK3RegionMapper = new CK3RegionMapper();
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void MatchOnProvinceFailsForWrongProvince() {
+		var reader = new BufferedReader("ck3 = dutch imp = german ck3Province = 17");
+		var mapping = ReligionMapping.Parse(reader);
 
-			Assert.Null(mapping.Match("german", 19, 0));
-		}
+		mapping.CK3RegionMapper = new CK3RegionMapper();
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
 
-		[Fact]
-		public void MatchOnProvinceFailsForNoProvince() {
-			var reader = new BufferedReader("ck3 = dutch imp = german ck3Province = 17");
-			var mapping = ReligionMapping.Parse(reader);
+		Assert.Null(mapping.Match("german", 19, 0));
+	}
 
-			mapping.CK3RegionMapper = new CK3RegionMapper();
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void MatchOnProvinceFailsForNoProvince() {
+		var reader = new BufferedReader("ck3 = dutch imp = german ck3Province = 17");
+		var mapping = ReligionMapping.Parse(reader);
 
-			Assert.Null(mapping.Match("german", 0, 0));
-		}
+		mapping.CK3RegionMapper = new CK3RegionMapper();
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
 
-		[Fact]
-		public void MatchOnRegion() {
-			var theMapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } } } } \n" +
-				"k_ghef = { d_hujhu = { c_defff = { b_newbarony2 = { province = 4 } } } } \n"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			theMapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+		Assert.Null(mapping.Match("german", 0, 0));
+	}
 
-			var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = test_region1");
-			var mapping = ReligionMapping.Parse(reader);
-			mapping.CK3RegionMapper = theMapper;
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void MatchOnRegion() {
+		var theMapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_ugada = { d_wakaba = { c_athens = { b_athens = { province = 79 } } } } \n" +
+			"k_ghef = { d_hujhu = { c_defff = { b_newbarony2 = { province = 4 } } } } \n"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		theMapper.LoadRegions(landedTitles, ck3Path);
 
-			Assert.Equal("dutch", mapping.Match("german", 4, 0));
-		}
+		var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = test_region1");
+		var mapping = ReligionMapping.Parse(reader);
+		mapping.CK3RegionMapper = theMapper;
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
 
-		[Fact]
-		public void MatchOnRegionFailsForWrongRegion() {
-			var theMapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_ugada = { d_wakaba = { } } \n" +
-				"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } } } } \n"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			theMapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+		Assert.Equal("dutch", mapping.Match("german", 4, 0));
+	}
 
-			var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = test_region1");
-			var mapping = ReligionMapping.Parse(reader);
-			mapping.CK3RegionMapper = theMapper;
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void MatchOnRegionFailsForWrongRegion() {
+		var theMapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_ugada = { d_wakaba = { } } \n" +
+			"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } } } } \n"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		theMapper.LoadRegions(landedTitles, ck3Path);
 
-			Assert.Null(mapping.Match("german", 79, 0));
-		}
+		var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = test_region1");
+		var mapping = ReligionMapping.Parse(reader);
+		mapping.CK3RegionMapper = theMapper;
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
 
-		[Fact]
-		public void MatchOnRegionFailsForNoRegion() {
-			var ck3Mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_ugada = { d_wakaba = { } } \n" +
-				"k_ghef = { d_hujhu = { } } \n"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			ck3Mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+		Assert.Null(mapping.Match("german", 79, 0));
+	}
 
-			var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = test_region3");
-			var mapping = ReligionMapping.Parse(reader);
-			mapping.CK3RegionMapper = ck3Mapper;
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void MatchOnRegionFailsForNoRegion() {
+		var ck3Mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_ugada = { d_wakaba = { } } \n" +
+			"k_ghef = { d_hujhu = { } } \n"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		ck3Mapper.LoadRegions(landedTitles, ck3Path);
 
-			Assert.Null(mapping.Match("german", 17, 0));
-		}
+		var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = test_region3");
+		var mapping = ReligionMapping.Parse(reader);
+		mapping.CK3RegionMapper = ck3Mapper;
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
 
-		[Fact]
-		public void MatchOnRegionFailsForNoProvince() {
-			var ck3Mapper = new CK3RegionMapper();
-			var landedTitles = new Title.LandedTitles();
-			var landedTitlesReader = new BufferedReader(
-				"k_ugada = { d_wakaba = { } } \n" +
-				"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } } \n"
-			);
-			landedTitles.LoadTitles(landedTitlesReader);
-			ck3Mapper.LoadRegions(landedTitles, regionPath, islandRegionPath);
+		Assert.Null(mapping.Match("german", 17, 0));
+	}
 
-			var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = d_hujhu");
-			var mapping = ReligionMapping.Parse(reader);
-			mapping.CK3RegionMapper = ck3Mapper;
-			mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+	[Fact]
+	public void MatchOnRegionFailsForNoProvince() {
+		var ck3Mapper = new CK3RegionMapper();
+		var landedTitles = new Title.LandedTitles();
+		var landedTitlesReader = new BufferedReader(
+			"k_ugada = { d_wakaba = { } } \n" +
+			"k_ghef = { d_hujhu = { c_defff = { b_cringe = { province = 6 } b_newbarony2 = { province = 4 } } } } \n"
+		);
+		landedTitles.LoadTitles(landedTitlesReader);
+		ck3Mapper.LoadRegions(landedTitles, ck3Path);
 
-			Assert.Null(mapping.Match("german", 0, 0));
-		}
+		var reader = new BufferedReader("ck3 = dutch imp = german ck3Region = d_hujhu");
+		var mapping = ReligionMapping.Parse(reader);
+		mapping.CK3RegionMapper = ck3Mapper;
+		mapping.ImperatorRegionMapper = new ImperatorRegionMapper();
+
+		Assert.Null(mapping.Match("german", 0, 0));
 	}
 }
