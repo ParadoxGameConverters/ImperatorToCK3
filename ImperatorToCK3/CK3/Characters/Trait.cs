32d1dc31bf930cb79c810dac6e3694d8e53ce6e6
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-04-04 23:22:42 +0200

    Initial Trait class

---
+++ ImperatorToCK3/CK3/Characters/Trait.cs
@@ -0,0 +1,22 @@
+﻿using commonItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImperatorToCK3.CK3.Characters {
+	internal class Trait {
+		public string Id { get; }
+		public ISet<string> Opposites = new HashSet<string>();
+
+		public Trait(string id, BufferedReader traitReader) {
+			Id = id;
+
+			var parser = new Parser();
+			parser.RegisterKeyword("opposites", reader => Opposites = reader.GetStrings().ToHashSet());
+			parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreItem);
+			parser.ParseStream(traitReader);
+		}
+	}
+}
