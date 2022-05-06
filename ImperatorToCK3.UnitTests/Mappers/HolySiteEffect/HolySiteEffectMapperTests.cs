9b1b9cae544975a530f10a8a980de763a9f05647
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-05-06 23:09:59 +0200

    HolySiteEffectMapperTests

--- ImperatorToCK3.UnitTests/Mappers/HolySiteEffect/HolySiteEffectMapperTests.cs
+++ ImperatorToCK3.UnitTests/Mappers/HolySiteEffect/HolySiteEffectMapperTests.cs
@@ -7,7 +7,7 @@ namespace ImperatorToCK3.UnitTests.Mappers.HolySiteEffect;
 public class HolySiteEffectMapperTests {
 	[Theory, MemberData(nameof(TestData))]
 	public void MapperReturnsCorrectValues(string imperatorEffect, double imperatorValue, KeyValuePair<string, double>? match) {
-		const string mappingsFilePath = "TestFiles/UnitTypeMapperTests/mappings.txt";
+		const string mappingsFilePath = "TestFiles/HolySiteEffectMapperTests/mappings.txt";
 		var mapper = new HolySiteEffectMapper(mappingsFilePath);
 		Assert.Equal(match, mapper.Match(imperatorEffect, imperatorValue));
 	}
@@ -15,7 +15,8 @@ public class HolySiteEffectMapperTests {
 	public static IEnumerable<object[]> TestData => 
 		new List<object[]> {
 			new object[] { "wrong_effect", 1, null },
-			new object[] { "effect_to_double", 2, new KeyValuePair<string, double>("doubled_effect", 4) },
-			new object[] { "effect_to_divide", 4, new KeyValuePair<string, double>("divided_effect", 2) }
+			new object[] { "effect_to_multiply", 2, new KeyValuePair<string, double>("doubled_effect", 4) },
+			new object[] { "effect_to_divide", 4, new KeyValuePair<string, double>("divided_effect", 2) },
+			new object[] { "effect_to_nullify", 1, new KeyValuePair<string, double>("nullified_effect", 0)}
 		};
 }
