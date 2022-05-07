b0b4db03eb20c4211dffad92209e09fd7fa3a645
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-05-07 18:33:39 +0200

    FaithTests

---
+++ ImperatorToCK3.UnitTests/CK3/Religions/FaithTests.cs
@@ -0,0 +1,18 @@
+using commonItems;
+using ImperatorToCK3.CK3.Religions;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.CK3.Religions; 
+
+public class FaithTests {
+	[Fact]
+	public void HolySitesAreLoaded() {
+		var reader = new BufferedReader("{ holy_site=rome holy_site=constantinople holy_site=antioch }");
+		var faith = new Faith("chalcedonian", reader);
+		
+		Assert.Collection(faith.HolySites,
+			site=>Assert.Equal("rome", site),
+			site=>Assert.Equal("constantinople", site),
+			site=>Assert.Equal("antioch", site));
+	}
+}
