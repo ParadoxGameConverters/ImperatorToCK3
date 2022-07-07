b27f9c6f0a85ccf6c0286e6dfbc656121c995551
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-07 20:31:13 +0200

    Output holy site localization

--- ImperatorToCK3/Outputter/ReligionsOutputter.cs
+++ ImperatorToCK3/Outputter/ReligionsOutputter.cs
@@ -11,9 +11,58 @@ public static class ReligionsOutputter {
 
 		using var outputStream = File.OpenWrite(outputPath);
 		using var output = new StreamWriter(outputStream, System.Text.Encoding.UTF8);
+		
+		using var englishLocStream = File.OpenWrite(Path.Combine("output", outputModName, "localization/english/IRtoCK3_holy_sites_l_english.yml"));
+		using var frenchLocStream = File.OpenWrite(Path.Combine("output", outputModName, "localization/french/IRtoCK3_holy_sites_l_french.yml"));
+		using var germanLocStream = File.OpenWrite(Path.Combine("output", outputModName, "localization/german/IRtoCK3_holy_sites_l_german.yml"));
+		using var russianLocStream = File.OpenWrite(Path.Combine("output", outputModName, "localization/russian/IRtoCK3_holy_sites_l_russian.yml"));
+		using var simpChineseLocStream = File.OpenWrite(Path.Combine("output", outputModName, "localization/simp_chinese/IRtoCK3_holy_sites_l_simp_chinese.yml"));
+		using var spanishLocStream = File.OpenWrite(Path.Combine("output", outputModName, "localization/spanish/IRtoCK3_holy_sites_l_spanish.yml"));
+		using var englishLocWriter = new StreamWriter(englishLocStream, System.Text.Encoding.UTF8);
+		using var frenchLocWriter = new StreamWriter(frenchLocStream, System.Text.Encoding.UTF8);
+		using var germanLocWriter = new StreamWriter(germanLocStream, System.Text.Encoding.UTF8);
+		using var russianLocWriter = new StreamWriter(russianLocStream, System.Text.Encoding.UTF8);
+		using var simpChineseLocWriter = new StreamWriter(simpChineseLocStream, System.Text.Encoding.UTF8);
+		using var spanishLocWriter = new StreamWriter(spanishLocStream, System.Text.Encoding.UTF8);
 
-		foreach (var site in ck3ReligionCollection.HolySites.Where(s=>s.IsGeneratedByConverter)) {
+		englishLocWriter.WriteLine("l_english:");
+		frenchLocWriter.WriteLine("l_french:");
+		germanLocWriter.WriteLine("l_german:");
+		russianLocWriter.WriteLine("l_russian:");
+		simpChineseLocWriter.WriteLine("l_simp_chinese:");
+		spanishLocWriter.WriteLine("l_spanish:");
+
+		var sitesToOutput = ck3ReligionCollection.HolySites.Where(s => s.IsGeneratedByConverter)
+			.ToList();
+
+		foreach (var site in sitesToOutput) {
 			output.WriteLine($"{site.Id}={PDXSerializer.Serialize(site)}");
+		}
+		
+		// Output localization
+		foreach (var site in sitesToOutput) {
+			// holy site name
+			var holySiteTitle = site.BaronyId ?? site.CountyId;
+			if (holySiteTitle is not null) {
+				string holySiteNameLocLine = $" holy_site_{site.Id}_name: \"${holySiteTitle}$\"";
+				englishLocWriter.WriteLine(holySiteNameLocLine);
+				frenchLocWriter.WriteLine(holySiteNameLocLine);
+				germanLocWriter.WriteLine(holySiteNameLocLine);
+				russianLocWriter.WriteLine(holySiteNameLocLine);
+				simpChineseLocWriter.WriteLine(holySiteNameLocLine);
+				spanishLocWriter.WriteLine(holySiteNameLocLine);
+			} else {
+				englishLocWriter.WriteLine($" holy_site_{site.Id}_name: \"Holy site\""); // fallback
+			}
+			
+			// holy site effect name
+			string holySiteEffectLocLine = $" holy_site_{site.Id}_effect_name: \"From [holy_site|E] #weak ($holy_site_{site.Id}_name$)#!\"";
+			englishLocWriter.WriteLine(holySiteEffectLocLine);
+			frenchLocWriter.WriteLine(holySiteEffectLocLine);
+			germanLocWriter.WriteLine(holySiteEffectLocLine);
+			russianLocWriter.WriteLine(holySiteEffectLocLine);
+			simpChineseLocWriter.WriteLine(holySiteEffectLocLine);
+			spanishLocWriter.WriteLine(holySiteEffectLocLine);
 		}
 	}
 }
