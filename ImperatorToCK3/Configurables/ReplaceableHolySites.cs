5beb07b6cc38a3945acb3b4da639bf6ce32b3265
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-16 18:05:00 +0200

    Delete ReplaceableHolySites.cs

--- ImperatorToCK3/Configurables/ReplaceableHolySites.cs
+++
@@ -1,9 +0,0 @@
-using commonItems;
-using commonItems.Collections;
-using ImperatorToCK3.CK3.Religions;
-using System.Collections.Generic;
-
-namespace ImperatorToCK3.Configurables; 
-
-public class ReplaceableHolySites {
-}
