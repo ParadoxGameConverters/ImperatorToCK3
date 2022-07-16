f43d275b0b518515cfcea4da18c655e77c234a9d
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-16 14:36:18 +0200

    ReligionAttributesAreReadAndSerialized test

--- ImperatorToCK3.UnitTests/CK3/Religions/ReligionTests.cs
+++ ImperatorToCK3.UnitTests/CK3/Religions/ReligionTests.cs
@@ -1,4 +1,6 @@
 using commonItems;
+using commonItems.Serialization;
+using FluentAssertions;
 using ImperatorToCK3.CK3.Religions;
 using Xunit;
 
@@ -13,5 +15,22 @@ public class ReligionTests {
 		Assert.Collection(religion.Faiths,
 			faith=>Assert.Equal("orthodox", faith.Id),
 			faith=>Assert.Equal("catholic", faith.Id));
+	}
+
+	[Fact]
+	public void ReligionAttributesAreReadAndSerialized() {
+		var reader = new BufferedReader(@"{
+			pagan_roots = yes
+			doctrine = doctrine_no_head
+			doctrine = doctrine_gender_male_dominated # should not replace the line above
+		}");
+		var religion = new Religion("celtic_religion", reader);
+
+		var religionStr = PDXSerializer.Serialize(religion);
+		religionStr.Should().ContainAll(
+			"pagan_roots=yes",
+			"doctrine=doctrine_no_head",
+			"doctrine=doctrine_gender_male_dominated"
+		);
 	}
 }
