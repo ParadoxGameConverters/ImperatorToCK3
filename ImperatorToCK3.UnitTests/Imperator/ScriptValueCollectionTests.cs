a0195b50d6e562f63b67463046c877f26c2535a3
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-17 00:06:21 +0200

    tests for GetModifierValue

--- ImperatorToCK3.UnitTests/Imperator/ScriptValueCollectionTests.cs
+++ ImperatorToCK3.UnitTests/Imperator/ScriptValueCollectionTests.cs
@@ -1,6 +1,7 @@
 using commonItems.Mods;
 using FluentAssertions;
 using ImperatorToCK3.Imperator;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -26,5 +27,38 @@ public class ScriptValueCollectionTests {
 		Assert.Equal(1f, scriptValueCollection["value3"]);
 		Assert.Equal(-3f, scriptValueCollection["value4"]);
 		Assert.Equal(3.2f, scriptValueCollection["mod_value"]);
+	}
+
+	[Fact]
+	public void GetModifierValueReturnsNumberForValidNumberString() {
+		var scriptValueCollection = new ScriptValueCollection();
+		
+		Assert.Equal(5.5f, scriptValueCollection.GetModifierValue("5.5"));
+	}
+
+	[Fact]
+	public void GetModifierValueReturnsDefaultValueForInvalidNumberString() {
+		var scriptValueCollection = new ScriptValueCollection();
+		const string invalidNumberString = "2e";
+		
+		// default value is 1
+		Assert.Equal(1f, scriptValueCollection.GetModifierValue(invalidNumberString));
+	}
+
+	[Fact]
+	public void GetModifierValueReturnsNumberForExistingScriptValue() {
+		var scriptValueCollection = new ScriptValueCollection();
+		scriptValueCollection.LoadScriptValues(imperatorModFS);
+		
+		Assert.Equal(0.4f, scriptValueCollection.GetModifierValue("value1"));
+	}
+
+	[Fact]
+	public void GetModifierValueReturnsDefaultValueForUndefinedScriptValue() {
+		var scriptValueCollection = new ScriptValueCollection();
+		scriptValueCollection.LoadScriptValues(imperatorModFS);
+		
+		// default value is 1
+		Assert.Equal(1f, scriptValueCollection.GetModifierValue("undefined_value"));
 	}
 }
