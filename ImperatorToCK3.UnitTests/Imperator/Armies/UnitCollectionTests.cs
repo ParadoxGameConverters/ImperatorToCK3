ef9248681ce3d6835852293e9edb8b3ad696cd53
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-30 15:14:49 +0200

    Unit tests for Subunit and UnitCollection

---
+++ ImperatorToCK3.UnitTests/Imperator/Armies/UnitCollectionTests.cs
@@ -0,0 +1,21 @@
+using commonItems;
+using FluentAssertions;
+using ImperatorToCK3.Imperator.Armies;
+using System.Linq;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.Imperator.Armies; 
+
+public class UnitCollectionTests {
+	[Fact]
+	public void SubunitsCanBeLoaded() {
+		var unitCollection = new UnitCollection();
+
+		var reader = new BufferedReader(@"1={} 2={} 3=none 1040187400={}");
+		unitCollection.LoadSubunits(reader);
+
+		unitCollection.Subunits
+			.Select(unit => unit.Id)
+			.Should().BeEquivalentTo(new ulong[] {1, 2, 1040187400});
+	}
+}
