927f94b7afcb34a8399c221f7af811fcc821f256
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-07-30 16:22:57 +0200

    Update commonItems

--- ImperatorToCK3/Imperator/Armies/Unit.cs
+++ ImperatorToCK3/Imperator/Armies/Unit.cs
@@ -64,7 +64,7 @@ public class Unit : IIdentifiable<ulong> {
 		if (baseNameLocBlock is not null) {
 			nameLocBlock.ModifyForEveryLanguage(baseNameLocBlock, (loc, baseLoc, language) => loc?.Replace("$BASE$", baseLoc));	
 		}
-		nameLocBlock.ModifyForEveryLanguage((loc, language) => loc?.Replace("$ROMAN$", CommonFunctions.CardinalToRoman(ordinal)));
+		nameLocBlock.ModifyForEveryLanguage((loc, language) => loc?.Replace("$ROMAN$", ordinal.ToRomanNumeral()));
 		nameLocBlock.ModifyForEveryLanguage((loc, language) => loc?.Replace("$NUM$", ordinal.ToString()));
 		nameLocBlock.ModifyForEveryLanguage((loc, language) => loc?.Replace("$ORDER$", ordinal.ToOrdinalSuffix(language)));
 
