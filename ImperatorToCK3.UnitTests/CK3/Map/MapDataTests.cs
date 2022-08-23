d7e4bf9787cf00ec4267740268174e1bf1da3137
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-08-23 19:56:29 +0200

    Improve CK3 map data mod support (#902)

--- ImperatorToCK3.UnitTests/CK3/Map/MapDataTests.cs
+++ ImperatorToCK3.UnitTests/CK3/Map/MapDataTests.cs
@@ -1,48 +1,53 @@
-﻿using ImperatorToCK3.CK3.Map;
+﻿using commonItems.Mods;
+using ImperatorToCK3.CK3.Map;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using Xunit;
 
-namespace ImperatorToCK3.UnitTests.CK3.Map {
-	[Collection("MapTests")]
-	[CollectionDefinition("MapTests", DisableParallelization = true)]
-	public class MapDataTests {
-		private const string testCK3Path = "TestFiles/MapData/CK3_1_province_map";
-		[Fact]
-		public void NeighborsDictDefaultsToEmpty() {
-			var data = new MapData(testCK3Path);
+namespace ImperatorToCK3.UnitTests.CK3.Map;
 
-			Assert.Empty(data.NeighborsDict);
-		}
-		[Fact]
-		public void NeighborProvincesCanBeDetermined() {
-			const string testCK3Path2 = "TestFiles/MapData/CK3_all_prov_defs";
-			const ulong byzantionId = 496;
+[Collection("MapTests")]
+[CollectionDefinition("MapTests", DisableParallelization = true)]
+public class MapDataTests {
+	[Fact]
+	public void NeighborsDictDefaultsToEmpty() {
+		const string ck3Root = "TestFiles/MapData/CK3_1_province_map/game";
+		var ck3ModFS = new ModFilesystem(ck3Root, new List<Mod>());
+		var data = new MapData(ck3ModFS);
 
-			var data = new MapData(testCK3Path2);
-			Assert.True(data.ProvinceDefinitions.ProvinceToColorDict.ContainsKey(byzantionId));
-			Assert.True(data.NeighborsDict.ContainsKey(byzantionId));
+		Assert.Empty(data.NeighborsDict);
+	}
 
-			var byzantionNeighborProvs = data.NeighborsDict[byzantionId];
-			var expectedByzantionNeighborProvs = new HashSet<ulong> {
-				3761, // Selymbria
-				8668, // sea_bosporus
-				947 // sea_marmara
-				// There is also a dark grey circle in the middle of Byzantion,
-				// but the color is not found in definition.csv, so there's no ID to add to neighbors.
-			};
-			Assert.True(byzantionNeighborProvs.SetEquals(expectedByzantionNeighborProvs));
-		}
+	[Fact]
+	public void NeighborProvincesCanBeDetermined() {
+		const string ck3Root = "TestFiles/MapData/CK3_all_prov_defs/game";
+		const ulong byzantionId = 496;
 
-		[Fact]
-		public void ProvinceNotFoundForColorIsLogged() {
-			var output = new StringWriter();
-			Console.SetOut(output);
+		var ck3ModFS = new ModFilesystem(ck3Root, new List<Mod>());
+		var data = new MapData(ck3ModFS);
+		Assert.True(data.ProvinceDefinitions.ProvinceToColorDict.ContainsKey(byzantionId));
+		Assert.True(data.NeighborsDict.ContainsKey(byzantionId));
 
-			const string testCK3Path2 = "TestFiles/MapData/CK3_all_prov_defs";
-			_ = new MapData(testCK3Path2);
-			Assert.Contains("Province not found for color Rgb24(30, 30, 30)", output.ToString());
-		}
+		var byzantionNeighborProvs = data.NeighborsDict[byzantionId];
+		var expectedByzantionNeighborProvs = new HashSet<ulong> {
+			3761, // Selymbria
+			8668, // sea_bosporus
+			947 // sea_marmara
+			// There is also a dark grey circle in the middle of Byzantion,
+			// but the color is not found in definition.csv, so there's no ID to add to neighbors.
+		};
+		Assert.True(byzantionNeighborProvs.SetEquals(expectedByzantionNeighborProvs));
+	}
+
+	[Fact]
+	public void ProvinceNotFoundForColorIsLogged() {
+		var output = new StringWriter();
+		Console.SetOut(output);
+
+		const string ck3Root = "TestFiles/MapData/CK3_all_prov_defs/game";
+		var ck3ModFS = new ModFilesystem(ck3Root, new List<Mod>());
+		_ = new MapData(ck3ModFS);
+		Assert.Contains("Province not found for color Rgb24(30, 30, 30)", output.ToString());
 	}
 }
