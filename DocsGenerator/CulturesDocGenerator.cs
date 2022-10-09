cb37e6550f0c628e22549b169d7cec26220aaaba
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-10-09 16:55:53 +0200

    GetLocForKey

--- DocsGenerator/CulturesDocGenerator.cs
+++ DocsGenerator/CulturesDocGenerator.cs
@@ -1,5 +1,6 @@
 using commonItems;
 using commonItems.Collections;
+using commonItems.Localization;
 using ImperatorToCK3.CK3.Cultures;
 
 namespace DocsGenerator;
@@ -24,7 +25,22 @@ public static class CulturesDocGenerator {
 		return cultures;
 	}
 
-	private static void OutputCulturesTable(IEnumerable<Culture> cultures) {
+	private static string GetLocForKey(LocDB locDB, string locKey) {
+		var locBlock = locDB.GetLocBlockForKey(locKey);
+		if (locBlock is null) {
+			Console.Error.WriteLine($"No localization found for \"{locKey}\"");
+			return locKey;
+		}
+
+		var englishLoc = locBlock["english"];
+		if (englishLoc is null) {
+			Console.Error.WriteLine($"No English localization found for \"{locKey}\"");
+			return locKey;
+		}
+		return englishLoc;
+	}
+
+	private static void OutputCulturesTable(IEnumerable<Culture> cultures, LocDB locDB) {
 		using var stream = File.OpenWrite("cultures_table.html");
 		using var output = new StreamWriter(stream, System.Text.Encoding.UTF8);
 
@@ -53,9 +69,18 @@ public static class CulturesDocGenerator {
 				</tr>
 			</thead>
 		""");
+		output.WriteLine("\t\t\t<tbody>");
 		foreach (var culture in cultures) {
-			
+			output.WriteLine("\t\t\t\t<tr>");
+			output.WriteLine($"\t\t\t\t<td>{GetLocForKey(locDB, culture.Id)}</td>");
+			output.WriteLine($"\t\t\t\t<td>{GetLocForKey(locDB, culture.HeritageId)}</td>");
+			output.WriteLine($"\t\t\t\t<td>{GetLocForKey(locDB, culture.EthosId)}</td>");
+			output.WriteLine($"\t\t\t\t<td>{string.Join("<br>", culture.Traditions.Select(t=>GetLocForKey(locDB, t)))}</td>");
+			output.WriteLine($"\t\t\t\t<td>{GetLocForKey(locDB, culture.LanguageId)}</td>");
+			output.WriteLine($"\t\t\t\t<td>{GetLocForKey(locDB, culture.MartialCustomId)}</td>");
+			output.WriteLine("\t\t\t\t</tr>");
 		}
+		output.WriteLine("\t\t\t</tbody>");
 		output.WriteLine("\t\t</table>");
 		output.WriteLine("\t</body>");
 		output.WriteLine("</html>");
