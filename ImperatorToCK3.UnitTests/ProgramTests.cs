3dcfe11387d58209921825facd0ace4c898e9e5b
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-11-02 19:50:21 +0100

    Add tests for Program class

---
+++ ImperatorToCK3.UnitTests/ProgramTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+using System.IO;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests; 
+
+public class ProgramTests {
+	[Fact]
+	public void CultureIsSetToInvariantCulture() {
+		Program.Main(Array.Empty<string>());
+		
+		Assert.Equal(CultureInfo.InvariantCulture, CultureInfo.CurrentCulture);
+		Assert.Equal(CultureInfo.InvariantCulture, CultureInfo.CurrentUICulture);
+		Assert.Equal(CultureInfo.InvariantCulture, CultureInfo.DefaultThreadCurrentCulture);
+		Assert.Equal(CultureInfo.InvariantCulture, CultureInfo.DefaultThreadCurrentUICulture);
+	}
+
+	[Fact]
+	public void WarningIsLoggedWhenParametersArePassed() {
+		var output = new StringWriter();
+		Console.SetOut(output);
+
+		Program.Main(new[] {"--debug"});
+
+		var outStr = output.ToString();
+		Assert.Contains("[WARN] ImperatorToCK3 takes no parameters.\n" +
+		                "It uses configuration.txt, configured manually or by the frontend.", outStr);
+	}
+}
