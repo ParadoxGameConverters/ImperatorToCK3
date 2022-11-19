d0da75132d287b8970fc52cc19fa620681dd6bf8
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-11-19 13:04:48 +0100

    Fix exception when loading holy sites from mods

--- ImperatorToCK3/CK3/Religions/ReligionCollection.cs
+++ ImperatorToCK3/CK3/Religions/ReligionCollection.cs
@@ -41,7 +41,7 @@ public class ReligionCollection : IdObjectCollection<string, Religion> {
 
 	private void RegisterHolySitesKeywords(Parser parser) {
 		parser.RegisterRegex(CommonRegexes.String, (holySiteReader, holySiteId) => {
-			HolySites.Add(new HolySite(holySiteId, holySiteReader));
+			HolySites.AddOrReplace(new HolySite(holySiteId, holySiteReader));
 		});
 		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
 	}
