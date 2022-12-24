0cf79c244f4562b7caf57ffdec8e407a3cd56f0f
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2022-12-24 13:00:16 +0100

    Dynasty.SetCultureFromImperator

--- ImperatorToCK3/CK3/Dynasties/Dynasty.cs
+++ ImperatorToCK3/CK3/Dynasties/Dynasty.cs
@@ -5,6 +5,7 @@ using commonItems.Serialization;
 using ImperatorToCK3.Imperator.Characters;
 using ImperatorToCK3.Imperator.Cultures;
 using ImperatorToCK3.Imperator.Families;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ImperatorToCK3.CK3.Dynasties;
@@ -13,18 +14,13 @@ public class Dynasty : IPDXSerializable, IIdentifiable<string> {
 	public Dynasty(Family imperatorFamily, CharacterCollection imperatorCharacters, CulturesDB irCulturesDB, LocDB locDB) {
 		Id = $"dynn_irtock3_{imperatorFamily.Id}";
 		Name = Id;
-
+		
 		var imperatorMemberIds = imperatorFamily.MemberIds;
-		var imperatorMembers = imperatorCharacters.Where(c => imperatorMemberIds.Contains(c.Id)).ToList();
-		if (imperatorMembers.Count > 0) {
-			var firstImperatorMember = imperatorMembers[0];
-			if (firstImperatorMember.CK3Character is not null) {
-				// Make head's culture the dynasty culture.
-				Culture = firstImperatorMember.CK3Character.CultureId;
-			}
-		} else {
-			Logger.Warn($"Couldn't determine culture for dynasty {Id}, needs manual setting!");
-		}
+		var imperatorMembers = imperatorCharacters
+			.Where(c => imperatorMemberIds.Contains(c.Id))
+			.ToList();
+
+		SetCultureFromImperator(imperatorFamily, imperatorMembers);
 
 		foreach (var member in imperatorMembers) {
 			var ck3Member = member.CK3Character;
@@ -55,4 +51,35 @@ public class Dynasty : IPDXSerializable, IIdentifiable<string> {
 
 	[NonSerialized] public LocBlock? LocalizedName { get; }
 	[NonSerialized] public StringOfItem? CoA { get; set; }
+
+	private void SetCultureFromImperator(Family irFamily, IReadOnlyList<Character> irMembers) {
+		if (irMembers.Count > 0) {
+			var firstImperatorMember = irMembers[0];
+			if (firstImperatorMember.CK3Character is not null) {
+				// Make head's culture the dynasty culture.
+				Culture = firstImperatorMember.CK3Character.CultureId;
+				return;
+			}
+			
+			// Try to set culture from other members.
+			var otherImperatorMembers = irMembers.Skip(1).ToList();
+			foreach (var otherImperatorMember in otherImperatorMembers) {
+				if (otherImperatorMember.CK3Character is null) {
+					continue;
+				}
+
+				// Make member's culture the dynasty culture.
+				Culture = otherImperatorMember.CK3Character.CultureId;
+				return;
+			}
+		}
+		
+		// Try to set culture from family.
+		if (irFamily.Culture is not null) {
+			Culture = irFamily.Culture;
+			return;
+		}
+		
+		Logger.Warn($"Couldn't determine culture for dynasty {Id}, needs manual setting!");
+	}
 }
