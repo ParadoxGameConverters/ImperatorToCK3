39a314ded44755427d4668efe6d5fe1c59592687
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-03-12 10:14:50 +0100

    cleanup

--- ImperatorToCK3/CK3/Characters/CharactersLoader.cs
+++ ImperatorToCK3/CK3/Characters/CharactersLoader.cs
@@ -1,6 +1,5 @@
 ﻿using commonItems;
 using commonItems.Mods;
-using ImperatorToCK3.CommonUtils;
 
 namespace ImperatorToCK3.CK3.Characters;
 
@@ -8,15 +7,12 @@ public partial class CharacterCollection {
 	public void LoadCK3Characters(ModFilesystem ck3ModFS) {
 		Logger.Info("Loading characters from CK3...");
 
-		var ignoredKeywords = new IgnoredKeywordsSet();
 		var parser = new Parser();
 		parser.RegisterRegex(CommonRegexes.String, (reader, characterId) => {
 			var character = new Character(characterId, reader, this);
 			AddOrReplace(character);
 		});
-		parser.IgnoreAndStoreUnregisteredItems(ignoredKeywords);
+		parser.IgnoreAndLogUnregisteredItems();
 		parser.ParseGameFolder("history/characters", ck3ModFS, "txt", recursive: true);
-		
-		Logger.Debug($"Ignored CK3 character keywords: {ignoredKeywords}");
 	}
 }
