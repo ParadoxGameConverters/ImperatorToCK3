7735ad5500adbfd2572de3a9c7fd4af1c2c6824f
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-03-20 08:26:58 +0100

    Fix loading of CK3 morph_genes

--- ImperatorToCK3/CommonUtils/Genes/MorphGene.cs
+++ ImperatorToCK3/CommonUtils/Genes/MorphGene.cs
@@ -10,6 +10,8 @@ public class MorphGene : Gene {
 		var parser = new Parser();
 		parser.RegisterKeyword("index", ParserHelpers.IgnoreItem);
 		parser.RegisterKeyword("ugliness_feature_categories", ParserHelpers.IgnoreItem);
+		parser.RegisterKeyword("can_have_portrait_extremity_shift", ParserHelpers.IgnoreItem);
+		parser.RegisterKeyword("visible", ParserHelpers.IgnoreItem);
 		parser.RegisterKeyword("group", ParserHelpers.IgnoreItem);
 		parser.RegisterRegex(CommonRegexes.String, (reader, geneTemplateName) => {
 			GeneTemplates[geneTemplateName] = new MorphGeneTemplate(reader);
