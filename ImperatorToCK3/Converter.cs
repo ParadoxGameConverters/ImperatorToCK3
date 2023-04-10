6dbf31f4d7e68001fefd6aac2dafc52a05711ad9
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-04-10 03:26:02 +0200

    ExtractSamplesFromBank

--- ImperatorToCK3/Converter.cs
+++ ImperatorToCK3/Converter.cs
@@ -1,7 +1,7 @@
 ﻿using commonItems;
 using ImperatorToCK3.Helpers;
 using System.IO;
-using System.Threading;
+using System.Linq;
 
 namespace ImperatorToCK3;
 
@@ -12,35 +12,33 @@ internal static class Converter {
 		SystemUtils.TryCreateFolder("temp");
 		var config = new Configuration(converterVersion);
 		
-		var irMusicTokenSource = PlayImperatorMusic(config);
-		var imperatorWorld = new Imperator.World(config, converterVersion);
-		irMusicTokenSource.Cancel();
+		PlayMusic(config);
 		
-		var ck3MusicTokenSource = PlayCK3Music(config);
+		var imperatorWorld = new Imperator.World(config, converterVersion);
+
 		var ck3World = new CK3.World(imperatorWorld, config);
 		Outputter.WorldOutputter.OutputWorld(ck3World, imperatorWorld, config);
+		
 		Logger.Info("* Conversion complete! *");
 		Logger.Progress(100);
-		ck3MusicTokenSource.Cancel();
 	}
 
-	private static CancellationTokenSource PlayImperatorMusic(Configuration config) {
-		var  tokenSource = new CancellationTokenSource();
-		CancellationToken token = tokenSource.Token;
+	private static void PlayMusic(Configuration config) {
+		var irBankPath = Path.Combine(config.ImperatorPath, "game/sound/banks/Music.bank");
+		var irSampleNames = new[] {"PDX_IR_HG_MomentOfTheBattle_201102_v1_R01"};
+		var irMusicPaths = MusicPlayer.ExtractSamplesFromBank(irBankPath, irSampleNames);
 		
-		var irMusicPath = Path.Combine(config.ImperatorPath, "game/sound/banks/Music.bank");
-		MusicPlayer.PlayMusic(irMusicPath, token);
-
-		return tokenSource;
-	}
-	
-	private static CancellationTokenSource PlayCK3Music(Configuration config) {
-		var  tokenSource = new CancellationTokenSource();
-		CancellationToken token = tokenSource.Token;
+		var ck3BankPath = Path.Combine(config.CK3Path, "game/sound/banks/Soundtrack.bank");
+		var ck3SampleNames = new[] {
+			"taberna_revisited_in_game_instrumental",
+			"thesacredrite_in_game",
+			"charge_of_the_knights_final",
+			"title_theme_the_dynasty_finalv2",
+			"lionheart_withvocals_in_game"
+		};
+		var ck3MusicPaths = MusicPlayer.ExtractSamplesFromBank(ck3BankPath, ck3SampleNames);
 		
-		var ck3MusicPath = Path.Combine(config.CK3Path, "game/sound/banks/Soundtrack.bank");
-		MusicPlayer.PlayMusic(ck3MusicPath, token);
-
-		return tokenSource;
+		var musicPaths = irMusicPaths.Concat(ck3MusicPaths);
+		MusicPlayer.PlayMusic(musicPaths);
 	}
 }
--- ImperatorToCK3/Helpers/MusicPlayer.cs
+++ ImperatorToCK3/Helpers/MusicPlayer.cs
@@ -1,7 +1,9 @@
 using commonItems;
 using Fmod5Sharp;
 using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -9,75 +11,77 @@ using System.Threading;
 namespace ImperatorToCK3.Helpers; 
 
 public static class MusicPlayer {
-	public static void PlayMusic(string filePath, CancellationToken cancellationToken) {
+	public static void PlayMusic(IEnumerable<string> filePaths) {
 		var thread = new Thread(() => {
 			try {
-				PlayMusicInternal(filePath, cancellationToken);
+				PlayMusicInternal(filePaths);
 			} catch (Exception e) {
-				Logger.Warn($"Failed to play music file \"{filePath}\"! {e}");
+				Logger.Warn($"Failed to play music! {e}");
 			}
 		}) {IsBackground = true};
 		thread.Start();
 	}
 
-	private static void PlayMusicInternal(string filePath, CancellationToken cancellationToken) {
-		if (!File.Exists(filePath)) {
-			throw new FileNotFoundException($"Music file \"{filePath}\" not found!");
-		}
-
-		var extension = CommonFunctions.GetExtension(filePath);
-		if (CommonFunctions.GetExtension(filePath) == "bank") {
-			// Extract music from bank.
-			Logger.Debug("Loading bank...");
-			var bytes = File.ReadAllBytes(filePath);
+	public static IEnumerable<string> ExtractSamplesFromBank(string bankFilePath, IEnumerable<string> sampleNames) {
+		var fileNamesToReturn = new List<string>();
+		
+		try {
+			Logger.Debug($"Loading bank {bankFilePath}...");
+			var bytes = File.ReadAllBytes(bankFilePath);
 			var index = bytes.AsSpan().IndexOf("FSB5"u8);
 			if (index > 0) {
 				bytes = bytes.AsSpan(index).ToArray();
 			}
 
 			var bank = FsbLoader.LoadFsbFromByteArray(bytes);
+			var baseFileNameWithoutExtension = Path.GetFileNameWithoutExtension(bankFilePath);
+			foreach (var sample in bank.Samples.Where(s => sampleNames.Contains(s.Name))) {
+				var name = sample.Name!;
+			
+				if (!sample.RebuildAsStandardFileFormat(out var data, out var extension)) {
+					Logger.Warn($"Failed to extract bank sample {name}!");
+					continue;
+				}
 
-			Logger.Debug("Extracting music from bank...");
-			var bankSample = bank.Samples.First(s => s.Name is not null);
-			var name = bankSample.Name!;
-			if (!bankSample.RebuildAsStandardFileFormat(out var data, out var extractedFileExtension)) {
-				Logger.Warn($"Failed to extract bank sample {name}!");
-				return;
-			}
+				var extractedFilePath = $"temp/music_{baseFileNameWithoutExtension}_{name}.{extension}";
+				File.WriteAllBytes(extractedFilePath, data);
 			
-			if (!Directory.Exists("temp")) {
-				Directory.CreateDirectory("temp");
+				fileNamesToReturn.Add(extractedFilePath);
 			}
-			var extractedFilePath = $"temp/extracted_{name}.{extractedFileExtension}";
-			File.WriteAllBytes(extractedFilePath, data);
-			Logger.Debug($"Extracted sample {name}.");
-			
-			filePath = extractedFilePath;
-			extension = extractedFileExtension;
+		} catch (Exception e) {
+			Logger.Error($"Failed to extract samples from bank file \"{bankFilePath}\"! {e}");
 		}
 
+		return fileNamesToReturn;
+	}
+
+	private static void PlayMusicInternal(IEnumerable<string> filePaths) {
+		var waveProviders = filePaths.Select(GetWaveProvider).ToList();
+		var playlist = new ConcatenatingSampleProvider(waveProviders);
+		
 		using var waveOut = new WaveOutEvent();
+		waveOut.Volume = 0.5f;
+		waveOut.Init(playlist);
+		waveOut.Play();
+		
+		while (waveOut.PlaybackState == PlaybackState.Playing) {
+			// Wait for playback to finish.
+			Thread.Sleep(1000);
+		}
+	}
+	
+	private static ISampleProvider GetWaveProvider(string filePath) {
+		var extension = CommonFunctions.GetExtension(filePath);
 		switch (extension) {
 			case "mp3":
 			case "wav": {
-				using var waveProvider = new AudioFileReader(filePath);
-				waveOut.Init(waveProvider);
-				break;
+				return new AudioFileReader(filePath);
 			}
 			case "ogg": {
-				using var waveProvider = new NAudio.Vorbis.VorbisWaveReader(filePath);
-				waveOut.Init(waveProvider);
-				break;
+				return new NAudio.Vorbis.VorbisWaveReader(filePath);
 			}
 			default:
 				throw new FormatException($"Unsupported music file format: {extension}!");
-		}
-
-		waveOut.Volume = 0.5f;
-		waveOut.Play();
-		while (!cancellationToken.IsCancellationRequested && waveOut.PlaybackState == PlaybackState.Playing) {
-			// Wait for playback to finish.
-			Thread.Sleep(1000);
 		}
 	}
 }
