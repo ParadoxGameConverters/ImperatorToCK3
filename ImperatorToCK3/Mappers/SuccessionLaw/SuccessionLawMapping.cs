41a433498db0823473d68223631791807ef0fd7a
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-07-05 23:31:13 +0200

    Use "ir" instead of "imp" keyword in succession law mappings

--- ImperatorToCK3/Mappers/SuccessionLaw/SuccessionLawMapping.cs
+++ ImperatorToCK3/Mappers/SuccessionLaw/SuccessionLawMapping.cs
@@ -6,11 +6,11 @@ namespace ImperatorToCK3.Mappers.SuccessionLaw;
 public class SuccessionLawMapping {
 	public string ImperatorLaw { get; set; } = "";
 	public SortedSet<string> CK3SuccessionLaws { get; } = new();
-	public SuccessionLawMapping(BufferedReader reader) {
+	public SuccessionLawMapping(BufferedReader mappingReader) {
 		var parser = new Parser();
-		parser.RegisterKeyword("imp", reader => ImperatorLaw = reader.GetString());
+		parser.RegisterKeyword("ir", reader => ImperatorLaw = reader.GetString());
 		parser.RegisterKeyword("ck3", reader => CK3SuccessionLaws.Add(reader.GetString()));
 		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
-		parser.ParseStream(reader);
+		parser.ParseStream(mappingReader);
 	}
 }
