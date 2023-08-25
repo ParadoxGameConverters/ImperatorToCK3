ff6056acf871ade0f3edb04cd9e2037d1c468770
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-08-25 20:19:32 +0200

    Update StateTests.cs

--- ImperatorToCK3.UnitTests/Imperator/States/StateTests.cs
+++ ImperatorToCK3.UnitTests/Imperator/States/StateTests.cs
@@ -17,7 +17,7 @@ public class StateTests {
 	private static readonly ModFilesystem irModFS = new(ImperatorRoot, new Mod[] { });
 	private static readonly ProvinceCollection provinces = new();
 	private static readonly AreaCollection areas = new();
-	private static readonly CountryCollection countries = new() {new Country(69)};
+	private static readonly CountryCollection countries = new() {new Country(69), new Country(70)};
 
 	public StateTests() {
 		areas.LoadAreas(irModFS, provinces);
@@ -25,10 +25,28 @@ public class StateTests {
 
 	[Fact]
 	public void StateCanBeInitialized() {
-		var stateData = new StateData {Area = areas["test_area"], CapitalProvinceId = 2, Country = countries[69]};
-		var state = new State(1, stateData);
-		Assert.Equal("test_area", state.Area.Id);
-		Assert.Equal((ulong)69, state.Country.Id);
+		var stateCollection = new StateCollection();
+		var statesReader = new BufferedReader("""
+			1 = {
+				area="test_area"
+				capital=2
+				country=69
+			}
+			2 = {
+				area="test_area"
+				capital=3
+				country=70
+			}
+		""");
+		stateCollection.LoadStates(statesReader, areas, countries);
+		var state1 = stateCollection[1];
+		Assert.Equal("test_area", state1.Area.Id);
+		Assert.Equal((ulong)69, state1.Country.Id);
+		Assert.Equal((ulong)2, state1.CapitalProvince.Id);
+		var state2 = stateCollection[2];
+		Assert.Equal("test_area", state2.Area.Id);
+		Assert.Equal((ulong)70, state2.Country.Id);
+		Assert.Equal((ulong)3, state2.CapitalProvince.Id);
 	}
 
 	[Fact]
