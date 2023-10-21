ef4b24b9a4cf1fff2924a3bd0f8a27ee7716b5a8
IhateTrains <IhateTrains@users.noreply.github.com>
2023-10-21 14:43:34 +0100

    Bump Rakaly to 0.4.19 (#1566) #patch

--- ImperatorToCK3/Helpers/RakalyCaller.cs
+++ ImperatorToCK3/Helpers/RakalyCaller.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 namespace ImperatorToCK3.Helpers;
 
 public static class RakalyCaller {
-	private const string RakalyVersion = "0.4.16";
+	private const string RakalyVersion = "0.4.19";
 	private static readonly string RakalyExecutablePath;
 
 	static RakalyCaller() {
