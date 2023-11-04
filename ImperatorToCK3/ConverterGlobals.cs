8a4c513144ad552d1e8ee1f92a2e8d77bcd1f11c
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-11-04 18:27:05 +0000

    Generate CK3 holy sites for Ajivika faiths

--- ImperatorToCK3/ConverterGlobals.cs
+++ ImperatorToCK3/ConverterGlobals.cs
@@ -5,9 +5,9 @@ namespace ImperatorToCK3;
 public static class ConverterGlobals {
 	public static string PrimaryLanguage => "english";
 
-	public static string[] SecondaryLanguages { get; } = {
-		"french", "german", "korean", "russian", "simp_chinese", "spanish"
-	};
+	public static string[] SecondaryLanguages { get; } = [
+		"french", "german", "korean", "russian", "simp_chinese", "spanish",
+	];
 	
 	public static IEnumerable<string> SupportedLanguages {
 		get {
