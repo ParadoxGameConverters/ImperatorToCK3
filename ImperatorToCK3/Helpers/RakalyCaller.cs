ff9a0e57429430b4df50efe18bbe7a88e433d326
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-12-26 18:14:22 +0100

    Fix some warnings

--- ImperatorToCK3/Helpers/RakalyCaller.cs
+++ ImperatorToCK3/Helpers/RakalyCaller.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
 
 namespace ImperatorToCK3.Helpers;
 
@@ -13,10 +12,10 @@ public static class RakalyCaller {
 	static RakalyCaller() {
 		string currentDir = Directory.GetCurrentDirectory();
 		RakalyExecutablePath = $"Resources/rakaly/rakaly-{RakalyVersion}-x86_64-pc-windows-msvc/rakaly.exe";
-		if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+		if (OperatingSystem.IsMacOS()) {
 			RakalyExecutablePath = $"Resources/rakaly/rakaly-{RakalyVersion}-x86_64-apple-darwin/rakaly";
 			Exec($"chmod +x {currentDir}/{RakalyExecutablePath}");
-		} else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+		} else if (OperatingSystem.IsLinux()) {
 			RakalyExecutablePath = $"Resources/rakaly/rakaly-{RakalyVersion}-x86_64-unknown-linux-musl/rakaly";
 			Exec($"chmod +x {currentDir}/{RakalyExecutablePath}");
 		}
