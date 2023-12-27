05d08aa04b5bc0f7f639d77f9c7ec038d20b1349
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-12-27 17:40:55 +0100

    Handle incorrect DNA strings from the save

--- ImperatorToCK3/Imperator/Characters/PortraitData.cs
+++ ImperatorToCK3/Imperator/Characters/PortraitData.cs
@@ -51,6 +51,10 @@ public class PortraitData {
 			}
 			
 			var geneTemplateByteIndex = geneIndex.Value * 4;
+			if (decodedDnaStr.Length <= geneTemplateByteIndex + 3) {
+				Logger.Warn($"DNA string is too short for gene {gene.Id}!");
+				continue;
+			}
 			var geneTemplateIndex = (uint)decodedDnaStr[geneTemplateByteIndex];
 			var geneTemplateRecessiveIndex = (uint)decodedDnaStr[geneTemplateByteIndex + 2];
 			var geneTemplateName = gene.GetGeneTemplateByIndex(geneTemplateIndex)?.Id;
@@ -83,13 +87,11 @@ public class PortraitData {
 				continue;
 			}
 
-			// Temporary fix for broken Invictus DNA strings which don't have a value for the headgear gene.
-			// TODO: verify if still require for Invictus version higher than the original pre-hotfix 1.7
-			if (gene.Id == "headgear") {
+			var geneTemplateByteIndex = geneIndex.Value * 4;
+			if (decodedDnaStr.Length <= geneTemplateByteIndex + 3) {
+				Logger.Warn($"DNA string is too short for gene {gene.Id}!");
 				continue;
 			}
-
-			var geneTemplateByteIndex = geneIndex.Value * 4;
 			var geneTemplateIndex = (uint)decodedDnaStr[geneTemplateByteIndex];
 			var geneTemplateRecessiveIndex = (uint)decodedDnaStr[geneTemplateByteIndex + 2];
 			var geneTemplateName = gene.GetGeneTemplateByIndex(geneTemplateIndex).Id;
