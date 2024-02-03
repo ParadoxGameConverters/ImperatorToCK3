aa22796720f42b3738e3299f971af2f1b21dac5b
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2024-02-03 10:36:45 +0100

    Remove special handling of ce_lion.dds colored emblem

--- ImperatorToCK3/Outputter/CoatOfArmsEmblemsOutputter.cs
+++ ImperatorToCK3/Outputter/CoatOfArmsEmblemsOutputter.cs
@@ -8,27 +8,6 @@ using System.IO;
 namespace ImperatorToCK3.Outputter;
 
 public static class CoatOfArmsEmblemsOutputter {
-	private static bool IsBrokenColoredEmblem(string fileName, Configuration config) {
-		var emblemFilePath = Path.Combine(
-			"output",
-			config.OutputModName,
-			"gfx/coat_of_arms/colored_emblems/ce_lion.dds"
-		);
-		// Something's wrong with ce_lion.dds.
-		if (fileName == "ce_lion.dds" && !File.Exists(emblemFilePath)) {
-			// Instead of converting a broken file from Imperator, copy closest CK3 emblem.
-			var wasCopied = SystemUtils.TryCopyFile(
-				Path.Combine(config.CK3Path, "game/gfx/coat_of_arms/colored_emblems/ce_lion_passant.dds"),
-				emblemFilePath
-			);
-			if (!wasCopied) {
-				Logger.Warn("Couldn't copy a replacement for ce_lion.dds!");
-			}
-			return true;
-		}
-		return false;
-	}
-
 	private static void CopyColoredEmblems(Configuration config, ModFilesystem imperatorModFS) {
 		Logger.Info("Copying colored emblems...");
 		var coloredEmblemsFolder = "gfx/coat_of_arms/colored_emblems";
@@ -47,9 +26,6 @@ public static class CoatOfArmsEmblemsOutputter {
 		void CopyEmblem(string emblemFilePath) {
 			var fileName = CommonFunctions.TrimPath(emblemFilePath);
 
-			if (IsBrokenColoredEmblem(fileName, config)) {
-				return;
-			}
 			// Load an image.
 			var image = new MagickImage(emblemFilePath);
 			image.Negate(channels: Channels.Red);
