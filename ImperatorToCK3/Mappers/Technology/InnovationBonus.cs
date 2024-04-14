f9889a6824100ee94ce5554827062f4e20ac1b32
IhateTrains <IhateTrains@users.noreply.github.com>
2024-04-14 14:58:19 +0200

    Technology conversion configurable through configurables/inventions_to_innovations_map.txt (#1878)
    part of #163

---
+++ ImperatorToCK3/Mappers/Technology/InnovationBonus.cs
@@ -0,0 +1,35 @@
+using commonItems;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImperatorToCK3.Mappers.Technology;
+
+public sealed class InnovationBonus { // TODO: add tests
+	private readonly HashSet<string> imperatorInventions = [];
+	private string? ck3Innovation;
+	
+	public InnovationBonus(BufferedReader bonusReader) {
+		var parser = new Parser();
+		parser.RegisterKeyword("ir", reader => imperatorInventions.Add(reader.GetString()));
+		parser.RegisterKeyword("ck3", reader => ck3Innovation = reader.GetString());
+		parser.IgnoreAndLogUnregisteredItems();
+		parser.ParseStream(bonusReader);
+		
+		// A bonus should have at most 3 inventions.
+		if (imperatorInventions.Count > 3) {
+			Logger.Warn($"Innovation bonus for {ck3Innovation} has more than 3 inventions: {string.Join(", ", imperatorInventions)}");
+		}
+	}
+	
+	public KeyValuePair<string, ushort>? GetProgress(IEnumerable<string> activeInventions) {
+		if (ck3Innovation is null) {
+			return null;
+		}
+		
+		// For each matching invention, add 25 to the progress.
+		int progress = activeInventions
+			.Where(invention => imperatorInventions.Contains(invention))
+			.Sum(invention => 25);
+		return new(ck3Innovation, (ushort)progress);
+	}
+}
