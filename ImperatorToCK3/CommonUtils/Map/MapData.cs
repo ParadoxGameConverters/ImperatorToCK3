91e555be40ac5c9c92a754cceadeacaea13ecd93
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2024-05-03 14:51:45 +0200

    Explicitly ignore more country keywords from the save

--- ImperatorToCK3/CommonUtils/Map/MapData.cs
+++ ImperatorToCK3/CommonUtils/Map/MapData.cs
@@ -5,7 +5,6 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
