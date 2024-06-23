0af3426c6a6c460cf5c273a879894eead94944df
IhateTrains <IhateTrains@users.noreply.github.com>
2024-06-23 16:07:55 +0100

    Speed up the loading of Imperator pops (#2007)

--- ImperatorToCK3/Imperator/Pops/Pop.cs
+++ ImperatorToCK3/Imperator/Pops/Pop.cs
@@ -1,13 +1,27 @@
-﻿using commonItems.Collections;
+﻿using commonItems;
+using commonItems.Collections;
 
 namespace ImperatorToCK3.Imperator.Pops;
 
-public partial class Pop : IIdentifiable<ulong> {
+public class Pop : IIdentifiable<ulong> {
 	public ulong Id { get; } = 0;
 	public string Type { get; set; } = "";
 	public string Culture { get; set; } = "";
 	public string Religion { get; set; } = "";
 	public Pop(ulong id) {
 		Id = id;
+	}
+	
+	public static Pop Parse(string idString, BufferedReader reader) {
+		var newPop = new Pop(ulong.Parse(idString));
+
+		var parser = new Parser();
+		parser.RegisterKeyword("type", r => newPop.Type = r.GetString());
+		parser.RegisterKeyword("culture", r => newPop.Culture = r.GetString());
+		parser.RegisterKeyword("religion", r => newPop.Religion = r.GetString());
+		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreAndLogItem);
+		parser.ParseStream(reader);
+		
+		return newPop;
 	}
 }
