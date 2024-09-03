5a9826104f7e414630acfc3686385cbb5c71b72f
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2024-09-03 22:33:55 +0200

    Remove more parts of vanilla game start on-actions

--- ImperatorToCK3/Outputter/OnActionOutputter.cs
+++ ImperatorToCK3/Outputter/OnActionOutputter.cs
@@ -29,8 +29,13 @@ public static class OnActionOutputter {
 		}
 		var fileContent = await File.ReadAllTextAsync(inputPath);
 
-		// List of blocks to remove as of 2024-09-01.
+		// List of blocks to remove as of 2024-09-03.
 		string[] partsToRemove = [
+			// events
+			"\t\tfp1_scandinavian_adventurers.0011\t# FP1 - Corral famous Norse adventurers that haven't done much yet.\n",
+			"\t\tfp1_scandinavian_adventurers.0021\t# FP1 - Mark game-start prioritised adventurers.\n",
+			"\t\teasteregg_event.0001\t\t\t\t# Charna and Jakub duel.\n",
+			"\t\tgame_rule.1011\t#Hungarian Migration management.\n",
 			"""
 					### 867 - RADHANITES IN KHAZARIA ###
 					character:74025 = {
@@ -96,6 +101,253 @@ public static class OnActionOutputter {
 						}
 					}
 			""",
+			// setup
+			"""
+					### 1066 - LOUIS THE GERMAN ###
+					if = {
+						limit = {
+							exists = character:90107
+							current_date >= 1066.1.1
+						}
+						character:90107 = { give_nickname = nick_the_german_post_mortem }
+					}
+			""",
+			"""
+					# UNITY CONFIG
+					## 867.
+					if = {
+						limit = { game_start_date = 867.1.1 }
+						# Twiddle some starting unities.
+						## The Abassids are in the middle of a self-killing frenzy, so we lower theirs substantially.
+						house:house_abbasid ?= {
+							add_unity_value = {
+								value = -100
+								# This is from historical circumstances, so we just do use the house head.
+								character = house_head
+								desc = clan_unity_historical_circumstances.desc
+							}
+						}
+						## The Samanids are juuuuust about to get started on killing each other over who gets to lead Transoxiana.
+						house:house_samanid ?= {
+							add_unity_value = {
+								value = -40
+								# This is from historical circumstances, so we just do use the house head.
+								character = house_head
+								desc = clan_unity_historical_circumstances.desc
+							}
+						}
+						## The Afrighids (both of them) are having fairly few arguments because only one of them can speak and it's very easy to manage relations with a baby.
+						dynasty:1042112.dynast.house ?= {
+							add_unity_value = {
+								value = 50
+								# This is from historical circumstances, so we just do use the house head.
+								character = house_head
+								desc = clan_unity_historical_circumstances.desc
+							}
+						}
+						## The Tahirids are scattered but actually get along quite well and support each other politically (mostly).
+						dynasty:811.dynast.house ?= {
+							add_unity_value = {
+								value = 100
+								# This is from historical circumstances, so we just do use the house head.
+								character = house_head
+								desc = clan_unity_historical_circumstances.desc
+							}
+						}
+						## The Umayyads are having something of a renaissance.
+						dynasty:597.dynast.house ?= {
+							add_unity_value = {
+								value = 100
+								# This is from historical circumstances, so we just do use the house head.
+								character = house_head
+								desc = clan_unity_historical_circumstances.desc
+							}
+						}
+					}
+					# LEGITIMACY CONFIG
+					## 867.
+					if = {
+						limit = { game_start_date = 867.1.1 }
+						## Basileus Basileios was actually elected, so he's technically legitimate, but starts at level 2. With this he should be level 3.
+						character:1700 = {
+							add_legitimacy = major_legitimacy_gain
+						}
+					}
+			""",
+			"""
+					if = { # Special historical events for Matilda!
+						limit = {
+							character:7757 ?= { is_alive = yes }
+						}
+						character:7757 ?= {
+							trigger_event = bookmark.1066 # Matildas marriage to her step-brother, with plausible historical options!
+							trigger_event = { # Matildas suspected witchcraft, the player decides if its true or not!
+								id = bookmark.1067
+								years = { 1 5 }
+							}
+						}
+					}
+			""",
+			"""
+					if = { # Special historical events for Vratislav!
+						limit = {
+							character:522 ?= { is_alive = yes }
+						}
+						character:522 ?= {
+							trigger_event = { # Vratislav and the Slavic Marches, he didn't historically get them (one briefly, but eh). The player chooses to appease the emperor or go after the coveted lands themselves!
+								id = bookmark.1068
+								days = { 35 120 }
+							}
+							trigger_event = { # Jaromir, Vratislav's brother, was a pain - this event is an opportunity for the player to handle the rivalry
+								id = bookmark.1069
+								days = { 1 29 }
+							}
+						}
+					}
+			""",
+			"""
+					if = { # Special historical events for Robert the Fox!
+						limit = {
+							character:1128 ?= { is_alive = yes }
+						}
+						character:1128 ?= {
+							trigger_event = { # A Norman Sicily - Robert historically conquered quite a bit here, the player can choose how far they want to go and the risk they want to take. The more risk, the more event troops/claims.
+								id = bookmark.1070
+								days = { 35 120 }
+							}
+							trigger_event = { # The Pretender Monk - Raiktor is a historical character, a monk wo pretended to be a deposed Byzantine emperor which Robert used to beat up Byzantium. Here you can follow historical conquests (taking a bit of the coast) or go full on 'install him as emperor for real'-mode!
+								id = bookmark.1071
+								years = { 1 7 }
+							}
+						}
+					}
+			""",
+			"""
+					if = { # Special historical events for Emir Yahya!
+						limit = {
+							character:3924 ?= { is_alive = yes }
+						}
+						character:3924 ?= {
+							trigger_event = { # Conquering Cordoba - Gain an opportunity to conquer Cordoba while gaining one of two buffs; one intrigue-focused, and one military. Historically he was poisoned after having conquered the city... but that's no fun for the player!
+								id = bookmark.1072
+								days = { 10 35 }
+							}
+						}
+					}
+			""",
+			"""
+					# Pre-defined historic regencies setup.
+					## NOTE: we do these first to avoid feed messages getting weird due to regents being replaced immediately after getting their position.
+					## 867.
+					### None. Yet.
+					## 1066.
+					if = {
+						limit = { game_start_date = 1066.9.15 }
+						# Designate some regents.
+						## King Philippe of France & Duke Boudewijn of Flanders (friend of his dad's)
+						character:214 = {
+							designate_diarch = character:364
+							# Baldwin of Flanders also promised the prior king he'd take care of Philippe, so we add that starting loyalty hook.
+							add_hook = {
+								type = predecessor_loyalty_hook
+								target = character:364
+								years = historic_regent_loyal_after_death_hook_duration_years_char_214_value
+							}
+						}
+						### Plus remember who the promise was made to.
+						character:364 = {
+							add_opinion = {
+								target = character:214
+								modifier = promise_to_predecessor
+								opinion = 50
+							}
+							set_variable = {
+								name = promise_to_predecessor
+								value = character:208
+								years = historic_regent_loyal_after_death_hook_duration_years_char_214_value
+							}
+						}
+						## Count Bouchard of Vendome & Guy de Bachaumont (his uncle)
+						character:40905 = { designate_diarch = character:40376 }
+						## Caliph al-Mustansir & Rasad (his mother)
+						character:3096 = { designate_diarch = character:additional_fatimids_1 }
+						## Count Ermengol of Urgell & Infanta Sancha of Aragon (his stepmother)
+						character:110550 = { designate_diarch = character:110514 }
+						## Duke Dirk of Holland & Count Robrecht of Zeeland (his stepfather)
+						character:106520 = { designate_diarch = character:368 }
+						## Duke Sven of Ostergotland & Kol Sverker (his father)
+						character:100530 = { designate_diarch = character:100529 }
+						## King Salamon of Hungary & Queen Mother Anastasia (his mother, in the absence of any better recorded options, and to keep other hostile relatives out of the job)
+						character:476 = { designate_diarch = character:637 }
+						## Prince Demetre of Georgia & Alda Oseti (his mother)
+						character:9957 = { designate_diarch = character:9956 }
+						## Sultan al-Muazzam Alp Arslan and Hassan "the Order of the Realm".
+						character:3040 = {
+							designate_diarch = character:3050
+							# This is a vizierate as well, so start the diarchy manually.
+							start_diarchy = vizierate
+							# Tell Alp that he appointed Hassan so he remembers not to dismiss him.
+							set_variable = {
+								name = my_vizier
+								value = character:3050
+							}
+						}
+						# Plus remove all the generated opinions.
+						## King Philippe of France & Duke Boudewijn of Flanders
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:364
+							LIEGE = character:214
+						}
+						## Count Bouchard of Vendome & Guy de Bachaumont
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:40376
+							LIEGE = character:40905
+						}
+						## Caliph al-Mustansir & Rasad
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:additional_fatimids_1
+							LIEGE = character:3096
+						}
+						## Count Ermengol of Urgell & Infanta Sancha of Aragon
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:110514
+							LIEGE = character:110550
+						}
+						## Duke Dirk of Holland & Count Robrecht of Zeeland
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:368
+							LIEGE = character:106520
+						}
+						## Duke Sven of Ostergotland & Kol Sverker
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:100529
+							LIEGE = character:100530
+						}
+						## King Salamon of Hungary & Queen Mother Anastasia
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:637
+							LIEGE = character:476
+						}
+						## Prince Demetre of Georgia & Alda Oseti
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:9956
+							LIEGE = character:9957
+						}
+						## Sultan al-Muazzam Alp Arslan and Hassan "the Order of the Realm".
+						remove_generated_diarch_consequences_effect = {
+							NEW_DIARCH = character:3050
+							LIEGE = character:3040
+						}
+					}
+			""",
+			"""
+					## Fatimid Caliphate - basically stuck in the back-end of an entrenched regencies from game start.
+					if = {
+						limit = { exists = character:3096 }
+						character:3096 = { trigger_event = diarchy.0012 }
+					}
+			""",
+			// achievements
 			"""
 							##11 Bod Chen Po
 							if = {
@@ -133,7 +385,19 @@ public static class OnActionOutputter {
 									VALUE = yes
 								}
 							}
+			""",
 			"""
+					### ACHIEVEMENT (FP3): The Ummayad Strikes Back
+					every_player = {
+						if = {
+							limit = {
+								dynasty = character:73683.dynasty
+								location = { geographical_region = world_europe_west_iberia }
+							}
+							set_global_variable = fp3_the_umma_strikes_back_achievement_tracker # Is not removed (sad!)
+						}
+					}
+			""",
 		];
 
 		foreach (var block in partsToRemove) {
