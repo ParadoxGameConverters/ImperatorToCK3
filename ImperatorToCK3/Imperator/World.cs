dfb9576a79b02dfe4978c6c10b9ec4de1f4be2a2
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2024-09-15 02:05:32 +0200

    improve log message

--- ImperatorToCK3/Imperator/World.cs
+++ ImperatorToCK3/Imperator/World.cs
@@ -160,7 +160,7 @@ public partial class World {
 		string imperatorBinaryName = OperatingSystem.IsWindows() ? "imperator.exe" : "imperator";
 		var imperatorBinaryPath = Path.Combine(config.ImperatorPath, "binaries", imperatorBinaryName);
 		if (!File.Exists(imperatorBinaryPath)) {
-			Logger.Warn("Imperator binary not found! Aborting!");
+			Logger.Warn("Imperator binary not found! Aborting the random CoA extraction!");
 			return;
 		}
 		
