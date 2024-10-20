9f7d534632b517ccf9fdba9510533fc31afe14df
iht <IhateTrains@users.noreply.github.com>
2024-10-20 17:35:05 +0200

    Rename DefinesTests to ImperatorDefinesTests

--- ImperatorToCK3.UnitTests/Imperator/ImperatorDefinesTests.cs
+++ ImperatorToCK3.UnitTests/Imperator/ImperatorDefinesTests.cs
@@ -1,18 +1,17 @@
-using commonItems;
 using commonItems.Mods;
 using ImperatorToCK3.Imperator;
 using Xunit;
 
 namespace ImperatorToCK3.UnitTests.Imperator;
 
-public class DefinesTests {
+public class ImperatorDefinesTests {
 	private const string ImperatorRoot = "TestFiles/Imperator/game";
 	private readonly ModFilesystem imperatorModFS = new(ImperatorRoot, System.Array.Empty<Mod>());
 
 	[Fact]
 	public void CohortSizeCanBeRead() {
-		var defines = new Defines();
+		var defines = new ImperatorDefines();
 		defines.LoadDefines(imperatorModFS);
-		Assert.Equal(601, int.Parse(defines.GetValue("NUnit", "COHORT_SIZE")!));
+		Assert.Equal(601, defines.CohortSize);
 	}
 }
