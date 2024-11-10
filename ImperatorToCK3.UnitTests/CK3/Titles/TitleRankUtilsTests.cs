f3d2780a9bc5ba074b86420a8c4c9feb0bc13653
IhateTrains <IhateTrains@users.noreply.github.com>
2024-11-10 11:51:20 +0100

    Unit tests for TitleRankUtils.CharToTitleRank (#2305)

---
+++ ImperatorToCK3.UnitTests/CK3/Titles/TitleRankUtilsTests.cs
@@ -0,0 +1,23 @@
+using ImperatorToCK3.CK3.Titles;
+using System;
+using Xunit;
+
+namespace ImperatorToCK3.UnitTests.CK3.Titles;
+
+public class TitleRankUtilsTests {
+	[Theory]
+	[InlineData('e', TitleRank.empire)]
+	[InlineData('k', TitleRank.kingdom)]
+	[InlineData('d', TitleRank.duchy)]
+	[InlineData('c', TitleRank.county)]
+	[InlineData('b', TitleRank.barony)]
+	public void CharToTitleRankReturnsCorrectTitleRank(char c, TitleRank expectedRank) {
+		var rank = TitleRankUtils.CharToTitleRank(c);
+		Assert.Equal(expectedRank, rank);
+	}
+	
+	[Fact]
+	public void CharToTitleRankThrowsOnUnknownRank() {
+		Assert.Throws<ArgumentOutOfRangeException>(() => TitleRankUtils.CharToTitleRank('x'));
+	}
+}
