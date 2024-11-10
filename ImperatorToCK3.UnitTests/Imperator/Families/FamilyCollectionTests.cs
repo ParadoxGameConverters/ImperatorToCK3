9088d5530d19f8aed93a9b33281f01e023a79e99
iht <IhateTrains@users.noreply.github.com>
2024-11-10 12:39:46 +0100

    More tests for the loading of families

--- ImperatorToCK3.UnitTests/Imperator/Families/FamilyCollectionTests.cs
+++ ImperatorToCK3.UnitTests/Imperator/Families/FamilyCollectionTests.cs
@@ -1,11 +1,13 @@
 ﻿using commonItems;
+using System;
+using System.IO;
 using Xunit;
 
 namespace ImperatorToCK3.UnitTests.Imperator.Families;
 
 [Collection("Sequential")]
 [CollectionDefinition("Sequential", DisableParallelization = true)]
-public class FamiliesTests {
+public class FamilyCollectionTests {
 	[Fact]
 	public void FamiliesDefaultToEmpty() {
 		var reader = new BufferedReader(
@@ -32,6 +34,19 @@ public class FamiliesTests {
 			item => Assert.Equal((ulong)42, item.Id),
 			item => Assert.Equal((ulong)43, item.Id));
 	}
+	
+	[Fact]
+	public void FamiliesCanBeLoadedFromBloc() {
+		var reader = new BufferedReader(
+			"families = {\n42={}\n43={}\n}"
+		);
+		var families = new ImperatorToCK3.Imperator.Families.FamilyCollection();
+		families.LoadFamiliesFromBloc(reader);
+
+		Assert.Collection(families,
+			item => Assert.Equal((ulong)42, item.Id),
+			item => Assert.Equal((ulong)43, item.Id));
+	}
 
 	[Fact]
 	public void LiteralNoneFamiliesAreNotLoaded() {
@@ -48,5 +63,19 @@ public class FamiliesTests {
 		Assert.Collection(families,
 			item => Assert.Equal((ulong)43, item.Id)
 		);
+	}
+
+	[Fact]
+	public void FamilyRedefinitionIsLogged() {
+		var reader = new BufferedReader(
+			"= {\n42={}\n42={}\n}"
+		);
+		var families = new ImperatorToCK3.Imperator.Families.FamilyCollection();
+		
+		var output = new StringWriter();
+		Console.SetOut(output);
+		families.LoadFamilies(reader);
+		
+		Assert.Contains("Redefinition of family 42.", output.ToString());
 	}
 }
