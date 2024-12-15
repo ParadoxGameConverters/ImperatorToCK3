75c0ff74b122788ac20664b4a2262eb306420587
IhateTrains <IhateTrains@users.noreply.github.com>
2024-12-15 09:57:18 +0000

    Make Trait and TraitMapper internal, reduce interface use (#2361)

--- ImperatorToCK3/CK3/Characters/Trait.cs
+++ ImperatorToCK3/CK3/Characters/Trait.cs
@@ -1,19 +1,18 @@
 ﻿using commonItems;
 using commonItems.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace ImperatorToCK3.CK3.Characters;
-public sealed class Trait : IIdentifiable<string> {
+internal sealed class Trait : IIdentifiable<string> {
 	public string Id { get; }
-	public ISet<string> Opposites { get; private set; } = new HashSet<string>();
+	public HashSet<string> Opposites { get; private set; } = [];
 
 	public Trait(string id) {
 		Id = id;
 	}
 	public Trait(string id, BufferedReader traitReader) : this(id) {
 		var parser = new Parser();
-		parser.RegisterKeyword("opposites", reader => Opposites = reader.GetStrings().ToHashSet());
+		parser.RegisterKeyword("opposites", reader => Opposites = [.. reader.GetStrings()]);
 		parser.RegisterRegex(CommonRegexes.Catchall, ParserHelpers.IgnoreItem);
 		parser.ParseStream(traitReader);
 	}
