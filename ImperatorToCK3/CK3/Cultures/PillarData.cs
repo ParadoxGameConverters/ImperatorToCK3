bb030eec261b34d47c24428494db47b4f043cf31
iht <IhateTrains@users.noreply.github.com>
2025-01-16 04:31:44 +0100

    Add warnings for pillars not having required CCU parameters

--- ImperatorToCK3/CK3/Cultures/PillarData.cs
+++ ImperatorToCK3/CK3/Cultures/PillarData.cs
@@ -6,8 +6,10 @@ namespace ImperatorToCK3.CK3.Cultures;
 
 internal record PillarData {
 	public List<string> InvalidatingPillarIds { get; set; } = [];
+	
 	public string? Type { get; set; }
 	public Color? Color { get; set; }
+	public Dictionary<string, string> Parameters { get; set; } = [];
 
 	public List<KeyValuePair<string, StringOfItem>> Attributes { get; } = [];
 }
