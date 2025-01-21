a7996140cf00a5ad83144694f84a2f4376fcd4c4
IhateTrains <IhateTrains@users.noreply.github.com>
2025-01-21 01:58:46 +0100

    Partially fix random Muslim and Christian characters being spawned in AEP (#2454)
    "Partially" because it seems the equivalent vanilla file is actually
    used in AEP.

--- ImperatorToCK3/Outputter/FileTweaker.cs
+++ ImperatorToCK3/Outputter/FileTweaker.cs
@@ -44,6 +44,14 @@ public static class FileTweaker {
 			Logger.Info("Reading unneeded parts of Rajas of Asia files...");
 			ReadPartsOfFileToRemove(partsToModifyPerFile, "configurables/removable_file_blocks_roa.txt", warnIfNotFound: true);
 		}
+
+		if (config.AsiaExpansionProjectEnabled) {
+			Logger.Info("Reading unneeded parts of Rajas of Asia files...");
+			ReadPartsOfFileToRemove(partsToModifyPerFile, "configurables/removable_file_blocks_aep.txt", warnIfNotFound: true);
+			
+			Logger.Info("Reading parts of Asia Expansion Project files to modify...");
+			ReadPartsOfFileToReplace(partsToModifyPerFile, "configurables/replaceable_file_blocks_aep.txt", warnIfNotFound: true);
+		}
 		
 		bool isVanilla = config.GetCK3ModFlags()["vanilla"];
 		Logger.Info("Reading unneeded parts of vanilla files...");
