c695e41f08b5135d255df700bac9663e47cea430
iht <IhateTrains@users.noreply.github.com>
2025-06-09 19:52:40 +0200

    Tweaks to the province outputting code

--- ImperatorToCK3/Outputter/ProvincesOutputter.cs
+++ ImperatorToCK3/Outputter/ProvincesOutputter.cs
@@ -17,7 +17,7 @@ internal static class ProvincesOutputter {
 		Title.LandedTitles titles
 	) {
 		Logger.Info("Writing provinces...");
-		
+
 		FrozenSet<ulong> countyCapitalProvinceIds = titles.Counties.AsValueEnumerable()
 			.Select(title => title.CapitalBaronyProvinceId)
 			.Where(id => id is not null)
@@ -25,7 +25,7 @@ internal static class ProvincesOutputter {
 			.ToFrozenSet();
 
 		// Output provinces to files named after their de jure kingdoms.
-		var alreadyOutputtedProvinces = new ConcurrentHashSet<ulong>();
+		var alreadyOutputtedProvIds = new ConcurrentHashSet<ulong>();
 
 		var deJureKingdoms = titles.GetDeJureKingdoms();
 		Parallel.ForEach(deJureKingdoms, kingdom => {
@@ -36,7 +36,7 @@ internal static class ProvincesOutputter {
 				}
 
 				ProvinceOutputter.WriteProvince(sb, province, countyCapitalProvinceIds.Contains(province.Id));
-				alreadyOutputtedProvinces.Add(province.Id);
+				alreadyOutputtedProvIds.Add(province.Id);
 			}
 
 			var filePath = $"{outputModPath}/history/provinces/{kingdom.Id}.txt";
@@ -44,52 +44,55 @@ internal static class ProvincesOutputter {
 			historyOutput.Write(sb.ToString());
 		});
 
-		if (alreadyOutputtedProvinces.Count != provinces.Count) {
+		if (alreadyOutputtedProvIds.Count != provinces.Count) {
 			var filePath = $"{outputModPath}/history/provinces/onlyDeJureDuchy.txt";
 			await using var historyOutput = TextWriter.Synchronized(new StreamWriter(filePath));
 			var deJureDuchies = titles.GetDeJureDuchies();
-			Parallel.ForEach(deJureDuchies, duchy => {
+			foreach (var duchy in deJureDuchies) {
 				var sb = new System.Text.StringBuilder();
 
 				foreach (var province in provinces) {
-					if (alreadyOutputtedProvinces.Contains(province.Id)) {
+					if (alreadyOutputtedProvIds.Contains(province.Id)) {
 						continue;
 					}
 
 					if (duchy.DuchyContainsProvince(province.Id)) {
 						sb.AppendLine($"# {duchy.Id}");
 						ProvinceOutputter.WriteProvince(sb, province, countyCapitalProvinceIds.Contains(province.Id));
-						alreadyOutputtedProvinces.Add(province.Id);
+						alreadyOutputtedProvIds.Add(province.Id);
 					}
 				}
 
 				if (sb.Length > 0) {
-					historyOutput.Write(sb.ToString());
+					await historyOutput.WriteAsync(sb.ToString());
 				}
-			});
-		}
-
-		// Create province mapping file.
-		if (alreadyOutputtedProvinces.Count != provinces.Count) {
-			var mappingsPath = $"{outputModPath}/history/province_mapping/province_mapping.txt";
-			await using var mappingsWriter = FileHelper.OpenWriteWithRetries(mappingsPath, System.Text.Encoding.UTF8);
-			await using var threadSafeWriter = TextWriter.Synchronized(mappingsWriter);
-
-			foreach (var province in provinces) {
-				if (alreadyOutputtedProvinces.Contains(province.Id)) {
-					continue;
-				}
-
-				var baseProvId = province.BaseProvinceId;
-				if (baseProvId is null) {
-					continue;
-				}
-
-				await threadSafeWriter.WriteLineAsync($"{province.Id} = {baseProvId}");
-				alreadyOutputtedProvinces.Add(province.Id);
 			}
 		}
 
+		if (alreadyOutputtedProvIds.Count != provinces.Count) {
+			await CreateProvinceMappingFile(outputModPath, provinces, alreadyOutputtedProvIds);
+		}
+
 		Logger.IncrementProgress();
+	}
+
+	private static async Task CreateProvinceMappingFile(string outputModPath, ProvinceCollection provinces, ConcurrentHashSet<ulong> alreadyOutputtedProvinceIds) {
+		var mappingsPath = $"{outputModPath}/history/province_mapping/province_mapping.txt";
+		await using var mappingsWriter = FileHelper.OpenWriteWithRetries(mappingsPath, System.Text.Encoding.UTF8);
+		await using var threadSafeWriter = TextWriter.Synchronized(mappingsWriter);
+
+		foreach (var province in provinces) {
+			if (alreadyOutputtedProvinceIds.Contains(province.Id)) {
+				continue;
+			}
+
+			var baseProvId = province.BaseProvinceId;
+			if (baseProvId is null) {
+				continue;
+			}
+
+			await threadSafeWriter.WriteLineAsync($"{province.Id} = {baseProvId}");
+			alreadyOutputtedProvinceIds.Add(province.Id);
+		}
 	}
 }
