fb9f4604e59590ab62ad665e0abfc4fc18972352
IhateTrains <IhateTrains@users.noreply.github.com>
2025-09-26 18:46:35 +0200

    Fix some problems found by ck3-tiger (#2741)

--- ImperatorToCK3/CK3/Characters/CharacterCollection.cs
+++ ImperatorToCK3/CK3/Characters/CharacterCollection.cs
@@ -678,18 +678,18 @@ internal sealed partial class CharacterCollection : ConcurrentIdObjectCollection
 		var randomForCharactersWithoutTitles = new Random((int)randomSeed);
 		foreach (var oldCharacter in oldCharacters.Except(oldTitleHolders)) {
 			// Roll a dice to determine how much longer the character will live.
-			var yearsToLive = randomForCharactersWithoutTitles.Next(0, 30);
+			var monthsToLive = randomForCharactersWithoutTitles.Next(1, 30 * 12); // Can live up to 30 years more.
 			
 			// If the character is female and pregnant, make sure she doesn't die before the pregnancy ends.
 			if (oldCharacter is {Female: true, ImperatorCharacter: not null}) {
 				var lastPregnancy = oldCharacter.Pregnancies.OrderBy(p => p.BirthDate).LastOrDefault();
 				if (lastPregnancy is not null) {
-					oldCharacter.DeathDate = lastPregnancy.BirthDate.ChangeByYears(yearsToLive);
+					oldCharacter.DeathDate = lastPregnancy.BirthDate.ChangeByMonths(monthsToLive);
 					continue;
 				}
 			}
-			
-			oldCharacter.DeathDate = irSaveDate.ChangeByYears(yearsToLive);
+
+			oldCharacter.DeathDate = irSaveDate.ChangeByMonths(monthsToLive);
 		}
 		
 		ConcurrentDictionary<string, Title[]> titlesByHolderId = new(titles
