9ed3237af3b4076e73953c2fd640519fc894745f
iht <IhateTrains@users.noreply.github.com>
2025-10-03 00:17:10 +0200

    Bump Rakaly CLI to 0.7.0

--- ImperatorToCK3/Helpers/RakalyCaller.cs
+++ ImperatorToCK3/Helpers/RakalyCaller.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 namespace ImperatorToCK3.Helpers;
 
 public static class RakalyCaller {
-	private const string RakalyVersion = "0.5.4";
+	private const string RakalyVersion = "0.7.0";
 	private static readonly string RelativeRakalyPath;
 
 	static RakalyCaller() {
