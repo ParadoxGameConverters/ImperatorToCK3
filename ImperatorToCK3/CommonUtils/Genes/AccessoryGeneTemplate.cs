6c2b8687ff2574ae55b3e7a8e06d16c2b6fe687f
IhateTrains <IhateTrains@users.noreply.github.com>
2026-03-15 10:54:19 +0100

    Optimize the usage of regexes in parsing

--- ImperatorToCK3/CommonUtils/Genes/AccessoryGeneTemplate.cs
+++ ImperatorToCK3/CommonUtils/Genes/AccessoryGeneTemplate.cs
@@ -19,21 +19,26 @@ internal sealed class AccessoryGeneTemplate : IIdentifiable<string> {
 	private void RegisterKeys(Parser parser) {
 		parser.RegisterKeyword("index", reader => Index = (uint)reader.GetInt());
 		parser.RegisterKeyword("set_tags", ParserHelpers.IgnoreItem);
-		parser.RegisterRegex("male|female|boy|girl", (reader, ageSexStr) => {
-			var stringOfItem = new StringOfItem(reader).ToString();
-			var tempStream = new BufferedReader(stringOfItem);
-			if (stringOfItem.Contains('{')) { // for full blocks: "male = { 6 = hoodie 7 = t_shirt }"
-				var ageSexBlock = new WeightBlock(tempStream);
-				AgeSexWeightBlocks.Add(ageSexStr, ageSexBlock);
-			} else { // for copies: "boy = male"
-				if (AgeSexWeightBlocks.TryGetValue(stringOfItem, out var blockToCopy)) {
-					AgeSexWeightBlocks.Add(ageSexStr, blockToCopy);
-				}
-			}
-		});
+		parser.RegisterKeyword("male", (reader) => AddAgeSexWeightBlock("male", reader));
+		parser.RegisterKeyword("female", (reader) => AddAgeSexWeightBlock("female", reader));
+		parser.RegisterKeyword("boy", (reader) => AddAgeSexWeightBlock("boy", reader));
+		parser.RegisterKeyword("girl", (reader) => AddAgeSexWeightBlock("girl", reader));
 		parser.IgnoreUnregisteredItems();
 	}
-	
+
+	private void AddAgeSexWeightBlock(string ageSexStr, BufferedReader reader) {
+		var stringOfItem = new StringOfItem(reader).ToString();
+		var tempStream = new BufferedReader(stringOfItem);
+		if (stringOfItem.Contains('{')) { // for full blocks: "male = { 6 = hoodie 7 = t_shirt }"
+			var ageSexBlock = new WeightBlock(tempStream);
+			AgeSexWeightBlocks.Add(ageSexStr, ageSexBlock);
+		} else { // for copies: "boy = male"
+			if (AgeSexWeightBlocks.TryGetValue(stringOfItem, out var blockToCopy)) {
+				AgeSexWeightBlocks.Add(ageSexStr, blockToCopy);
+			}
+		}
+	}
+
 	public int ObjectCountForAgeSex(string ageSex) {
 		return AgeSexWeightBlocks.TryGetValue(ageSex, out var weightBlock) ? weightBlock.ObjectCount : 0;
 	}
