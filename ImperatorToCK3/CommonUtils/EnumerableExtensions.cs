66f2cc8b2ff0741c56fe2c48c3f893e7291efc03
codefactor-io <support@codefactor.io>
2026-03-17 00:49:07 +0000

    [CodeFactor] Apply fixes to commit 625a05e

--- ImperatorToCK3/CommonUtils/EnumerableExtensions.cs
+++ ImperatorToCK3/CommonUtils/EnumerableExtensions.cs
@@ -6,12 +6,8 @@ namespace ImperatorToCK3.CommonUtils;
 
 public static class EnumerableExtensions {
 	public static T? LastOrNull<T>(this IEnumerable<T> source, Func<T, bool> predicate) where T : struct {
-		if (source is null) {
-			throw new ArgumentNullException(nameof(source));
-		}
-		if (predicate is null) {
-			throw new ArgumentNullException(nameof(predicate));
-		}
+		ArgumentNullException.ThrowIfNull(source);
+		ArgumentNullException.ThrowIfNull(predicate);
 
 		T? last = null;
 		foreach (var element in source) {
@@ -24,9 +20,7 @@ public static class EnumerableExtensions {
 
 	public static KeyValuePair<TKey, TValue>? LastOrNull<TKey, TValue>(
 		this IEnumerable<KeyValuePair<TKey, TValue>> source) {
-		if (source is null) {
-			throw new ArgumentNullException(nameof(source));
-		}
+		ArgumentNullException.ThrowIfNull(source);
 
 		KeyValuePair<TKey, TValue>? last = null;
 		foreach (var kvp in source) {
