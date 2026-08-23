a443790bf780c592490356c0f46cea1cd2ccd472
IhateTrains <IhateTrains@users.noreply.github.com>
2026-08-23 03:32:20 +0200

    Mod docs generator (#998)
    Executed like this:
    
    ![image](https://user-images.githubusercontent.com/29546927/194772923-38d0ebcc-49b8-4e8b-b01e-e0ec50e255ba.png)
    
    Generates a `generated_docs` folder, currently with just
    `cultures_table.html`.
    
    [generated_docs.zip](https://github.com/ParadoxGameConverters/ImperatorToCK3/files/9742293/generated_docs.zip)
    
    ![image](https://user-images.githubusercontent.com/29546927/194773140-a4134234-648e-46bf-891f-8ca26e500431.png)

---
+++ DocsGenerator/Program.cs
@@ -0,0 +1,52 @@
+﻿using CommandLine;
+using commonItems;
+using commonItems.Colors;
+using commonItems.Localization;
+using commonItems.Mods;
+using DocsGenerator;
+using System.Collections.Generic;
+using Parser = CommandLine.Parser;
+
+Parser.Default.ParseArguments<Options>(args)
+	.WithParsed(o => {
+		var gameRoot = o.GameRoot;
+		var modPath = o.ModPath;
+		bool cultureColorUnderName = o.CultureColorUnderName;
+
+		if (!Directory.Exists(gameRoot)) {
+			Logger.Error($"\"{gameRoot}\" is not a directory.");
+			return;
+		}
+		if (!Directory.Exists(modPath)) {
+			Logger.Error($"\"{modPath}\" is not a directory.");
+			return;
+		}
+
+		Logger.Info($"Generating docs for mod located in \"{modPath}\"...");
+		Directory.CreateDirectory("generated_docs");
+
+		var mod = new Mod("analyzed mod", modPath);
+		var modFS = new ModFilesystem(gameRoot, new[] {mod});
+
+		var namedColors = new NamedColorCollection();
+		namedColors.LoadNamedColors("common/named_colors", modFS);
+		var colorFactory = new ColorFactory();
+		colorFactory.AddNamedColorDict(namedColors);
+
+		var locDB = new LocDB("english");
+		locDB.ScrapeLocalizations(modFS);
+
+		// The analyzed mod is treated as a standalone mod for vanilla CK3.
+		var ck3ModFlags = new OrderedDictionary<string, bool> {
+			["tfe"] = false,
+			["wtwsms"] = false,
+			["roa"] = false,
+			["aep"] = false,
+			["confed_league"] = false,
+			["vanilla_ck3"] = true,
+		};
+
+		CulturesDocGenerator.GenerateCulturesTable(modFS, colorFactory, locDB, ck3ModFlags, cultureColorUnderName);
+
+		Logger.Info("Finished generating mod docs.");
+	});
